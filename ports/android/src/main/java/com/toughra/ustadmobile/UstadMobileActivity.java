ff166660db6a8928e7c2c0ca454ee6149898792a
Mike Dawson <mike@paiwastoon.net>
2015-06-07 17:10:46 +0400

    Add Android project

---
+++ ports/android/src/main/java/com/toughra/ustadmobile/UstadMobileActivity.java
@@ -0,0 +1,15 @@
+package com.toughra.ustadmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UstadMobileActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
