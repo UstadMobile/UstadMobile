5f0d40add14fe970638da7a3986daca247c73e3c
Varuna <varuna.singh@gmail.com>
2015-08-30 13:09:10 +0400

    Added all abstract methods

--- ports/j2me/src/com/ustadmobile/port/j2me/impl/UstadMobileSystemImplJ2ME.java
+++ ports/j2me/src/com/ustadmobile/port/j2me/impl/UstadMobileSystemImplJ2ME.java
@@ -17,6 +17,7 @@ import com.ustadmobile.core.impl.UstadMobileSystemImpl;
 import com.ustadmobile.port.j2me.app.UserPref;
 import com.ustadmobile.core.impl.HTTPResult;
 import com.ustadmobile.core.impl.UMLog;
+import com.ustadmobile.core.impl.ZipFileHandle;
 import com.ustadmobile.core.opds.UstadJSOPDSEntry;
 import com.ustadmobile.core.view.AppView;
 import java.io.InputStream;
@@ -321,6 +322,10 @@ public class UstadMobileSystemImplJ2ME  extends UstadMobileSystemImpl {
     }
 
     public InputStream getFileInputStreamFromZip(String zipURI, String filename) {
+        return null;
+    }
+
+    public ZipFileHandle openZip(String name) throws IOException {
         return null;
     }
 
