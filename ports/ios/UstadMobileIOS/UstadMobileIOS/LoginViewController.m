0779ae65603a29d31663ccd19b8573a95b80ec68
Mike Dawson <mikedawson@BENITAIR.local>
2016-08-03 21:13:18 +0400

    Add clases for LoginViewController

---
+++ ports/ios/UstadMobileIOS/UstadMobileIOS/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  UstadMobileIOS
+//
+//  Created by Mike Dawson on 03/08/16.
+//  Copyright © 2016 UstadMobile FZ-LLC. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
