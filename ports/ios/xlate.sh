0aa407ca7a553ae202ffd02e85daa6602bd2d4c8
Mike Dawson <mike@ustadmobile.com>
2016-08-30 23:11:11 +0400

    Merge HTTPD Changes into iOS - Update xcode project

--- ports/ios/xlate.sh
+++ ports/ios/xlate.sh
@@ -5,11 +5,13 @@ JAVA_SRC_DIR=../../core/src/main/java
 IMPL_SHAREDSE_DIR=../sharedse/src/main/java
 QR_SRC_DIR=lib/checkout/qrcode/src
 NANO_HTTPD_SRC_DIR=lib/checkout/nanohttpd/core/src/main/java
+NANO_HTTPD_NANOLETS_DIR=lib/checkout/nanohttpd/nanolets/src/main/java
 
 JAVA_SRC_FILES=$(find $JAVA_SRC_DIR -iname "*.java")
 IMPL_SHAREDSE_FILES=$(find $IMPL_SHAREDSE_DIR -name "*.java") 
 QR_SRC_FILES=$(find $QR_SRC_DIR -iname "*.java")
 NANO_HTTPD_FILES=$(find $NANO_HTTPD_SRC_DIR -name "*.java")
+NANO_HTTPD_NANOLETS_FILES=$(find $NANO_HTTPD_NANOLETS_DIR -name "*.java")
 
 mkdir -p lib/checkout
 cd lib/checkout
@@ -38,9 +40,10 @@ cd ../..
 pwd
 
 $J2OBJC_DIR/j2objc -d UstadMobileIOS/Generated/ \
-   -sourcepath $JAVA_SRC_DIR:$QR_SRC_DIR:$IMPL_SHAREDSE_DIR:$NANO_HTTPD_SRC_DIR \
+   -sourcepath $JAVA_SRC_DIR:$QR_SRC_DIR:$IMPL_SHAREDSE_DIR:$NANO_HTTPD_SRC_DIR:$NANO_HTTPD_NANOLETS_DIR \
    --no-package-directories $JAVA_SRC_FILES \
-   $IMPL_SHAREDSE_FILES $QR_SRC_FILES $NANO_HTTPD_FILES
+   $IMPL_SHAREDSE_FILES $QR_SRC_FILES $NANO_HTTPD_FILES \
+   $NANO_HTTPD_NANOLETS_FILES
 
 #Copy resources (e.g. locale)
 if [ -e UstadMobileIOS/res ]; then
