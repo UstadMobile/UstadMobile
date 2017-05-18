687af49f53368eecf65168131f29607d17d17e61
Lukundo Kileha <kileha3@Lukundos-MacBook-Pro.local>
2017-05-18 13:53:10 +0400

    Rework method of launching test slave server

---
+++ app-android/src/remotetestslave/java/com/ustadmobile/port/android/view/RemoteTestSlaveServerActivity.java
@@ -0,0 +1,73 @@
+package com.ustadmobile.port.android.view;
+
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.toughra.ustadmobile.R;
+import com.ustadmobile.core.impl.UstadMobileSystemImpl;
+import com.ustadmobile.port.android.netwokmanager.NetworkManagerAndroid;
+import com.ustadmobile.port.android.netwokmanager.NetworkServiceAndroid;
+import com.ustadmobile.port.sharedse.impl.UstadMobileSystemImplSE;
+import com.ustadmobile.test.core.buildconfig.TestConstants;
+import com.ustadmobile.test.sharedse.http.RemoteTestServerHttpd;
+
+import java.io.IOException;
+
+public class RemoteTestSlaveServerActivity extends UstadBaseActivity implements ServiceConnection {
+
+    private RemoteTestServerHttpd serverHttpd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_test_slave_server_acitivity);
+    }
+
+    /**
+     * UstadBaseActivity and SystemImpl will automatically bind this to the network service.
+     *
+     * Any activity that implements ServiceConnection will get a onServiceConnected call when
+     * the service is bound
+     *
+     * @param name
+     * @param iBinder
+     */
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder iBinder) {
+        super.onServiceConnected(name, iBinder);
+
+        if(iBinder instanceof NetworkServiceAndroid.LocalServiceBinder) {
+            //network service has been bound - OK to start Httpd
+            serverHttpd = new RemoteTestServerHttpd(TestConstants.TEST_REMOTE_SLAVE_SERVER_PORT,
+                    UstadMobileSystemImplSE.getInstanceSE().getNetworkManager());
+            try {
+                serverHttpd.start();
+                Toast.makeText(this, "Test slave server started", Toast.LENGTH_LONG).show();
+            }catch(IOException e) {
+                //show a pop
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                builder.setTitle("NanoHTTPD Server");
+                builder.setMessage("NanoHTTPD server failed to start");
+                builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        super.onServiceDisconnected(name);
+    }
+}
