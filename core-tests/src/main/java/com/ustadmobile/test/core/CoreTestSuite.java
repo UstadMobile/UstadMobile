b12aaec93e95dbc54653de378cb49f6cd9d547a4
Mike Dawson <mike@ustadmobile.com>
2018-01-07 01:54:42 +0400

    Add SCORM manifest processing.

--- core-tests/src/main/java/com/ustadmobile/test/core/CoreTestSuite.java
+++ core-tests/src/main/java/com/ustadmobile/test/core/CoreTestSuite.java
@@ -2,6 +2,7 @@ package com.ustadmobile.test.core;
 
 import com.ustadmobile.test.core.catalog.contenttype.TestXapiPackageTypePlugin;
 import com.ustadmobile.test.core.impl.TestUstadJSOPF;
+import com.ustadmobile.test.core.scorm.TestScormManifest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -18,7 +19,8 @@ import org.junit.runners.Suite;
         TestUstadJSOPDSEntry.class,
         TestUMFileUtil.class,
         TestUstadJSOPF.class,
-        TestUmCalendarUtil.class
+        TestUmCalendarUtil.class,
+        TestScormManifest.class
 })
 public abstract class CoreTestSuite {
 }
--- core/src/main/java/com/ustadmobile/core/scorm/ScormManifest.java
+++ core/src/main/java/com/ustadmobile/core/scorm/ScormManifest.java
@@ -7,26 +7,61 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * Represents a scorm manifest.
  *
  * Created by mike on 1/6/18.
  */
-
 public class ScormManifest {
 
     public static class Organization {
+
+        String identifier;
+
+        String title;
+
         List<OrganizationItem> items;
 
+        public Organization() {
+            items = new ArrayList<>();
+        }
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public List<OrganizationItem> getItems() {
+            return items;
+        }
     }
 
     public static class OrganizationItem {
         String title;
 
         String identifierRef;
+
+        String identifier;
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getIdentifierRef() {
+            return identifierRef;
+        }
     }
 
     public static class Resource {
@@ -37,6 +72,22 @@ public class ScormManifest {
         String scormType;
 
         String href;
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public String getiType() {
+            return iType;
+        }
+
+        public String getScormType() {
+            return scormType;
+        }
+
+        public String getHref() {
+            return href;
+        }
     }
 
     private Map<String, String> title;
@@ -51,9 +102,31 @@ public class ScormManifest {
 
     public static final String NS_IMS = "http://www.imsproject.org/xsd/imscp_rootv1p1p2";
 
+    public static final String NS_ADLCP = "http://www.adlnet.org/xsd/adlcp_rootv1p2";
+
     public static final String ATTR_IDENTIFIER = "identifier";
 
+    public static final String ATTR_IDENTIFIERREF = "identifierref";
+
+    public static final String ATTR_DEFAULT = "default";
+
+    public static final String ATTR_HREF = "href";
+
+    public static final String ATTR_TYPE = "type";
+
+    public static final String ATTR_SCORMTYPE = "scormtype";
+
     public static final String TAG_MANIFEST = "manifest";
+
+    public static final String TAG_ORGANIZATIONS = "organizations";
+
+    public static final String TAG_ORGANIZATION = "organization";
+
+    public static final String TAG_ITEM = "item";
+
+    public static final String TAG_TITLE = "title";
+
+    public static final String TAG_RESOURCE = "resource";
 
 
     public ScormManifest() {
@@ -65,13 +138,55 @@ public class ScormManifest {
         int evtType;
         String tagName;
 
+        Organization currentOrg = null;
+        OrganizationItem currentOrgItem = null;
+
+        Resource currentResource;
+
         while((evtType = xpp.next()) != XmlPullParser.END_DOCUMENT) {
             switch(evtType) {
                 case XmlPullParser.START_TAG:
                     tagName = xpp.getName();
 
                     if(TAG_MANIFEST.equals(tagName)) {
-                        identifier = xpp.getAttributeValue(NS_IMS, ATTR_IDENTIFIER);
+                        identifier = xpp.getAttributeValue(null, ATTR_IDENTIFIER);
+                    }else if(TAG_ORGANIZATIONS.equals(tagName)) {
+                        defaultOrganizationIdentifier = xpp.getAttributeValue(null, ATTR_DEFAULT);
+                    }else if(TAG_ORGANIZATION.equals(tagName)) {
+                        currentOrg = new Organization();
+                        currentOrg.identifier = xpp.getAttributeValue(null, ATTR_IDENTIFIER);
+                        organizationsMap.put(currentOrg.identifier, currentOrg);
+                    }else if(currentOrg != null && TAG_ITEM.equals(tagName)) {
+                        currentOrgItem = new OrganizationItem();
+                        currentOrgItem.identifierRef = xpp.getAttributeValue(null, ATTR_IDENTIFIERREF);
+                        currentOrgItem.identifier = xpp.getAttributeValue(null, ATTR_IDENTIFIER);
+                        currentOrg.items.add(currentOrgItem);
+                    }else if(currentOrgItem != null && TAG_TITLE.equals(tagName)
+                            && xpp.next() == XmlPullParser.TEXT) {
+                        currentOrgItem.title = xpp.getText();
+                    }else if(currentOrg != null && TAG_TITLE.equals(tagName)
+                        && xpp.next() == XmlPullParser.TEXT) {
+                        currentOrg.title = xpp.getText();
+                    }else if(TAG_RESOURCE.equals(tagName)) {
+                        currentResource = new Resource();
+                        currentResource.identifier = xpp.getAttributeValue(null, ATTR_IDENTIFIER);
+                        currentResource.iType = xpp.getAttributeValue(null, ATTR_TYPE);
+                        currentResource.href = xpp.getAttributeValue(null, ATTR_HREF);
+                        currentResource.scormType = xpp.getAttributeValue(NS_ADLCP, ATTR_SCORMTYPE);
+                        resourceMap.put(currentResource.identifier, currentResource);
+                    }
+
+                    break;
+
+                case XmlPullParser.END_TAG:
+                    tagName = xpp.getName();
+
+                    if(TAG_ORGANIZATION.equals(tagName)) {
+                        currentOrg = null;
+                    }else if(TAG_ITEM.equals(tagName)) {
+                        currentOrgItem = null;
+                    }else if(TAG_RESOURCE.equals(tagName)){
+                        currentResource = null;
                     }
 
                     break;
@@ -105,5 +220,17 @@ public class ScormManifest {
 
     public void setIdentifier(String identifier) {
         this.identifier = identifier;
+    }
+
+    public Organization getOrganizationByIdentifier(String identifier){
+        return organizationsMap.get(identifier);
+    }
+
+    public String getDefaultOrganizationIdentifier() {
+        return defaultOrganizationIdentifier;
+    }
+
+    public Resource getResourceByIdentifier(String identifier) {
+        return resourceMap.get(identifier);
     }
 }
