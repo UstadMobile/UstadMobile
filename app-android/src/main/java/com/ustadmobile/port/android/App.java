8d4b78afc158a24a07342d20684f65e6170af380
Mike Dawson <mike@ustadmobile.com>
2018-02-27 01:24:34 +0200

    Configure ACRA crash and error reporting

--- app-android/src/main/java/com/ustadmobile/port/android/App.java
+++ app-android/src/main/java/com/ustadmobile/port/android/App.java
@@ -1,12 +1,23 @@
 package com.ustadmobile.port.android;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.evernote.android.job.Job;
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
+import com.toughra.ustadmobile.BuildConfig;
 import com.ustadmobile.port.android.job.UMSyncJob;
 import com.ustadmobile.port.android.job.UMSyncJobCreator;
+
+import org.acra.ACRA;
+import org.acra.ErrorReporter;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraHttpSender;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.HttpSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
+import org.acra.sender.HttpSender;
 
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -29,54 +40,54 @@ public class App extends UmBaseApplication {
         System.out.println("UMSyncJob:App:");
         super.onCreate();
 
-        if(mJobManager == null){
-            JobManager.create(this).addJobCreator(new UMSyncJobCreator());
-            mJobManager = JobManager.instance();
-        }
-
-
-        Set<Job> allJobs = mJobManager.getAllJobs();
-        Set<Job> allUMSyncJobs = mJobManager.getAllJobsForTag(UMSyncJob.TAG);
-        Set<JobRequest> allJobRequests = mJobManager.getAllJobRequests();
-        Set<JobRequest> allUMSyncJobRequests = mJobManager.getAllJobRequestsForTag(UMSyncJob.TAG);
-
-        System.out.println(" UMSyncJob:App: Currently there are :"
-                + allJobs.size() + " jobs running.");
-        System.out.println(" UMSyncJob:App: Currently there are :"
-                + allUMSyncJobs.size() +
-                " UMSyncJobs running.");
-
-        System.out.println(" UMSyncJob:App: Currently there are: "
-                + allJobRequests.size() + " job REQUESTS.");
-        System.out.println(" UMSyncJob:App: Currently there are: "
-                + allUMSyncJobRequests.size() + " UMSyncJob REQUESTS.");
-
-        for(JobRequest thisJobRequest:allUMSyncJobRequests){
-            //sup?
-        }
-
-        /* just in case condition */
-        if(allUMSyncJobRequests.size() > 5){
-            System.out.println(" >>UMSyncJob:App: More than 5 job requests. Cancelling all.");
-            mJobManager.cancelAllForTag(UMSyncJob.TAG);
-        }
-
-        if(mJobManager.getAllJobRequestsForTag(UMSyncJob.TAG).size() > 0){
-            System.out.println(" UMSyncJob:App UMSyncJob Requests running. Monitor this: size: "
-                    + allUMSyncJobRequests.size());
-            System.out.println(" >>UMSyncJob:App UMSyncJob Not running any more jobs..");
-        }
-        else
-        if(syncJobId > -1 && mJobManager.getJobRequest(syncJobId) == null
-                && mJobManager.getJob(syncJobId) == null){
-            System.out.println(" >>UMSyncJob:App: Job : " + syncJobId + " not running. Scheduling it..");
-            //System.out.println("UMSyncJob:App: DISABLING SCHEDULING FOR NOW..TODO: FIX");
-            scheduleJob();
-            System.out.print("\n");
-        }else{
-            System.out.println(" >>UMSyncJob:App: Job: " + syncJobId + " is already running. " +
-                    "Not scheduling again.");
-        }
+//        if(mJobManager == null){
+//            JobManager.create(this).addJobCreator(new UMSyncJobCreator());
+//            mJobManager = JobManager.instance();
+//        }
+//
+//
+//        Set<Job> allJobs = mJobManager.getAllJobs();
+//        Set<Job> allUMSyncJobs = mJobManager.getAllJobsForTag(UMSyncJob.TAG);
+//        Set<JobRequest> allJobRequests = mJobManager.getAllJobRequests();
+//        Set<JobRequest> allUMSyncJobRequests = mJobManager.getAllJobRequestsForTag(UMSyncJob.TAG);
+//
+//        System.out.println(" UMSyncJob:App: Currently there are :"
+//                + allJobs.size() + " jobs running.");
+//        System.out.println(" UMSyncJob:App: Currently there are :"
+//                + allUMSyncJobs.size() +
+//                " UMSyncJobs running.");
+//
+//        System.out.println(" UMSyncJob:App: Currently there are: "
+//                + allJobRequests.size() + " job REQUESTS.");
+//        System.out.println(" UMSyncJob:App: Currently there are: "
+//                + allUMSyncJobRequests.size() + " UMSyncJob REQUESTS.");
+//
+//        for(JobRequest thisJobRequest:allUMSyncJobRequests){
+//            //sup?
+//        }
+//
+//        /* just in case condition */
+//        if(allUMSyncJobRequests.size() > 5){
+//            System.out.println(" >>UMSyncJob:App: More than 5 job requests. Cancelling all.");
+//            mJobManager.cancelAllForTag(UMSyncJob.TAG);
+//        }
+//
+//        if(mJobManager.getAllJobRequestsForTag(UMSyncJob.TAG).size() > 0){
+//            System.out.println(" UMSyncJob:App UMSyncJob Requests running. Monitor this: size: "
+//                    + allUMSyncJobRequests.size());
+//            System.out.println(" >>UMSyncJob:App UMSyncJob Not running any more jobs..");
+//        }
+//        else
+//        if(syncJobId > -1 && mJobManager.getJobRequest(syncJobId) == null
+//                && mJobManager.getJob(syncJobId) == null){
+//            System.out.println(" >>UMSyncJob:App: Job : " + syncJobId + " not running. Scheduling it..");
+//            //System.out.println("UMSyncJob:App: DISABLING SCHEDULING FOR NOW..TODO: FIX");
+//            scheduleJob();
+//            System.out.print("\n");
+//        }else{
+//            System.out.println(" >>UMSyncJob:App: Job: " + syncJobId + " is already running. " +
+//                    "Not scheduling again.");
+//        }
     }
 
     public void scheduleJob(){
@@ -87,14 +98,30 @@ public class App extends UmBaseApplication {
         //Nothing to force requirement check (as above)
 
         //mJobManager = JobManager.instance();
-        syncJobId = new JobRequest.Builder(UMSyncJob.TAG)
-                //15 min is min and flex min is 5 min
-                .setPeriodic(TimeUnit.MINUTES.toMillis(15), TimeUnit.MINUTES.toMillis(5))
-                //Will make sure it gets run on boot
-                .setPersisted(true)
-                .build()
-                .schedule();
-        System.out.println("UMSyncJob:App: Job scheduled. ID: " + syncJobId);
+//        syncJobId = new JobRequest.Builder(UMSyncJob.TAG)
+//                //15 min is min and flex min is 5 min
+//                .setPeriodic(TimeUnit.MINUTES.toMillis(15), TimeUnit.MINUTES.toMillis(5))
+//                //Will make sure it gets run on boot
+//                .setPersisted(true)
+//                .build()
+//                .schedule();
+//        System.out.println("UMSyncJob:App: Job scheduled. ID: " + syncJobId);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
+        builder.setReportFormat(StringFormat.JSON);
+
+        builder.getPluginConfigurationBuilder(HttpSenderConfigurationBuilder.class)
+                .setUri(BuildConfig.ACRA_HTTP_URI)
+                .setBasicAuthLogin(BuildConfig.ACRA_BASIC_LOGIN)
+                .setBasicAuthPassword(BuildConfig.ACRA_BASIC_PASS)
+                .setHttpMethod(HttpSender.Method.POST).setEnabled(true);
+
+        ACRA.init(this, builder);
     }
 }
 
