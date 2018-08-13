add3c85e28c974f3ce26931646d4f0ae2b69f2fb
Lukundo Kileha <lkileha@gmail.com>
2018-08-13 15:13:21 +0400

    Added BleEntryStatusTask with its android port class

--- app-android/src/main/java/com/ustadmobile/port/android/netwokmanager/BleEntryStatusTaskAndroid.java
+++ app-android/src/main/java/com/ustadmobile/port/android/netwokmanager/BleEntryStatusTaskAndroid.java
@@ -1,4 +1,25 @@
 package com.ustadmobile.port.android.netwokmanager;
 
-public class BleEntryStatusTaskAndroid {
+import com.ustadmobile.lib.db.entities.NetworkNode;
+import com.ustadmobile.port.sharedse.networkmanager.BleEntryStatusTask;
+
+import java.util.List;
+
+public class BleEntryStatusTaskAndroid extends BleEntryStatusTask {
+
+    private BleMessageGattClientCallback mCallback;
+
+    public BleEntryStatusTaskAndroid(List<Long> entryUidsToCheck, NetworkNode peerToCheck) {
+        super(entryUidsToCheck, peerToCheck);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+    public BleMessageGattClientCallback getGattClientCallback() {
+        return mCallback;
+    }
 }
--- sharedse/src/main/java/com/ustadmobile/port/sharedse/networkmanager/BleEntryStatusTask.java
+++ sharedse/src/main/java/com/ustadmobile/port/sharedse/networkmanager/BleEntryStatusTask.java
@@ -1,4 +1,29 @@
 package com.ustadmobile.port.sharedse.networkmanager;
 
-public class BleEntryStatusTask implements Runnable {
+import com.ustadmobile.lib.db.entities.NetworkNode;
+
+import java.util.List;
+
+/**
+ * <h1>BleEntryStatusTask</h1>
+ *
+ * BleEntryStatusTask is executed by an Executor. It will query a given peer to determine if
+ * it has a set of entryUids for local download.
+ */
+public abstract class BleEntryStatusTask implements Runnable,BleMessageResponseListener {
+
+    public BleMessage message;
+
+    public BleEntryStatusTask(List<Long> entryUidsToCheck, NetworkNode peerToCheck) {
+
+    }
+
+    @Override
+    public void onResponseReceived(BleMessage response) {
+
+    }
+
+    public BleMessage getMessage() {
+        return message;
+    }
 }
