f00d0640f168180ffb0d964d4eb7c92cc9158bb3
Lukundo Kileha <lkileha@gmail.com>
2018-08-15 11:34:51 +0400

    Added util method for entry Id conversion from list of Long to bytes and vice-versa

---
+++ sharedse/src/test/java/com/ustadmobile/port/sharedse/networkmanager/BleMessageUtilTest.java
@@ -0,0 +1,37 @@
+package com.ustadmobile.port.sharedse.networkmanager;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * <h1>BleMessageUtilTest</h1>
+ *
+ * test class which test {{@link BleMessageUtil}} to make sure it behaves as expected when
+ * converting List of entries to bytes and vice-versa
+ * @author kileha3
+ */
+public class BleMessageUtilTest {
+
+    @Test
+    public void givenListOfEntriesInLong_whenConverted_shouldReturnBytes(){
+        List<Long> entryList = Arrays.asList(1056289670L,4590875612L,9076137860L,2912543894L);
+
+        byte[] entriesInBytes = BleMessageUtil.bleMessageLongToBytes(entryList);
+
+        assertTrue("List was converted to bytes ",entriesInBytes.getClass().isArray());
+    }
+
+    @Test
+    public void givenListOfEntriesInLong_whenConvertedToBytesAndReConvertedBackToList_thenShouldBeEqual(){
+        List<Long> entryList = Arrays.asList(1056289670L,4590875612L,9076137860L,2912543894L,8965341254L);
+
+        byte[] entriesInBytes = BleMessageUtil.bleMessageLongToBytes(entryList);
+        List<Long> entriesInListOfLong =  BleMessageUtil.bleMessageBytesToLong(entriesInBytes);
+
+        assertTrue("Lists are equal",entryList.equals(entriesInListOfLong));
+    }
+}
