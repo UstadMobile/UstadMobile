7714524c0c22ab97e704e169b22e46193c685ae0
Lukundo Kileha <lkileha@gmail.com>
2018-08-15 10:02:53 +0400

    Added callbacks logic to handle test cases

--- app-android/src/main/java/com/ustadmobile/port/android/netwokmanager/BleGattServerAndroid.java
+++ app-android/src/main/java/com/ustadmobile/port/android/netwokmanager/BleGattServerAndroid.java
@@ -1,17 +1,24 @@
 package com.ustadmobile.port.android.netwokmanager;
 
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import android.util.Log;
+
+import com.ustadmobile.port.sharedse.networkmanager.BleMessage;
+
+import static com.ustadmobile.port.sharedse.networkmanager.NetworkManagerBle.USTADMOBILE_BLE_SERVICE_UUID;
 
 public class BleGattServerAndroid extends BleGattServer {
 
-    private BluetoothManager bluetoothManager;
-
     private BluetoothGattServer gattServer;
+
+    private BleMessage receivedMessage;
 
     private BluetoothGattServerCallback mCallback = new BluetoothGattServerCallback() {
         @Override
@@ -22,11 +29,27 @@ public class BleGattServerAndroid extends BleGattServer {
         @Override
         public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset, BluetoothGattCharacteristic characteristic) {
             super.onCharacteristicReadRequest(device, requestId, offset, characteristic);
+
+            boolean needResponse = characteristic.getProperties() == BluetoothGattCharacteristic.PROPERTY_WRITE;
+            if(needResponse){
+                //Reject all direct characteristics read from unknown source (one of our characteristics has NO_RESPONSE set).
+                gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_FAILURE, 0, characteristic.getValue());
+            }else{
+                gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, characteristic.getValue());
+
+            }
         }
 
         @Override
         public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
             super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite, responseNeeded, offset, value);
+
+            if (USTADMOBILE_BLE_SERVICE_UUID.equals(characteristic.getUuid())) {
+                //Grant permission to the client device to write on this characteristics
+                gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, 0, null);
+                //start receiving packets from the client device
+                receivedMessage.onPackageReceived(value);
+            }
         }
 
 
@@ -37,11 +60,12 @@ public class BleGattServerAndroid extends BleGattServer {
     };
 
     public BleGattServerAndroid(Context context, BluetoothManager bluetoothManager) {
-
+        this.receivedMessage = new BleMessage();
+        this.gattServer = bluetoothManager.openGattServer(context,mCallback);
     }
 
 
-    //For testing purposes
+    @VisibleForTesting
     protected BluetoothGattServerCallback getGattServerCallback() {
         return mCallback;
     }
