c0f07c92cf3ed655c54ba8ad8c9cd949dd5eab5f
Varuna Singh <varuna.singh@gmail.com>
2018-08-25 18:19:06 +0400

    Added ClazzMember Dao and returning it when asked for ClazzMember's Dao

---
+++ core/src/main/java/com/ustadmobile/core/db/dao/ClazzMemberDao.java
@@ -0,0 +1,20 @@
+package com.ustadmobile.core.db.dao;
+
+import com.ustadmobile.core.impl.UmCallback;
+import com.ustadmobile.lib.database.annotation.UmDao;
+import com.ustadmobile.lib.database.annotation.UmInsert;
+import com.ustadmobile.lib.database.annotation.UmQuery;
+import com.ustadmobile.lib.db.entities.ClazzMember;
+
+@UmDao
+public abstract class ClazzMemberDao implements BaseDao<ClazzMember> {
+
+    @UmInsert
+    public abstract long insert(ClazzMember entity);
+
+    @UmInsert
+    public abstract void insertAsync(ClazzMember entity, UmCallback<Long> result);
+
+    @UmQuery("SELECT * FROM ClazzMember WHERE clazzMemberUid = :uid")
+    public abstract ClazzMember findByUid(long uid);
+}
