d46cd133fc8dcab56b5baf8a80d9d49b525cf44f
Lukundo Kileha <lkileha@gmail.com>
2018-10-09 16:11:56 +0400

    Modified query to fetch all the WamdaClazz with social info person

--- lib-database/src/main/java/com/ustadmobile/core/db/dao/WamdaClazzDao.java
+++ lib-database/src/main/java/com/ustadmobile/core/db/dao/WamdaClazzDao.java
@@ -22,14 +22,14 @@ public abstract class WamdaClazzDao {
             "FROM Clazz")
     public abstract UmProvider<WamdaClazzWithSocialInfo> findAll(long personUid);
 
-    @UmQuery("SELECT Clazz.* , WamdaClazz.*, Person.*, " +
-            "(SELECT COUNT(*) FROM WamdaLike WHERE WamdaLike.wamdaLikeClazzUid = Clazz.clazzUid) AS numLikes," +
-            "(SELECT COUNT(*) FROM ClazzMember WHERE ClazzMember.clazzMemberClazzUid = Clazz.clazzUid) AS numStudents," +
-            "(SELECT  COUNT(*) FROM WamdaShare WHERE WamdaShare.wamdaShareClazzUid = Clazz.clazzUid) AS numShares " +
-            "FROM Clazz " +
-            "LEFT JOIN WamdaClazz ON Clazz.clazzUid = WamdaClazz.wamdaClazzClazzUid " +
-            "AND Clazz.clazzUid = :wamdaClazzUid "+
-            "LEFT JOIN Person ON Person.personUid = WamdaClazz.wamdaClazzPersonUid ")
+    @UmQuery("SELECT Clazz.* , WamdaClazz.*, Person.*, \n" +
+            "(SELECT COUNT(*) FROM WamdaLike WHERE WamdaLike.wamdaLikeClazzUid = Clazz.clazzUid) AS numLikes,\n" +
+            "(SELECT COUNT(*) FROM ClazzMember WHERE ClazzMember.clazzMemberClazzUid = Clazz.clazzUid) AS numStudents,\n" +
+            "(SELECT  COUNT(*) FROM WamdaShare WHERE WamdaShare.wamdaShareClazzUid = Clazz.clazzUid) AS numShares \n" +
+            "FROM Clazz \n" +
+            "LEFT JOIN WamdaClazz ON Clazz.clazzUid = WamdaClazz.wamdaClazzClazzUid \n" +
+            "LEFT JOIN Person ON Person.personUid = WamdaClazz.wamdaClazzPersonUid\n" +
+            "WHERE Clazz.clazzUid = :wamdaClazzUid")
     public abstract UmLiveData<WamdaClazzWithSocialInfoClazzMember> findByClazzUid(long wamdaClazzUid);
 
     @UmInsert
