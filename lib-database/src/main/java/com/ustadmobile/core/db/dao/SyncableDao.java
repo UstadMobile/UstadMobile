3c4cc1122c71fdbd019e5d0aa8de54a5d4ac6b2a
Mike Dawson <mike@ustadmobile.com>
2018-10-16 14:21:20 +0430

    Base syncable DAO

--- lib-database/src/main/java/com/ustadmobile/core/db/dao/SyncableDao.java
+++ lib-database/src/main/java/com/ustadmobile/core/db/dao/SyncableDao.java
@@ -1,4 +1,15 @@
 package com.ustadmobile.core.db.dao;
 
-public class SyncableDao {
+import java.util.List;
+
+public interface SyncableDao<T> {
+
+    void syncWith(SyncableDao<T> otherDao);
+
+    List<T> handlingIncomingSync(List<T> incomingChanges, long fromChangeSequenceNumber,
+                                 long userId);
+
+
+
+
 }
