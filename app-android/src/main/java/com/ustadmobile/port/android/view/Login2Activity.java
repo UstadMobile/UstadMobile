8dd9301c79cb421d4de0ef53511f1b29d32159b1
Mike Dawson <mike@ustadmobile.com>
2018-12-12 16:33:17 +0200

    Update Login screen related items

---
+++ app-android/src/main/java/com/ustadmobile/port/android/view/Login2Activity.java
@@ -0,0 +1,15 @@
+package com.ustadmobile.port.android.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.toughra.ustadmobile.R;
+
+public class Login2Activity extends UstadBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
