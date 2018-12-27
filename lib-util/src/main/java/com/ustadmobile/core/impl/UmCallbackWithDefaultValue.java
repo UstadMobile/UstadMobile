8b3e2e464dd28113753197579fc7c57a0c0d23e8
Mike Dawson <mike@ustadmobile.com>
2018-12-27 13:55:44 +0400

    Add callback wrapper with default onSuccess value in case of null result

---
+++ lib-util/src/main/java/com/ustadmobile/core/impl/UmCallbackWithDefaultValue.java
@@ -0,0 +1,25 @@
+package com.ustadmobile.core.impl;
+
+public class UmCallbackWithDefaultValue<T> implements UmCallback<T>{
+
+    private T defaultVal;
+
+    private UmCallback<T> callback;
+
+    public UmCallbackWithDefaultValue(T defaultVal, UmCallback callback) {
+        this.defaultVal = defaultVal;
+    }
+
+    @Override
+    public void onSuccess(T result) {
+        if(result != null)
+            UmCallbackUtil.onSuccessIfNotNull(callback, result);
+        else
+            UmCallbackUtil.onSuccessIfNotNull(callback, defaultVal);
+    }
+
+    @Override
+    public void onFailure(Throwable exception) {
+        UmCallbackUtil.onFailIfNotNull(callback, exception);
+    }
+}
