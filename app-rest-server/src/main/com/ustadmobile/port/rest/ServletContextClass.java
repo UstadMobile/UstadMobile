76c4efe0ddb42a9018433efdfea0fecbb4426da2
Varuna Singh <varuna.singh@gmail.com>
2019-01-15 14:46:41 +0400

    Removed officer creation

--- app-rest-server/src/main/com/ustadmobile/port/rest/ServletContextClass.java
+++ app-rest-server/src/main/com/ustadmobile/port/rest/ServletContextClass.java
@@ -103,7 +103,7 @@ public class ServletContextClass implements ServletContextListener
             addRolesAndPermissions();
 
             //Create officer
-            createOfficer();
+            //createOfficer();
 
         }
 
