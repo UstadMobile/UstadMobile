7e49abdcecf8c452371f3c83290d5aad55401ebb
Varuna Singh <varuna.singh@gmail.com>
2019-01-29 16:08:34 +0400

    Fix bug in Servlet Context that was creating sel user twice.

--- app-rest-server/src/main/com/ustadmobile/port/rest/ServletContextClass.java
+++ app-rest-server/src/main/com/ustadmobile/port/rest/ServletContextClass.java
@@ -324,7 +324,7 @@ public class ServletContextClass implements ServletContextListener
                             }
                         });
 
-                        createSELOfficer();
+                        //createSELOfficer();
 
                     }
 
