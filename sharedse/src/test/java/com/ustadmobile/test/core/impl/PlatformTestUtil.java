6ea26eb78f40112264ac93974640df415c149f2a
Mike Dawson <mike@ustadmobile.com>
2019-01-30 15:49:12 +0400

    Refactor sharedse tests to remove obsolete code

--- sharedse/src/test/java/com/ustadmobile/test/core/impl/PlatformTestUtil.java
+++ sharedse/src/test/java/com/ustadmobile/test/core/impl/PlatformTestUtil.java
@@ -1,10 +1,6 @@
 package com.ustadmobile.test.core.impl;
 
 
-import com.ustadmobile.core.impl.UstadMobileSystemImpl;
-import com.ustadmobile.test.port.sharedse.impl.UstadMobileSystemImplTestSE;
-
-
 /**
  * TestUtil is designed to abstract away the differences between conducting testing on "smart"
  * devices where we can run NanoHTTPD and J2ME where we need to use an external server.
@@ -23,8 +19,4 @@ public class PlatformTestUtil {
         return new Object();
     }
 
-    public static String getRemoteTestEndpoint() {
-        UstadMobileSystemImplTestSE implTestSE = (UstadMobileSystemImplTestSE)UstadMobileSystemImpl.getInstance();
-        return "http://localhost:" + implTestSE.getMockTestDriver().getTestControlServerPort() + "/";
-    }
 }
