2abd6c5a9c9826c53379222d53e30d2d0bd345ec
Mike Dawson <mike@ustadmobile.com>
2019-02-27 02:41:25 +0400

    Add support for ACRA crash reporting on Android

---
+++ app-android-launcher/src/vanillamultidex/java/com/ustadmobile/port/android/UmBaseApplication.java
@@ -0,0 +1,11 @@
+package com.ustadmobile.port.android;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by mike on 2/3/18.
+ */
+
+public class UmBaseApplication extends MultiDexApplication {
+
+}
