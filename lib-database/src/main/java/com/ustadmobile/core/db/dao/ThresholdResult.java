8386b0086490ec458abf6aa95a409b24f6a960f7
Varuna Singh <varuna.singh@gmail.com>
2019-05-02 15:15:49 +0400

    Fixed Attendance bar chart logic.

---
+++ lib-database/src/main/java/com/ustadmobile/core/db/dao/ThresholdResult.java
@@ -0,0 +1,30 @@
+package com.ustadmobile.core.db.dao;
+
+public class ThresholdResult {
+
+    float low, mid, high;
+
+    public float getLow() {
+        return low;
+    }
+
+    public void setLow(float low) {
+        this.low = low;
+    }
+
+    public float getMid() {
+        return mid;
+    }
+
+    public void setMid(float mid) {
+        this.mid = mid;
+    }
+
+    public float getHigh() {
+        return high;
+    }
+
+    public void setHigh(float high) {
+        this.high = high;
+    }
+}
