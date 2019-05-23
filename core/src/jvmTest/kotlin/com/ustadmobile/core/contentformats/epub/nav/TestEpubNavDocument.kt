dbf926b95c810f1bd35be9914762b6f46f7cb028
LAPTOP-42LH8G8N\Samih <samih@ustadmobile.com>
2019-05-23 10:44:02 +0400

    moved tests to jvmTest

---
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/contentformats/epub/nav/TestEpubNavDocument.kt
@@ -0,0 +1,54 @@
+package com.ustadmobile.core.contentformats.epub.nav
+
+import com.ustadmobile.core.impl.UstadMobileSystemImpl
+import org.junit.Assert
+import org.junit.Test
+import org.kmp.io.KMPPullParserException
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+
+class TestEpubNavDocument {
+
+    @Test
+    @Throws(KMPPullParserException::class, IOException::class)
+    fun givenValidDoc_whenParsed_thenPropertiesShouldMatchFile() {
+        val navDoc = EpubNavDocument()
+        val docIn = javaClass.getResourceAsStream("TestEPUBNavDocument-valid.xhtml")
+
+        navDoc.load(
+                UstadMobileSystemImpl.instance.newPullParser(docIn, "UTF-8"))
+
+        Assert.assertNotNull("Navigation doc has found table of contents", navDoc.toc)
+        Assert.assertEquals("Navigation doc has 7 children", 7,
+                navDoc.toc!!.size().toLong())
+    }
+
+
+    @Test
+    @Throws(KMPPullParserException::class, IOException::class)
+    fun givenDocLoaded_whenSerializedAndReloaded_thenShouldBeTheSame() {
+        val navDoc = EpubNavDocument()
+        val docIn = javaClass.getResourceAsStream("TestEPUBNavDocument-valid.xhtml")
+        navDoc.load(
+                UstadMobileSystemImpl.instance.newPullParser(docIn, "UTF-8"))
+
+        val serializer = UstadMobileSystemImpl.instance.newXMLSerializer()
+        val bout = ByteArrayOutputStream()
+        serializer.setOutput(bout, "UTF-8")
+        navDoc.serialize(serializer)
+        bout.flush()
+
+
+        val loadedDoc = EpubNavDocument()
+        val loadedXpp = UstadMobileSystemImpl.instance.newPullParser(
+                ByteArrayInputStream(bout.toByteArray()), "UTF-8")
+        loadedDoc.load(loadedXpp)
+
+        Assert.assertEquals("Loaded and reserialized docs have same toc id",
+                navDoc.toc!!.id, loadedDoc.toc!!.id)
+        Assert.assertEquals("Loaded and reserialized tocs have same number of child entries",
+                navDoc.toc!!.size().toLong(), loadedDoc.toc!!.size().toLong())
+    }
+
+}
