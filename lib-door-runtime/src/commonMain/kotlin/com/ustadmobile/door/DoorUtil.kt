a3e60b1f4ab253d34738ba238546c96c4382c8c3
Mike Dawson <mike@ustadmobile.com>
2019-05-28 15:41:17 +0430

    Move util function to avoid compile error

--- lib-door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorUtil.kt
+++ lib-door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorUtil.kt
@@ -1,10 +1,9 @@
 package com.ustadmobile.door
 
-fun <T: Any> doorObserve(liveData: DoorLiveData<T>, lifecycleOwner: Any, observer:(T) -> Unit) {
-    liveData.observe(lifecycleOwner as DoorLifecycleOwner, object : DoorObserver<T> {
+fun <T: Any?> DoorLiveData<T>.observe(lifecycleOwner: Any, observer: (T) -> Unit) {
+    this.observe(lifecycleOwner as DoorLifecycleOwner, object : DoorObserver<T> {
         override fun onChanged(t: T) {
             observer.invoke(t)
         }
     })
 }
-
