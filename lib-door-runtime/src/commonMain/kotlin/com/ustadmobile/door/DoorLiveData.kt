147c391535cd91151ab13eebe03d628d023406d1
Mike Dawson <mike@ustadmobile.com>
2019-05-28 15:11:48 +0430

    Add convenience top level observer method

--- lib-door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorLiveData.kt
+++ lib-door-runtime/src/commonMain/kotlin/com/ustadmobile/door/DoorLiveData.kt
@@ -1,5 +1,13 @@
 package com.ustadmobile.door
 
+fun <T: Any> doorObserve(liveData: DoorLiveData<T>, lifecycleOwner: Any, observer:(T) -> Unit) {
+    liveData.observe(lifecycleOwner as DoorLifecycleOwner, object : DoorObserver<T> {
+        override fun onChanged(t: T) {
+            observer.invoke(t)
+        }
+    })
+}
+
 expect abstract class DoorLiveData<T>() {
 
     open fun observe(lifecycleOwner: DoorLifecycleOwner, observer: DoorObserver<in T>)
