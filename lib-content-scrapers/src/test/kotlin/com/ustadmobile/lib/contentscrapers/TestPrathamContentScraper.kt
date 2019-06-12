45e277278ddd511ef6d1172a9245f2253f1c1bb3
LAPTOP-42LH8G8N\Samih <samih@ustadmobile.com>
2019-06-12 15:20:52 +0400

    converted tests to kotlin

---
+++ lib-content-scrapers/src/test/kotlin/com/ustadmobile/lib/contentscrapers/TestPrathamContentScraper.kt
@@ -0,0 +1,164 @@
+package com.ustadmobile.lib.contentscrapers
+
+import com.ustadmobile.core.db.UmAppDatabase
+import com.ustadmobile.lib.contentscrapers.africanbooks.AsbScraper
+import com.ustadmobile.lib.contentscrapers.ddl.DdlContentScraper
+import com.ustadmobile.lib.contentscrapers.prathambooks.IndexPrathamContentScraper
+
+import org.apache.commons.io.IOUtils
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.net.URISyntaxException
+import java.nio.file.Files
+
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import okio.Buffer
+import okio.BufferedSource
+import okio.Okio
+
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.ETAG_TXT
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.LAST_MODIFIED_TXT
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.UTF_ENCODING
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.spy
+
+class TestPrathamContentScraper {
+
+
+    internal val dispatcher: Dispatcher = object : Dispatcher() {
+        @Throws(InterruptedException::class)
+        override fun dispatch(request: RecordedRequest): MockResponse {
+
+            try {
+
+                if (request.path.contains("json")) {
+
+                    val fileName = request.path.substring(5)
+                    val body = IOUtils.toString(javaClass.getResourceAsStream(fileName), UTF_ENCODING)
+                    val response = MockResponse().setResponseCode(200)
+                    response.setHeader("ETag", UTF_ENCODING.hashCode())
+                    if (!request.method.equals("HEAD", ignoreCase = true))
+                        response.setBody(body)
+
+                    return response
+
+                } else if (request.path.contains("content")) {
+
+                    val fileLocation = request.path.substring(8)
+                    val videoIn = javaClass.getResourceAsStream(fileLocation)
+                    val source = Okio.buffer(Okio.source(videoIn))
+                    val buffer = Buffer()
+                    source.readAll(buffer)
+
+                    val response = MockResponse().setResponseCode(200)
+                    response.setHeader("ETag", (buffer.size().toString() + UTF_ENCODING).hashCode())
+                    if (!request.method.equals("HEAD", ignoreCase = true))
+                        response.body = buffer
+
+                    return response
+                }
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return MockResponse().setResponseCode(404)
+        }
+    }
+
+    @Before
+    fun clearDb() {
+        val db = UmAppDatabase.getInstance(null)
+        db.clearAllTables()
+    }
+
+
+    @Test
+    @Throws(IOException::class, URISyntaxException::class)
+    fun givenServerOnline_whenDdlSiteScraped_thenShouldFindConvertAndDownloadAllFiles() {
+
+        val tmpDir = Files.createTempDirectory("testindexPrathamcontentscraper").toFile()
+        val containerDir = Files.createTempDirectory("container").toFile()
+
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val scraper = spy(IndexPrathamContentScraper())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/pratham/prathamonebook.txt").url()).`when`(scraper).generatePrathamUrl("1")
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/pratham/prathamlist.txt").url()).`when`(scraper).generatePrathamUrl("2")
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/pratham/prathamempty.txt").url()).`when`(scraper).generatePrathamUrl("3")
+        doReturn(mockWebServer.url("/content/com/ustadmobile/lib/contentscrapers/pratham/24620-a-book-for-puchku.zip").url()).`when`(scraper).generatePrathamEPubFileUrl(Mockito.anyString())
+        doReturn("").`when`(scraper).loginPratham()
+
+        scraper.findContent(tmpDir, containerDir)
+
+        val resourceFolder = File(tmpDir, "24620")
+        Assert.assertEquals(true, resourceFolder.isDirectory)
+
+        val contentFile = File(resourceFolder, "24620$ETAG_TXT")
+        Assert.assertEquals(true, ContentScraperUtil.fileHasContent(contentFile))
+
+
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun givenServerOnline_whenAsbSiteScraped_thenShouldFindConvertAndDownloadAllFiles() {
+
+        val tmpDir = Files.createTempDirectory("testindexAsbcontentscraper").toFile()
+        val containerDir = Files.createTempDirectory("container").toFile()
+
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val scraper = spy(AsbScraper())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/africanbooks/abslist.txt").url()).`when`(scraper).generateURL()
+        doReturn(mockWebServer.url("/content/com/ustadmobile/lib/contentscrapers/africanbooks/asb18187.epub").url()).`when`(scraper).generateEPubUrl(Mockito.any<URL>(), Mockito.anyString())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/africanbooks/abslist.txt").url()).`when`(scraper).generatePublishUrl(Mockito.any<URL>(), Mockito.anyString())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/africanbooks/abslist.txt").url()).`when`(scraper).generateMakeUrl(Mockito.any<URL>(), Mockito.anyString())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/africanbooks/asbreader.txt").url().toString()).`when`(scraper).generateReaderUrl(Mockito.any<URL>(), Mockito.anyString())
+        doReturn(mockWebServer.url("/json/com/ustadmobile/lib/contentscrapers/africanbooks/asburl.txt").url().toString()).`when`(scraper).africanStoryBookUrl
+
+        scraper.findContent(tmpDir, containerDir)
+
+        val contentFile = File(tmpDir, "10674$LAST_MODIFIED_TXT")
+        Assert.assertEquals(true, ContentScraperUtil.fileHasContent(contentFile))
+
+    }
+
+
+    @Test
+    @Throws(IOException::class)
+    fun givenServerOnline_whenDdlEpubScraped_thenShouldConvertAndDownload() {
+
+        val tmpDir = Files.createTempDirectory("testindexDdlontentscraper").toFile()
+        val containerDir = Files.createTempDirectory("container").toFile()
+
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val scraper = DdlContentScraper(
+                mockWebServer.url("json/com/ustadmobile/lib/contentscrapers/ddl/ddlcontent.txt").toString(),
+                tmpDir, containerDir, "en")
+        scraper.scrapeContent()
+        scraper.parentSubjectAreas
+
+        val contentFolder = File(tmpDir, "ddlcontent")
+        Assert.assertEquals(true, contentFolder.isDirectory)
+
+        val contentFile = File(contentFolder, "311$ETAG_TXT")
+        Assert.assertEquals(true, ContentScraperUtil.fileHasContent(contentFile))
+
+        Assert.assertTrue("container has the file", containerDir.listFiles()!!.size > 0)
+    }
+
+}
---
+++ lib-content-scrapers/src/test/kotlin/com/ustadmobile/lib/contentscrapers/edraakK12/TestEdraakContentScraper.kt
@@ -0,0 +1,283 @@
+package com.ustadmobile.lib.contentscrapers.edraakK12
+
+import com.google.gson.GsonBuilder
+import com.ustadmobile.lib.contentscrapers.ContentScraperUtil
+import com.ustadmobile.lib.contentscrapers.ScraperConstants
+
+import org.apache.commons.io.IOUtils
+import org.junit.Assert
+import org.junit.Test
+
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.nio.file.Files
+
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import okio.Buffer
+import okio.BufferedSource
+import okio.Okio
+
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.ARABIC_FONT_BOLD
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.ARABIC_FONT_REGULAR
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.CONTENT_JSON
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.ComponentType
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.INDEX_HTML
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.JQUERY_JS
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.MATERIAL_CSS
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.MATERIAL_JS
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.QUESTIONS_JSON
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.TINCAN_FILENAME
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.UTF_ENCODING
+import com.ustadmobile.lib.contentscrapers.ScraperConstants.VIDEO_FILENAME_WEBM
+
+class TestEdraakContentScraper {
+
+
+    private val MALFORMED_COMPONENT_ID = "eada"
+
+    private val DETAIL_JSON_CONTENT_FILE = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-detail.txt"
+
+    private val MAIN_CONTENT_CONTENT_FILE = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-main-content.txt"
+
+    private val MAIN_DETAIL_WITHOUT_TARGET_FILE = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-detail-without-target.txt"
+
+    private val MAIN_DETAIL_WITHOUT_CHILDREN_FILE = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-detail-without-children.txt"
+
+    private val MAIN_DETAIL_NO_VIDEO_FOUND = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-detail-no-video-info.txt"
+
+    private val MAIN_DETAIL_NO_QUESTIONS_FOUND = "/com/ustadmobile/lib/contentscrapers/edraakK12/edraak-detail-no-question-set-children.txt"
+
+    private val VIDEO_LOCATION_FILE = "/com/ustadmobile/lib/contentscrapers/files/video.mp4"
+
+    private val RESOURCE_PATH = "/com/ustadmobile/lib/contentscrapers/files/"
+
+    internal val COMPONENT_API_PREFIX = "/api/component/"
+
+    internal val dispatcher: Dispatcher = object : Dispatcher() {
+        @Throws(InterruptedException::class)
+        override fun dispatch(request: RecordedRequest): MockResponse {
+
+
+            try {
+
+                if (request.path.startsWith(COMPONENT_API_PREFIX)) {
+
+                    val prefixLength = COMPONENT_API_PREFIX.length
+                    val fileName = request.path.substring(prefixLength,
+                            request.path.indexOf(".txt", prefixLength))
+                    return MockResponse().setBody(IOUtils.toString(javaClass.getResourceAsStream("$fileName.txt"), UTF_ENCODING))
+
+                } else if (request.path == "/media/video.mp4") {
+                    val videoIn = javaClass.getResourceAsStream(VIDEO_LOCATION_FILE)
+                    val source = Okio.buffer(Okio.source(videoIn))
+                    val buffer = Buffer()
+                    source.readAll(buffer)
+                    val response = MockResponse().setResponseCode(200)
+                    response.setHeader("ETag", (buffer.size().toString() + VIDEO_LOCATION_FILE).hashCode())
+                    if (!request.method.equals("HEAD", ignoreCase = true))
+                        response.body = buffer
+
+                    return response
+                } else if (request.path.contains("picture")) {
+                    val length = "/media/".length
+                    val fileName = request.path.substring(request.path.indexOf("/media/") + length,
+                            request.path.indexOf(".png", length))
+                    val pictureIn = javaClass.getResourceAsStream("$RESOURCE_PATH$fileName.png")
+                    val source = Okio.buffer(Okio.source(pictureIn))
+                    val buffer = Buffer()
+                    source.readAll(buffer)
+                    val response = MockResponse().setResponseCode(200)
+                    response.setHeader("ETag", (buffer.size().toString() + RESOURCE_PATH).hashCode())
+                    if (!request.method.equals("HEAD", ignoreCase = true))
+                        response.body = buffer
+
+                    return response
+                }
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return MockResponse().setResponseCode(404)
+        }
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun givenServerOnline_whenEdraakContentScraped_thenShouldConvertAndDownload() {
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), DETAIL_JSON_CONTENT_FILE, 41)
+
+        val courseDirectory = File(tmpDir, "5a60a25f0ed49f0498cb201d")
+        courseDirectory.mkdirs()
+
+        val scraper = EdraakK12ContentScraper(url, courseDirectory)
+        scraper.scrapeContent()
+
+        val jsonFile = File(courseDirectory, CONTENT_JSON)
+        Assert.assertTrue("Downloaded content info json exists", ContentScraperUtil.fileHasContent(jsonFile))
+        val jsonStr = String(Files.readAllBytes(jsonFile.toPath()), UTF_ENCODING)
+        val gsonContent = GsonBuilder().disableHtmlEscaping().create().fromJson(jsonStr, ContentResponse::class.java)
+        Assert.assertNotNull("Created Gson POJO Object", gsonContent)
+
+        Assert.assertTrue("Downloaded Questions json exist", ContentScraperUtil.fileHasContent(File(courseDirectory, QUESTIONS_JSON)))
+
+        val questionSetList = scraper.getQuestionSet(gsonContent)
+        Assert.assertNotNull("Has Questions Set", questionSetList)
+        Assert.assertTrue("Has more than 1 question", questionSetList.size > 0)
+
+        val video = File(courseDirectory, VIDEO_FILENAME_WEBM)
+        if (ComponentType.ONLINE.type.equals(gsonContent.target_component!!.component_type!!, ignoreCase = true)) {
+            Assert.assertEquals("Has Video", true, ContentScraperUtil.fileHasContent(video))
+        } else {
+            Assert.assertEquals("Should not have video", false, ContentScraperUtil.fileHasContent(video))
+        }
+
+        Assert.assertTrue("tincan file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getTINCAN_FILENAME())))
+        Assert.assertTrue("index html file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getINDEX_HTML())))
+        Assert.assertTrue("jquery file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getJQUERY_JS())))
+        Assert.assertTrue("material js file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getMATERIAL_JS())))
+        Assert.assertTrue("material css file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getMATERIAL_CSS())))
+        Assert.assertTrue("arabic font regular file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getARABIC_FONT_REGULAR())))
+        Assert.assertTrue("arabic font bold file exists", ContentScraperUtil.fileHasContent(File(courseDirectory, INSTANCE.getARABIC_FONT_BOLD())))
+
+
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    @Throws(IOException::class)
+    fun givenNotImportedContent_whenEdraakContentScraped_thenShouldThrowIllegalArgumentException() {
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), MAIN_CONTENT_CONTENT_FILE, 41)
+
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+        scraper.scrapeContent()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    @Throws(IOException::class)
+    fun givenNullTargetComponent_whenEdraakContentScraped_thenShouldThrowIllegalArgumentException() {
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), MAIN_DETAIL_WITHOUT_TARGET_FILE, 41)
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+        scraper.scrapeContent()
+
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    @Throws(IOException::class)
+    fun givenNullTargetComponentChildren_whenEdraakContentScraped_thenShouldThrowIllegalArgumentException() {
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), MAIN_DETAIL_WITHOUT_CHILDREN_FILE, 41)
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+        scraper.scrapeContent()
+
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun givenEncodedVideoListIsEmpty_whenEdraakContentScraped_thenShouldThrowIllegalArgumentException() {
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), MAIN_DETAIL_NO_VIDEO_FOUND, 41)
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+        scraper.scrapeContent()
+
+        val folder = File(tmpDir, "5a60ac073d99e104fb62ce12")
+        val video = File(folder, "video.mp4")
+        Assert.assertEquals(false, video.exists())
+
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    @Throws(IOException::class)
+    fun givenMalformedContent_whenEdraakContentScraped_thenShouldThrowIllegalArgumentException() {
+
+
+        val tmpDir = Files.createTempDirectory("testedxcontentscraper").toFile()
+
+        val mockWebServer = MockWebServer()
+
+        try {
+            mockWebServer.enqueue(MockResponse().setBody("{id"))
+
+            mockWebServer.start()
+
+            val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), MALFORMED_COMPONENT_ID, 41)
+            val scraper = EdraakK12ContentScraper(url, tmpDir)
+            scraper.scrapeContent()
+
+        } finally {
+            mockWebServer.close()
+        }
+    }
+
+
+    @Test
+    @Throws(IOException::class)
+    fun givenVideoModified_whenEdraakContentScrapedAgain_thenShouldVideoOnlyAgain() {
+
+        val tmpDir = Files.createTempDirectory("testmodifiededraak").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), DETAIL_JSON_CONTENT_FILE, 41)
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+
+        scraper.scrapeContent()
+
+        val firstDownloadTime = File(tmpDir, INSTANCE.getVIDEO_FILENAME_WEBM()).lastModified()
+        //now run scrapeContent again...
+        scraper.scrapeContent()
+
+        val lastModified = File(tmpDir, INSTANCE.getVIDEO_FILENAME_WEBM()).lastModified()
+        //Assert that last modified dates are lower than firstDownloadCompleteTime
+        Assert.assertEquals("last modified time = firstdownload time", lastModified, firstDownloadTime)
+
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun givenQuestionSetModified_whenEdraakContentScrapedAgain_thenShouldDownloadImagesOnlyAgain() {
+
+        val tmpDir = Files.createTempDirectory("testmodifiededraak").toFile()
+        val mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(dispatcher)
+
+        val url = EdraakK12ContentScraper.generateUrl(mockWebServer.url("/api/").toString(), DETAIL_JSON_CONTENT_FILE, 41)
+        val scraper = EdraakK12ContentScraper(url, tmpDir)
+
+        scraper.scrapeContent()
+        val firstDownloadTime = File(tmpDir, INSTANCE.getQUESTIONS_JSON()).lastModified()
+        //now run scrapeContent again...
+
+        scraper.scrapeContent()
+
+        val lastModified = File(tmpDir, INSTANCE.getQUESTIONS_JSON()).lastModified()
+        //Assert that last modified dates are lower than firstDownloadCompleteTime
+        Assert.assertEquals("last modified time = firstdownload time", lastModified, firstDownloadTime)
+
+    }
+}
