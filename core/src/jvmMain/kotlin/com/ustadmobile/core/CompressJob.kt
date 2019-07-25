759ccb0840a2510621914e3bcb8d24a5562637fb
Samih Mustafa <samih@ustadmobile.com>
2019-07-25 12:52:06 +0400

    added logs to compress job

--- core/src/jvmMain/kotlin/com/ustadmobile/core/CompressJob.kt
+++ core/src/jvmMain/kotlin/com/ustadmobile/core/CompressJob.kt
@@ -9,6 +9,7 @@ import sun.misc.IOUtils
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
 import java.nio.file.Files
 import java.util.zip.GZIPOutputStream
 
@@ -26,32 +27,50 @@ class CompressJob {
 
         GlobalScope.launch {
             var fileList = entryFileDao.getAllFilesForCompression()
+            println("found ${fileList.size} files")
             do {
                 fileList.forEach {
 
                     val sourceFile = File(it.cefPath!!)
                     val destFile = File(sourceFile.parentFile, "tmp")
-
                     val fileInput = FileInputStream(sourceFile)
-
                     val fileOutput = FileOutputStream(destFile)
                     val gzipOut = GZIPOutputStream(fileOutput)
+                    
+                    try {
 
-                    UMIOUtils.readFully(fileInput, gzipOut)
+                        println("sourceFile original length ${sourceFile.length()}")
 
-                    UMIOUtils.closeInputStream(fileInput)
-                    UMIOUtils.closeOutputStream(fileOutput)
-                    UMIOUtils.closeOutputStream(gzipOut)
+                        UMIOUtils.readFully(fileInput, gzipOut)
 
-                    sourceFile.delete()
-                    destFile.renameTo(sourceFile)
+                        UMIOUtils.closeInputStream(fileInput)
+                        UMIOUtils.closeOutputStream(fileOutput)
+                        UMIOUtils.closeOutputStream(gzipOut)
 
-                    it.compression = COMPRESSION_GZIP
-                    it.ceCompressedSize = sourceFile.length()
+                        val isDeleted = sourceFile.delete()
+                        if (isDeleted) {
+                            destFile.renameTo(sourceFile)
+                        } else {
+                            throw IOException("original source file was not deleted for file ${sourceFile.path}")
+                        }
 
-                    entryFileDao.updateCompressedFile(it.compression, it.ceCompressedSize, it.cefUid)
+                        it.compression = COMPRESSION_GZIP
+                        it.ceCompressedSize = sourceFile.length()
+
+                        println("sourceFile compressed length ${sourceFile.length()}")
+
+                        entryFileDao.updateCompressedFile(it.compression, it.ceCompressedSize, it.cefUid)
+                    } catch (io: IOException) {
+                        println("error for compressing file ${sourceFile.path}")
+                        io.printStackTrace()
+                    } finally {
+                        UMIOUtils.closeInputStream(fileInput)
+                        UMIOUtils.closeOutputStream(fileOutput)
+                        UMIOUtils.closeOutputStream(gzipOut)
+                    }
                 }
                 fileList = entryFileDao.getAllFilesForCompression()
+                println("found ${fileList.size} more files")
 
             } while (fileList.isNotEmpty())
 
