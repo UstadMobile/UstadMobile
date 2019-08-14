ca20be54b5c3fda780a66b4ae5f9e7f2a168b0eb
Samih Mustafa <samih@ustadmobile.com>
2019-08-14 16:37:40 +0400

    added the import link option when adding new content

--- core/src/commonMain/kotlin/com/ustadmobile/core/controller/ContentEntryListPresenter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/controller/ContentEntryListPresenter.kt
@@ -4,14 +4,16 @@ import com.ustadmobile.core.controller.ContentEntryListFragmentPresenter.Compani
 import com.ustadmobile.core.impl.UstadMobileSystemImpl
 import com.ustadmobile.core.view.ContentEditorView
 import com.ustadmobile.core.view.ContentEntryEditView
+import com.ustadmobile.core.view.ContentEntryImportLinkView
 import com.ustadmobile.core.view.ContentEntryListView
 import com.ustadmobile.core.view.ContentEntryListView.Companion.CONTENT_CREATE_CONTENT
 import com.ustadmobile.core.view.ContentEntryListView.Companion.CONTENT_CREATE_FOLDER
 import com.ustadmobile.core.view.ContentEntryListView.Companion.CONTENT_IMPORT_FILE
+import com.ustadmobile.core.view.ContentEntryListView.Companion.CONTENT_IMPORT_LINK
 import kotlinx.coroutines.Runnable
 
-class ContentEntryListPresenter (context: Any, arguments: Map<String, String>?, view: ContentEntryListView)
-    : UstadBaseController<ContentEntryListView>(context, arguments!!, view)  {
+class ContentEntryListPresenter(context: Any, arguments: Map<String, String>?, view: ContentEntryListView)
+    : UstadBaseController<ContentEntryListView>(context, arguments!!, view) {
 
     private var contentEntryUid = 0L
 
@@ -22,40 +24,43 @@ class ContentEntryListPresenter (context: Any, arguments: Map<String, String>?, 
         contentEntryUid = arguments.getValue(ARG_CONTENT_ENTRY_UID)!!.toLong()
     }
 
-    fun handleShowContentEditorOptios(show: Boolean){
-        view.runOnUiThread(Runnable{
+    fun handleShowContentEditorOptios(show: Boolean) {
+        view.runOnUiThread(Runnable {
             view.showCreateContentOption(show)
         })
     }
 
 
-    fun handleBackNavigation(){
-        view.runOnUiThread(Runnable{
+    fun handleBackNavigation() {
+        view.runOnUiThread(Runnable {
             view.navigateBack()
         })
     }
 
 
-    fun handleContentCreation(contentType: Int, newContent: Boolean){
-        val args = HashMap<String,String?>()
+    fun handleContentCreation(contentType: Int, newContent: Boolean) {
+        val args = HashMap<String, String?>()
         args.putAll(arguments)
-        args[ContentEditorView.CONTENT_ENTRY_UID] = (if(newContent) 0
+        args[ContentEditorView.CONTENT_ENTRY_UID] = (if (newContent) 0
         else contentEntryUid).toString()
         args[ContentEntryEditView.CONTENT_ENTRY_LEAF] = true.toString()
         args[ContentEntryEditView.CONTENT_TYPE] = contentType.toString()
 
-        when(contentType){
+        when (contentType) {
             CONTENT_CREATE_FOLDER -> {
                 args[ContentEntryEditView.CONTENT_ENTRY_LEAF] = false.toString()
-                view.runOnUiThread(Runnable{view.createNewFolder(args)})
+                view.runOnUiThread(Runnable { view.createNewFolder(args) })
             }
 
             CONTENT_IMPORT_FILE -> {
-                view.runOnUiThread(Runnable{view.startFileBrowser(args)})
+                view.runOnUiThread(Runnable { view.startFileBrowser(args) })
             }
 
             CONTENT_CREATE_CONTENT -> {
-                view.runOnUiThread(Runnable{view.createNewContent(args)})
+                view.runOnUiThread(Runnable { view.createNewContent(args) })
+            }
+            CONTENT_IMPORT_LINK -> {
+                impl.go(ContentEntryImportLinkView.VIEW_NAME, args, context)
             }
         }
     }
