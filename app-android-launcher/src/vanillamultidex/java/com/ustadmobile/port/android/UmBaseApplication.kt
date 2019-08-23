149a11c789a062df34edebc15bf8c7773e61463f
Mike Dawson <mike@ustadmobile.com>
2019-08-23 17:00:17 +0400

    Move to Kotlin for wrapper classes in app-android-launcher. Fix multidex import to use androidx.
    Signed-off-by: Mike Dawson <mike@ustadmobile.com>

---
+++ app-android-launcher/src/vanillamultidex/java/com/ustadmobile/port/android/UmBaseApplication.kt
@@ -0,0 +1,9 @@
+package com.ustadmobile.port.android
+
+import androidx.multidex.MultiDexApplication
+
+/**
+ * Created by mike on 2/3/18.
+ */
+
+open class UmBaseApplication : MultiDexApplication()
