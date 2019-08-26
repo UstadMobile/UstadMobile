5b3cf72bfe8bc75a7095df14f0eda42415c43e4b
Lukundo Kileha <lkileha@gmail.com>
2019-08-26 19:30:00 +0300

    Added test reports both (HML & XML)

--- app-angular/e2e/protractor.conf.js
+++ app-angular/e2e/protractor.conf.js
@@ -2,6 +2,8 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
+
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -27,5 +29,37 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+
+    jasmine.getEnv().addReporter(new JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './build/test-results/',
+        filePrefix: 'results_xml'
+    }));
+  },
+  
+  onComplete() {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+       browserName = caps.get('browserName');
+       browserVersion = caps.get('version');
+       platform = caps.get('platform');
+
+       var HTMLReport = require('protractor-html-reporter-2');
+
+       testConfig = {
+           reportTitle: 'Test Report',
+           outputPath: './build/test-results/',
+           outputFilename: 'results_html',
+           screenshotPath: './build/test-results/screenshots',
+           testBrowser: browserName,
+           browserVersion: browserVersion,
+           modifiedSuiteName: false,
+           screenshotsOnlyOnFailure: true,
+           testPlatform: platform
+       };
+       new HTMLReport().from('./build/test-results/results_xml.xml', testConfig);
+   });
+}
 };
