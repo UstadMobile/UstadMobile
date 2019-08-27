1cdf8438e74fc38539eb111d5a26b495f1b4de78
Mike Dawson <mike@ustadmobile.com>
2019-08-27 19:12:54 +0400

    Remove obsolete database annotations

--- lib-database-annotations/src/commonMain/kotlin/com/ustadmobile/lib/annotation/UmPrimaryKey.kt
+++
@@ -1,8 +0,0 @@
-package com.ustadmobile.lib.database.annotation
-
-/**
- * Created by mike on 1/21/18.
- */
-@Target(AnnotationTarget.FIELD)
-@Retention(AnnotationRetention.BINARY)
-annotation class UmPrimaryKey(val autoIncrement: Boolean = false, val autoGenerateSyncable: Boolean = false)
