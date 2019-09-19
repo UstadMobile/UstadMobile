bdde647699b6a5f435f56d45ffcea385b513e7f4
Lukundo Kileha <lkileha@gmail.com>
2019-09-19 09:07:50 +0300

    Fixed issues regarding kotlinx-serialization package

--- app-angular/e2e/protractor.conf.js
+++ app-angular/e2e/protractor.conf.js
@@ -16,6 +16,7 @@ exports.config = {
       args: ["--disable-gpu", "--window-size=1024x768"]
     }
   },
+  ignoreUncaughtExceptions: true,
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
