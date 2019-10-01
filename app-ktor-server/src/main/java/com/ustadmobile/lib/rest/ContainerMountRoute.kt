d71d180ce048764b81787a6bc7b2fc7fd4555e9d
Lukundo Kileha <lkileha@gmail.com>
2019-10-01 16:46:12 +0300

    Added range request support to container mounter

--- app-ktor-server/src/main/java/com/ustadmobile/lib/rest/ContainerMountRoute.kt
+++ app-ktor-server/src/main/java/com/ustadmobile/lib/rest/ContainerMountRoute.kt
@@ -3,14 +3,21 @@ package com.ustadmobile.lib.rest
 import com.ustadmobile.core.container.ContainerManager
 import com.ustadmobile.core.db.UmAppDatabase
 import com.ustadmobile.lib.db.entities.ContainerEntryFile
+import com.ustadmobile.lib.util.parseRangeRequestHeader
+import com.ustadmobile.port.sharedse.impl.http.RangeInputStream
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.response.*
+import io.ktor.request.header
+import io.ktor.response.header
+import io.ktor.response.respond
+import io.ktor.response.respondBytes
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
-import java.io.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.util.zip.GZIPInputStream
 import javax.naming.InitialContext
 
@@ -32,18 +39,23 @@ fun Route.ContainerMount(db: UmAppDatabase) {
             val container = db.containerDao.findByUid(containerUid)
             if(container != null){
 
+
                 val containerManager = ContainerManager(container, db, db, containerDir.absolutePath)
                 val entryWithContainerEntryFile  = containerManager.getEntry(pathInContainer)
 
                 if(entryWithContainerEntryFile!= null){
 
                     val entryFile = entryWithContainerEntryFile.containerEntryFile!!
+
+                    val rangeHeader = call.request.header("Range")?: "bytes=0-"
+                    val rangeResponse = parseRangeRequestHeader(rangeHeader, entryFile.ceTotalSize)
                     call.response.header("X-Content-Length-Uncompressed", entryFile.ceTotalSize.toString())
-                    var inputStream: InputStream = FileInputStream(File(entryFile.cefPath!!))
+                    var inputStream: InputStream = RangeInputStream(FileInputStream(File(entryFile.cefPath!!)), rangeResponse.fromByte, rangeResponse.toByte)
+
                     if(entryFile.compression == ContainerEntryFile.COMPRESSION_GZIP){
                         inputStream = GZIPInputStream(inputStream)
                     }
-                    call.respondBytes(inputStream.readBytes(), getContentType(pathInContainer), HttpStatusCode.OK)
+                    call.respondBytes(inputStream.readBytes(), getContentType(pathInContainer), HttpStatusCode(rangeResponse.statusCode,""))
 
                 }else{
                     call.respond(HttpStatusCode.NotFound, "No such file in specified container${containerManager.allEntries.size}")
