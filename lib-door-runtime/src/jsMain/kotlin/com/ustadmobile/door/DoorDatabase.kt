83ae67d577b8b49e557b5cebbfea4204da0d8922
Mike Dawson <mike@ustadmobile.com>
2019-10-22 14:55:22 +0400

    Receive syncable entities using runInTransaction
    A syncable entity can have multiple types (e.g. multiple tables) embedded
    within it. Inserting all entities within a runInTransaction call will
    avoid triggering LiveData update calls for each entity type.
    
    Signed-off-by: Mike Dawson <mike@ustadmobile.com>

--- lib-door-runtime/src/jsMain/kotlin/com/ustadmobile/door/DoorDatabase.kt
+++ lib-door-runtime/src/jsMain/kotlin/com/ustadmobile/door/DoorDatabase.kt
@@ -1,7 +1,13 @@
 package com.ustadmobile.door
 
+import kotlinx.coroutines.Runnable
+
 actual abstract class DoorDatabase {
 
     actual abstract fun clearAllTables()
+
+    actual open fun runInTransaction(runnable: Runnable) {
+        runnable.run()
+    }
 
 }
