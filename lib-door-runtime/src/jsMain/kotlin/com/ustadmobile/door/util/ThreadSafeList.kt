758836e174c863fbc8f5d214dea5c6944b25bbaf
Mike Dawson <mike@ustadmobile.com>
2019-12-23 14:21:23 +0400

    Update implementation of DoorLiveData for Javascript
    Signed-off-by: Mike Dawson <mike@ustadmobile.com>

--- lib-door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/ThreadSafeList.kt
+++ lib-door-runtime/src/jsMain/kotlin/com/ustadmobile/door/util/ThreadSafeList.kt
@@ -1,3 +1,3 @@
 package com.ustadmobile.door.util
 
-actual fun <T> threadSafeListOf(vararg items: T) = mutableListOf(items)
+actual fun <T> threadSafeListOf(vararg items: T) = mutableListOf(*items)
