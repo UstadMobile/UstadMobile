d45402cc4dcd5073b0faf9315d371c3e9d6ad636
Samih Mustafa <samih@ustadmobile.com>
2019-12-30 10:47:49 +0400

    actual test signatures

---
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/controller/ContentEntryListPresenterTest.kt
@@ -0,0 +1,69 @@
+package com.ustadmobile.core.controller
+
+import org.junit.Test
+
+class ContentEntryListPresenterTest {
+
+    @Test
+    fun givenArgToViewDownloadedContent_whenOnCreateCalled_thenShouldSetListOfDownloadedContent(){
+
+
+    }
+
+    @Test
+    fun givenArgToViewRecycledContent_whenOnCreateCalled_thenShouldSetListOfRecycledContent(){
+
+
+    }
+
+    @Test
+    fun givenArgToViewLibraryContent_whenOnCreateCalled_thenShouldSetListOfLibraryContentAndShowFiltersOfLanguagesAndCategory(){
+
+
+    }
+
+    @Test
+    fun givenListOfLanguages_whenUserSelectsEnglish_thenFilterAndShowListOfEnglishContent(){
+
+
+    }
+
+    @Test
+    fun givenListOfCategory_whenUserSelectsACategory_thenFilterAndShowListOfCategoryContent(){
+
+
+    }
+
+    @Test
+    fun givenListOfContent_whenUserClicksOnDownloadIcon_thenShouldOpenDownloadDialog(){
+
+
+    }
+
+    @Test
+    fun givenListOfContent_whenUserClicksOnLeafContent_thenShouldOpenContentEntryDetail(){
+
+
+    }
+
+    @Test
+    fun givenListOfContent_whenUserClicksOnEntryNotLeaf_thenShouldOpenContentEntryListActivity(){
+
+
+    }
+
+    @Test
+    fun givenUserIsAdmin_whenClicksOnAddContentIcon_thenShouldOpenContentEntryEditActivity(){
+
+
+    }
+
+    @Test
+    fun givenUserIsAdmin_whenClicksOnEditContent_thenShouldOpenContentEntryEditActivity(){
+
+
+    }
+
+
+
+}
