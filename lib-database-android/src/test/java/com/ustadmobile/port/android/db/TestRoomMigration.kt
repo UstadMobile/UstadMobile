566642ecb78ee3ed12c000799cabe680de9fa1ae
Mike Dawson <mike@ustadmobile.com>
2019-12-30 15:34:22 +0400

    Change Javascript database test to use headless chrome by default
    Signed-off-by: Mike Dawson <mike@ustadmobile.com>

---
+++ lib-database-android/src/test/java/com/ustadmobile/port/android/db/TestRoomMigration.kt
@@ -0,0 +1,16 @@
+package com.ustadmobile.port.android.db
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class TestRoomMigration {
+
+    @Test
+    fun initialTest() {
+        Assert.assertEquals(4, 2+2)
+    }
+
+}
