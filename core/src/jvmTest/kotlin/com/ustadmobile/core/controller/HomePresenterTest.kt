097178309ac44dbbdb7f9be2f9a7635ce5bc92a8
Lukundo Kileha <lkileha@gmail.com>
2020-01-13 14:22:56 +0300

    Added homepresenter test method signature

---
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/controller/HomePresenterTest.kt
@@ -0,0 +1,23 @@
+package com.ustadmobile.core.controller
+
+import org.junit.Before
+import org.junit.Test
+
+class HomePresenterTest {
+
+
+    @Before
+    fun setup(){
+
+    }
+
+    @Test
+    fun givenApplicationLaunched_WhenNoAdminLoggedIn_shouldShowOnlyContentsNavWithLibrariesAndDownloadedFilters(){
+
+    }
+
+    @Test
+    fun givenApplicationLaunched_WhenAdminLoggedIn_shouldShowBothContentsAndReportsNavsWithAllFilters(){
+
+    }
+}
