91506c5b7011a3e6b60418b8d5a2bf0a7ea6d81d
Varuna Singh <varuna.singh@gmail.com>
2020-03-09 19:52:00 +0400

    Added progress to assignment detail

--- app-ktor-server/src/main/java/com/ustadmobile/lib/rest/UmRestApplication.kt
+++ app-ktor-server/src/main/java/com/ustadmobile/lib/rest/UmRestApplication.kt
@@ -1,21 +1,23 @@
 package com.ustadmobile.lib.rest
 
+import ch.qos.logback.core.util.ContentTypeUtil
 import com.google.gson.Gson
 import com.ustadmobile.core.container.ContainerManager
 import com.ustadmobile.core.container.addEntriesFromZipToContainer
+import com.ustadmobile.core.controller.HomePresenter
 import com.ustadmobile.core.db.UmAppDatabase
 import com.ustadmobile.core.db.UmAppDatabase_KtorRoute
 import com.ustadmobile.core.db.dao.PersonAuthDao
-import com.ustadmobile.lib.db.entities.Container
-import com.ustadmobile.lib.db.entities.Person
-import com.ustadmobile.lib.db.entities.PersonAuth
+import com.ustadmobile.lib.db.entities.*
 import com.ustadmobile.lib.util.encryptPassword
+import com.ustadmobile.port.sharedse.contentformats.importContentEntryFromFile
 import com.ustadmobile.port.sharedse.util.UmFileUtilSe
 import com.ustadmobile.staging.lib.rest.LoadInitialData
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.AutoHeadResponse
 import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
@@ -27,6 +29,7 @@ import io.ktor.http.HttpMethod
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import kotlinx.coroutines.runBlocking
 import org.apache.commons.lang3.RandomStringUtils
 import java.io.File
 import java.nio.file.Files
@@ -40,6 +43,9 @@ private val _restApplicationDb = UmAppDatabase.getInstance(Any(), "UmAppDatabase
 fun Application.umRestApplication(devMode: Boolean = false, db : UmAppDatabase = _restApplicationDb) {
 
     val adminuser = db.personDao.findByUsername("admin")
+    val iContext = InitialContext()
+    val containerDirPath = iContext.lookup("java:/comp/env/ustadmobile/app-ktor-server/containerDirPath") as String
+
     if(adminuser == null) {
         val adminPerson = Person("admin", "Admin", "User")
         adminPerson.admin = true
@@ -50,8 +56,8 @@ fun Application.umRestApplication(devMode: Boolean = false, db : UmAppDatabase =
                 PersonAuthDao.ENCRYPTED_PASS_PREFIX + encryptPassword(adminPass)))
 
 
-        val iContext = InitialContext()
-        val containerDirPath = iContext.lookup("java:/comp/env/ustadmobile/app-ktor-server/containerDirPath") as String
+
+
         val adminPassFile = File(containerDirPath, "admin.txt")
         if(!adminPassFile.parentFile.isDirectory) {
             adminPassFile.parentFile.mkdirs()
@@ -79,6 +85,10 @@ fun Application.umRestApplication(devMode: Boolean = false, db : UmAppDatabase =
             register(ContentType.Application.Json, GsonConverter())
             register(ContentType.Any, GsonConverter())
         }
+    }
+
+    runBlocking {
+        initLamsustadContent(db, containerDirPath)
     }
 
     install(Routing) {
@@ -125,10 +135,47 @@ fun Application.umRestApplication(devMode: Boolean = false, db : UmAppDatabase =
             }
         }
     }
+}
 
-    //IRC specific:
-    val loadData = LoadInitialData(_restApplicationDb)
-    loadData.loadData()
+suspend fun Application.initLamsustadContent(db: UmAppDatabase, containerPath: String) {
+    val folderSrcUrl = "http://www.ustadmobile.com/lamsustad/stories"
+    val storiesEntry = db.contentEntryDao.findBySourceUrl(folderSrcUrl)
+    if(storiesEntry == null) {
+        val storiesContentEntry = ContentEntry("قصص", "", false, false).apply {
+            sourceUrl = folderSrcUrl
+            publik = true
+            thumbnailUrl = "https://www.ustadmobile.com/files/lamsustad/stories.webp"
+        }
+
+        storiesContentEntry.contentEntryUid = db.contentEntryDao.insert(storiesContentEntry)
+        db.contentEntryParentChildJoinDao.insert(ContentEntryParentChildJoin().apply {
+            cepcjChildContentEntryUid = storiesContentEntry.contentEntryUid
+            cepcjParentContentEntryUid = HomePresenter.MASTER_SERVER_ROOT_ENTRY_UID
+            childIndex = 0
+        })
+
+        for(i in 1..5) {
+            val file = File("build/lamsustad/Book${i}_xapi.zip")
+            if(!file.exists()) {
+                println("Lamsustad file $file does not exist")
+                continue
+            }
+            val importedEntry = importContentEntryFromFile(file, db, db, containerPath)
+            if(importedEntry != null) {
+                importedEntry.first.thumbnailUrl = "https://www.ustadmobile.com/files/lamsustad/story${i}.webp"
+                db.contentEntryDao.update(importedEntry.first)
+                db.contentEntryParentChildJoinDao.insert(
+                        ContentEntryParentChildJoin(storiesContentEntry, importedEntry.first, i))
+                println("Imported and joined \"${importedEntry.first.title}\"")
+            }else {
+                println("Error importing content")
+            }
+        }
+
+        val loadData = LoadInitialData(_restApplicationDb)
+        loadData.loadData()
+
+    }
 }
 
 private suspend fun handleInvalidRequest(call: ApplicationCall){
@@ -160,5 +207,4 @@ private fun prepareResources(resourceName: String?, path: String, entryId: Strin
     UmFileUtilSe.extractResourceToFile("/com/ustadmobile/core/contentformats/$path/${resourceName}", tempFile)
     val tempDir = UmFileUtilSe.makeTempDir("testFile", "containerDirTmp")
     return arrayOf(tempDir, tempFile, epubContainer)
-
 }
