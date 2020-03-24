dc23e8242f41baf5097b6e5517cb52594c31d189
Samih Mustafa <samih@ustadmobile.com>
2020-03-24 18:58:46 +0400

    changed master root uid by mistake

--- core/src/commonMain/kotlin/com/ustadmobile/core/controller/HomePresenter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/controller/HomePresenter.kt
@@ -137,7 +137,7 @@ class HomePresenter(context: Any, arguments: Map<String, String?>,  view: HomeVi
 
     companion object {
         @JsName("MASTER_SERVER_ROOT_ENTRY_UID")
-        const val MASTER_SERVER_ROOT_ENTRY_UID = 842253629
+        const val MASTER_SERVER_ROOT_ENTRY_UID = -4103245208651563007L
 
         /**
          * This view name will generate a tabbed set of ContentEntryList views. See
