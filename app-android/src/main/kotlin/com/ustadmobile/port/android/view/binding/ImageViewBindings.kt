161b62eda2402847b06529b331270bd21d243638
Mike Dawson <mike@ustadmobile.com>
2020-04-25 23:52:59 +0400

    Two-way URI databinding for ImageView

--- app-android/src/main/kotlin/com/ustadmobile/port/android/view/binding/ImageViewBindings.kt
+++ app-android/src/main/kotlin/com/ustadmobile/port/android/view/binding/ImageViewBindings.kt
@@ -8,6 +8,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import androidx.core.net.toFile
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
@@ -20,79 +21,42 @@ import androidx.navigation.findNavController
 import com.toughra.ustadmobile.R
 import com.ustadmobile.port.android.util.ext.getActivityContext
 
-//TODO: Make this show a dialog to select between removing an image, selecting an image from gallery, and taking a photo
-//Left here for reference purposes for creating the dialog
-//            val items = listOf(R.string.remove_photo, R.string.take_new_photo_from_camera,
-//                    R.string.select_new_photo_from_gallery).map { v.context.getString(it) }.toTypedArray()
-//            MaterialAlertDialogBuilder(itemView.context)
-//                    .setTitle(R.string.change_photo)
-//                    .setItems(items) {dialog, which ->
-//                        //handle this
-//                        val presenterFieldRowVal = presenterFieldRow ?: return@setItems
-//                        if(which == 1) {
-//                            //imageHelper?.onClickTakePicture(presenterFieldRowVal)
-//                        }
-//                    }
-//                    .show()
-class ImageViewLifecycleObserver(var imageView: ImageView?,
-                                 private val registry : ActivityResultRegistry,
-                                 private val inverseBindingListener: InverseBindingListener) : DefaultLifecycleObserver {
+class ImageViewLifecycleObserver2(view: ImageView, registry: ActivityResultRegistry, inverseBindingListener: InverseBindingListener)
+    : ViewActivityLauncherLifecycleObserver<ImageView>(view, registry, inverseBindingListener) {
 
-    lateinit var takePhoto: ActivityResultLauncher<Uri>
-
-    private var imageFilePath: String? = null
-
-    override fun onCreate(owner: LifecycleOwner) {
-        super.onCreate(owner)
-
-        takePhoto = registry.register("image", owner, ActivityResultContracts.TakePicture(), ActivityResultCallback {
-            imageView?.setImageFilePath(imageFilePath)
-            inverseBindingListener.onChange()
-        })
-    }
-
-    fun takePicture() {
-        val context = imageView?.context ?: return
-        val fileDest = File(context.cacheDir, "image.jpg")
-        imageFilePath = fileDest.absolutePath
-        val fileUri = FileProvider.getUriForFile(context.applicationContext,
-                "${context.packageName}.provider", fileDest)
-        takePhoto.launch(fileUri)
-    }
-
-    override fun onDestroy(owner: LifecycleOwner) {
-        super.onDestroy(owner)
-        imageView?.setOnClickListener(null)
-        imageView = null
-
+    override fun onPictureTakenOrSelected(pictureUri: Uri?) {
+        view.setImageFilePath(pictureUri?.toString())
+        inverseBindingListener?.onChange()
     }
 }
 
-@BindingAdapter("imageFilePath")
+@BindingAdapter("imageUri")
 fun ImageView.setImageFilePath(imageFilePath: String?) {
     //start observing
-    if(imageFilePath == null)
+    if(imageFilePath == null){
+        setImageResource(android.R.color.transparent)
         return
+    }
 
     setTag(R.id.tag_imagefilepath, imageFilePath)
     Picasso.get()
-            .load(Uri.fromFile(File(imageFilePath)))
+            .load(Uri.parse(imageFilePath))
             .noFade()
             .into(this)
 }
-@BindingAdapter("imageFilePathAttrChanged")
+@BindingAdapter("imageUriAttrChanged")
 fun ImageView.getImageFilePath(inverseBindingListener: InverseBindingListener) {
     val activity = context.getActivityContext() as ComponentActivity
-    val imageViewLifecycleObserver = ImageViewLifecycleObserver(this,
+    val imageViewLifecycleObserver = ImageViewLifecycleObserver2(this,
             activity.activityResultRegistry, inverseBindingListener)
     findViewTreeLifecycleOwner()?.lifecycle?.addObserver(imageViewLifecycleObserver)
 
     setOnClickListener {
-        imageViewLifecycleObserver.takePicture()
+        imageViewLifecycleObserver.showOptionsDialog()
     }
 }
 
-@InverseBindingAdapter(attribute = "imageFilePath")
+@InverseBindingAdapter(attribute = "imageUri")
 fun ImageView.getRealImageFilePath(): String {
     return (getTag(R.id.tag_imagefilepath) as? String) ?: ""
 }
---
+++ app-android/src/main/kotlin/com/ustadmobile/port/android/view/binding/ViewActivityLauncherLifecycleObserver.kt
@@ -0,0 +1,157 @@
+    package com.ustadmobile.port.android.view.binding
+
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
+import android.view.View
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.databinding.InverseBindingListener
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.toughra.ustadmobile.R
+import java.io.File
+
+    /**
+ *
+ */
+abstract class ViewActivityLauncherLifecycleObserver<V: View>(
+        var view: V,
+        private var registry: ActivityResultRegistry?,
+        protected var inverseBindingListener: InverseBindingListener?
+) : DefaultLifecycleObserver, DialogInterface.OnClickListener {
+
+    private var cameraLauncher: ActivityResultLauncher<Uri>? = null
+
+    private var galleryLauncher: ActivityResultLauncher<String>? = null
+
+    private var requestCameraPermission: ActivityResultLauncher<String>? = null
+
+    private var requestStoragePermission: ActivityResultLauncher<String>? = null
+
+    private val cameraUriSavedStateKey: String
+        get() = PREFIX_URI_KEY + view.id
+
+    val takePictureOnClickListener = View.OnClickListener {
+        takePicture()
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+
+        cameraLauncher = registry?.register("cameraFilePath_${view.id}", owner,
+            ActivityResultContracts.TakePicture(), ActivityResultCallback {
+                val uriStr = view.findNavController().currentBackStackEntry
+                        ?.savedStateHandle?.get<String>(cameraUriSavedStateKey) ?: return@ActivityResultCallback
+                onPictureTakenOrSelected(Uri.parse(uriStr))
+        })
+
+        galleryLauncher = registry?.register("galleryFilePath_${view.id}", owner,
+            ActivityResultContracts.GetContent(), ActivityResultCallback {
+            onPictureTakenOrSelected(it)
+        })
+
+        requestCameraPermission = registry?.register("cameraPermission_${view.id}", owner,
+            ActivityResultContracts.RequestPermission(), ActivityResultCallback { granted ->
+            if(granted)
+                takePictureInternal()
+        })
+
+        requestStoragePermission = registry?.register("storagePermission_${view.id}", owner,
+            ActivityResultContracts.RequestPermission(), ActivityResultCallback {granted ->
+            if(granted) {
+                openPictureInternal()
+            }
+        })
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+
+        cameraLauncher = null
+        galleryLauncher = null
+        requestCameraPermission = null
+        requestStoragePermission = null
+        inverseBindingListener = null
+        view.setOnClickListener(null)
+    }
+
+    fun showOptionsDialog() {
+        MaterialAlertDialogBuilder(view.context)
+            .setTitle(R.string.change_photo)
+            .setItems(OPTIONS_STRING_IDS.map { view.context.getString(it) }.toTypedArray(), this)
+            .show()
+
+    }
+
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        when(OPTIONS_STRING_IDS[which]) {
+            R.string.take_new_photo_from_camera -> takePicture()
+            R.string.select_new_photo_from_gallery -> openPicture()
+        }
+    }
+
+    private fun hasPermission(permission: String): Boolean
+            = ContextCompat.checkSelfPermission(view.context, permission) == PackageManager.PERMISSION_GRANTED
+
+    fun takePicture() {
+        if(hasPermission(Manifest.permission.CAMERA)) {
+            takePictureInternal()
+        }else {
+            requestCameraPermission?.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    protected fun takePictureInternal() {
+        val fileDest = File(view.context.cacheDir, "image.jpg")
+        val fileUri = FileProvider.getUriForFile(view.context.applicationContext,
+                "${view.context.packageName}.provider", fileDest)
+
+        //This is required to grant permission to the third party activity (e.g. camera) to save
+        //the image. Only seems to be required on Android 4.4
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val resInfoList = view.context.packageManager.queryIntentActivities(cameraIntent,
+                PackageManager.MATCH_DEFAULT_ONLY)
+        for (resolveInfo in resInfoList) {
+            val packageName = resolveInfo.activityInfo.packageName
+            view.context.grantUriPermission(packageName, fileUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        view.findNavController().currentBackStackEntry?.savedStateHandle?.set(
+                cameraUriSavedStateKey, fileUri.toString())
+        cameraLauncher?.launch(fileUri)
+    }
+
+    fun openPictureInternal() {
+        galleryLauncher?.launch("image/*")
+    }
+
+    fun openPicture() {
+        if(hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            openPictureInternal()
+        }else {
+            requestStoragePermission?.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    abstract fun onPictureTakenOrSelected(pictureUri: Uri?)
+
+    companion object {
+
+        val PREFIX_URI_KEY = "galleryFileUri_"
+
+        val OPTIONS_STRING_IDS = arrayOf(R.string.remove_photo, R.string.take_new_photo_from_camera,
+                    R.string.select_new_photo_from_gallery)
+    }
+
+}
