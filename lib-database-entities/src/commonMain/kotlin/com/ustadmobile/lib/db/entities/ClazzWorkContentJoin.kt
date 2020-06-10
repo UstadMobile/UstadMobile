82080f1d03d000c9d3be8648f1c06d868970a846
Varuna Singh <varuna.singh@gmail.com>
2020-06-11 03:06:54 +0400

    Refactor

--- lib-database-entities/src/commonMain/kotlin/com/ustadmobile/lib/db/entities/ClazzWorkContentJoin.kt
+++ lib-database-entities/src/commonMain/kotlin/com/ustadmobile/lib/db/entities/ClazzWorkContentJoin.kt
@@ -6,7 +6,6 @@ import com.ustadmobile.door.annotation.LastChangedBy
 import com.ustadmobile.door.annotation.LocalChangeSeqNum
 import com.ustadmobile.door.annotation.MasterChangeSeqNum
 import com.ustadmobile.door.annotation.SyncableEntity
-import com.ustadmobile.lib.db.entities.ClazzAssignmentContentJoin.Companion.TABLE_ID
 import kotlinx.serialization.Serializable
 
 @Entity
