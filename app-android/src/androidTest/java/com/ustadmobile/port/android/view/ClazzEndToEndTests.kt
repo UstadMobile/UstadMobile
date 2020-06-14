245764fbff7b3efb7b09b51f781930520d85b886
Mike Dawson <mike@ustadmobile.com>
2020-06-14 20:05:00 +0400

    Update DataBindingIdlingResource to support monitoring an ActivityScenario

--- app-android/src/androidTest/java/com/ustadmobile/port/android/view/ClazzEndToEndTests.kt
+++ app-android/src/androidTest/java/com/ustadmobile/port/android/view/ClazzEndToEndTests.kt
@@ -1,16 +1,13 @@
 package com.ustadmobile.port.android.view
 
 import android.widget.DatePicker
-import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -18,11 +15,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.toughra.ustadmobile.R
 import com.ustadmobile.adbscreenrecorder.client.AdbScreenRecord
 import com.ustadmobile.adbscreenrecorder.client.AdbScreenRecordRule
-import com.ustadmobile.core.db.UmAppDatabase
-import com.ustadmobile.core.impl.UmAccountManager
 import com.ustadmobile.lib.db.entities.HolidayCalendar
-import com.ustadmobile.lib.db.entities.UmAccount
-import org.hamcrest.Matchers.*
+import com.ustadmobile.test.rules.DataBindingIdlingResourceRule
+import com.ustadmobile.test.rules.UmAppDatabaseAndroidClientRule
+import com.ustadmobile.test.rules.withDataBindingIdlingResource
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,26 +29,27 @@ import org.junit.runner.RunWith
 @AdbScreenRecord("Class end-to-end test")
 class ClazzEndToEndTests {
 
-    private lateinit var db: UmAppDatabase
+    @JvmField
+    @Rule
+    var dbRule = UmAppDatabaseAndroidClientRule(useDbAsRepo = true)
 
     @JvmField
     @Rule
     val screenRecordRule = AdbScreenRecordRule()
 
+    @JvmField
+    @Rule
+    val dataBindingIdlingResourceRule = DataBindingIdlingResourceRule()
+
     @AdbScreenRecord("Given an empty class list, when the user clicks add class and fills in form, then the new class is shown in list")
     @Test
     fun givenEmptyClazzList_whenUserClicksAddAndFillsInForm_thenClassIsCreatedAndShownInList() {
-        val activeAccount = UmAccount(7L, "bond", "", "http://localhost")
-        UmAccountManager.setActiveAccount(activeAccount, ApplicationProvider.getApplicationContext())
-
-        db = UmAccountManager.getActiveDatabase(ApplicationProvider.getApplicationContext())
-        db.clearAllTables()
-
-        val calendarUid = db.holidayCalendarDao.insert(HolidayCalendar().apply {
+        val calendarUid = dbRule.db.holidayCalendarDao.insert(HolidayCalendar().apply {
             this.umCalendarName = "Test Calendar"
         })
 
         val activityScenario = launchActivity<MainActivity>()
+                .withDataBindingIdlingResource(dataBindingIdlingResourceRule)
 
         onView(withId(R.id.home_clazzlist_dest)).perform(click())
         onView(withText(R.string.clazz)).perform(click())
@@ -73,7 +72,7 @@ class ClazzEndToEndTests {
         onView(withId(R.id.menu_done)).perform(click())
 
 
-        val createdClazz = db.clazzDao.findByClazzName("Test Class").first()
+        val createdClazz = dbRule.db.clazzDao.findByClazzName("Test Class").first()
         onView(allOf(withId(R.id.item_clazzlist_clazz_cl), withTagValue(equalTo(createdClazz.clazzUid))))
                 .check(matches(isDisplayed()))
     }
--- app-android/src/androidTest/java/com/ustadmobile/test/core/impl/DataBindingIdlingResource.kt
+++ app-android/src/androidTest/java/com/ustadmobile/test/core/impl/DataBindingIdlingResource.kt
@@ -1,12 +1,16 @@
 package com.ustadmobile.test.core.impl
 
+import android.app.Activity
+import android.os.Handler
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.testing.FragmentScenario
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.IdlingResource
 import java.util.UUID
 
@@ -18,6 +22,9 @@ import java.util.UUID
  * calling [monitorFragment] with a [FragmentScenario], thereby monitoring all bindings in that
  * fragment and any child views.
  */
+//Originally from:
+// https://github.com/android/architecture-components-samples/blob/master/GithubBrowserSample/app/src/androidTest/java/com/android/example/github/util/DataBindingIdlingResource.kt
+
 //See also (for activity) https://github.com/googlecodelabs/android-testing/blob/codelab2019/app/src/sharedTest/java/com/example/android/architecture/blueprints/todoapp/util/DataBindingIdlingResource.kt
 
 class DataBindingIdlingResource : IdlingResource {
@@ -30,7 +37,9 @@ class DataBindingIdlingResource : IdlingResource {
     // onTransitionToIdle callbacks if Espresso never thought we were idle in the first place.
     private var wasNotIdle = false
 
-    private lateinit var scenario: FragmentScenario<out Fragment>
+    private var fragmentScenario: FragmentScenario<out Fragment>? = null
+
+    private var activityScenario: ActivityScenario<out Activity>? = null
 
     override fun getName() = "DataBinding $id"
 
@@ -38,7 +47,11 @@ class DataBindingIdlingResource : IdlingResource {
      * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
      */
     fun monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
-        scenario = fragmentScenario
+        this.fragmentScenario = fragmentScenario
+    }
+
+    fun monitorActivity(activityScenario: ActivityScenario<out Activity>) {
+        this.activityScenario = activityScenario
     }
 
     override fun isIdleNow(): Boolean {
@@ -52,13 +65,28 @@ class DataBindingIdlingResource : IdlingResource {
             wasNotIdle = false
         } else {
             wasNotIdle = true
-            // check next frame
-            scenario.onFragment { fragment ->
-                fragment.view?.postDelayed({
-                    if (fragment.view != null) {
+            val fragmentScenarioVal = fragmentScenario
+            val activityScenarioVal = activityScenario
+
+            if(fragmentScenarioVal != null) {
+                fragmentScenarioVal.onFragment { fragment ->
+                    fragment.view?.postDelayed({
+                        if (fragment.view != null) {
+                            isIdleNow
+                        }
+                    }, 16)
+                }
+            }else if(activityScenarioVal != null) {
+                activityScenarioVal.onActivity {
+                    Handler().postDelayed({
                         isIdleNow
-                    }
-                }, 16)
+                    }, 16)
+                }
+            }else {
+                throw IllegalStateException("DataBindingIdlingResource is not connected to any " +
+                        "fragment or activity scenario! If you are using a test @Rule, please " +
+                        "make sure to call monitorFragment or monitorActivity **BEFORE** any Espresso" +
+                        "onView or onIdle call")
             }
         }
         return idle
@@ -72,13 +100,26 @@ class DataBindingIdlingResource : IdlingResource {
      * Find all binding classes in all currently available fragments.
      */
     private fun getBindings(): List<ViewDataBinding> {
-        lateinit var bindings: List<ViewDataBinding>
-        scenario.onFragment {  fragment ->
-            bindings = fragment.requireView().flattenHierarchy().mapNotNull { view ->
-                DataBindingUtil.getBinding<ViewDataBinding>(view)
-            }
+        val bindings = mutableListOf<ViewDataBinding>()
+        fragmentScenario?.onFragment { fragment ->
+            bindings += fragment.viewBindings()
         }
+
+        activityScenario?.onActivity {
+            val fragments = (it as? FragmentActivity)?.supportFragmentManager
+                    ?.fragments ?: listOf()
+            bindings += fragments.flatMap { it.viewBindings() }
+        }
+
         return bindings
+    }
+
+    private fun Fragment.viewBindings(): List<ViewDataBinding> {
+        val bindings = view?.flattenHierarchy()?.mapNotNull { view ->
+            DataBindingUtil.getBinding<ViewDataBinding>(view)
+        } ?: listOf()
+        val childBindings = childFragmentManager.fragments.flatMap { it.viewBindings() }
+        return bindings + childBindings
     }
 
     private fun View.flattenHierarchy(): List<View> = if (this is ViewGroup) {
