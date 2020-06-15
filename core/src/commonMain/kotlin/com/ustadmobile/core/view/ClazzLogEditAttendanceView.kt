b9d747113ef0366ec1e39050a5370427cc390e1f
Mike Dawson <mike@ustadmobile.com>
2020-06-16 01:13:01 +0400

    Add presenter functionality to allow the user to change days within the attendance record screen

--- core/src/commonMain/kotlin/com/ustadmobile/core/view/ClazzLogEditAttendanceView.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/view/ClazzLogEditAttendanceView.kt
@@ -1,5 +1,6 @@
 package com.ustadmobile.core.view
 
+import androidx.paging.DataSource
 import com.ustadmobile.door.DoorMutableLiveData
 import com.ustadmobile.lib.db.entities.ClazzLog
 import com.ustadmobile.lib.db.entities.ClazzLogAttendanceRecord
@@ -11,6 +12,8 @@ interface ClazzLogEditAttendanceView: UstadEditView<ClazzLog> {
     var clazzLogAttendanceRecordList: DoorMutableLiveData<List<ClazzLogAttendanceRecordWithPerson>>?
 
     var clazzLogTimezone: String?
+
+    var clazzLogsList: DataSource.Factory<Int, ClazzLog>?
 
     companion object {
 
