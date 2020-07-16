63b413c38cb6d5e52942d7eddfcd0bf552803cf9
Lukundo Kileha <lkileha@gmail.com>
2020-07-16 18:53:57 +0300

    Added person account presenter tests

--- core/src/jvmTest/kotlin/com/ustadmobile/core/controller/PersonAccountEditPresenterTest.kt
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/controller/PersonAccountEditPresenterTest.kt
@@ -3,14 +3,13 @@ package com.ustadmobile.core.controller
 import com.nhaarman.mockitokotlin2.*
 import com.ustadmobile.core.account.UstadAccountManager
 import com.ustadmobile.core.db.UmAppDatabase
-import com.ustadmobile.core.db.dao.PersonDao
 import com.ustadmobile.core.util.UstadTestRule
 import com.ustadmobile.core.util.directActiveRepoInstance
-import com.ustadmobile.core.view.PersonEditView
+import com.ustadmobile.core.view.PersonAccountEditView
 import com.ustadmobile.core.view.UstadView
-import com.ustadmobile.core.view.UstadView.Companion.ARG_SERVER_URL
 import com.ustadmobile.door.DoorLifecycleOwner
-import com.ustadmobile.lib.db.entities.*
+import com.ustadmobile.lib.db.entities.Person
+import com.ustadmobile.lib.db.entities.UmAccount
 import junit.framework.Assert.assertEquals
 import kotlinx.serialization.json.Json
 import okhttp3.mockwebserver.MockResponse
@@ -25,31 +24,31 @@ import org.kodein.di.bind
 import org.kodein.di.singleton
 
 
-class PersonEditPresenterTest  {
+class PersonAccountEditPresenterTest  {
 
     @JvmField
     @Rule
     var ustadTestRule = UstadTestRule()
 
-    private lateinit var mockView: PersonEditView
+    private lateinit var mockView: PersonAccountEditView
 
     private lateinit var context: Any
 
     private lateinit var mockLifecycleOwner: DoorLifecycleOwner
 
-    private val timeoutInMill: Long = 5000
-
-    private lateinit var accountManager: UstadAccountManager
-
     private lateinit var di: DI
 
     private lateinit var mockWebServer: MockWebServer
 
-    private lateinit var mockDao:PersonDao
-
     private lateinit var repo: UmAppDatabase
 
     private lateinit var serverUrl: String
+
+    private val defaultTimeOut: Long = 2000
+
+    private val mPersonUid: Long = 234567
+
+    private lateinit var accountManager: UstadAccountManager
 
 
     @Before
@@ -66,7 +65,7 @@ class PersonEditPresenterTest  {
         serverUrl = mockWebServer.url("/").toString()
 
         accountManager = mock{
-            on{activeAccount}.thenReturn(UmAccount(0L,"","",serverUrl))
+            on{activeAccount}.thenReturn(UmAccount(mPersonUid,"","",serverUrl))
         }
 
         di = DI {
@@ -75,8 +74,6 @@ class PersonEditPresenterTest  {
         }
 
         repo = di.directActiveRepoInstance()
-        mockDao = spy(repo.personDao)
-        whenever(repo.personDao).thenReturn(mockDao)
     }
 
     @After
@@ -85,120 +82,80 @@ class PersonEditPresenterTest  {
     }
 
 
-    private fun createPerson(): Person {
+    private fun createPerson(withUsername: Boolean = false, isAdmin: Boolean = false): Person {
         return Person().apply {
             fatherName = "Doe"
             firstNames = "Jane"
             lastName = "Doe"
+            if(withUsername){
+                username = "dummyUserName"
+            }
+            admin = isAdmin
+            personUid = mPersonUid
+            repo.personDao.insert(this)
         }
     }
 
     @Test
-    fun givenPresenterCreatedInRegistrationMode_whenUsernameAndPasswordNotFilledClickSave_shouldShowErrors() {
-        val args = mapOf(UstadView.ARG_WORKSPACE to Json.stringify(WorkSpace.serializer(), WorkSpace().apply {
-            registrationAllowed = true
-        }))
+    fun givenPresenterCreated_whenUsernameIsNullAndSaveClicked_shouldShowErrors(){
+        val person = createPerson(false)
+        val args = mapOf(UstadView.ARG_ENTITY_UID to person.personUid.toString())
+        val presenter = PersonAccountEditPresenter(context, args,mockView, di, mockLifecycleOwner)
+        presenter.onCreate(null)
+        presenter.handleClickSave(person)
+        argumentCaptor<Boolean>{
+            verify(mockView, timeout(defaultTimeOut).atLeastOnce()).usernameRequiredErrorVisible = capture()
+            assertEquals("Username field error was shown", true, firstValue)
+        }
+    }
 
+    @Test
+    fun givenPresenterCreated_whenUserIsNotAdminAndSaveClicked_shouldShowErrors() {
         val person = createPerson()
-        val presenter = PersonEditPresenter(context, args,mockView, di,mockLifecycleOwner)
-
+        val args = mapOf(UstadView.ARG_ENTITY_UID to person.personUid.toString())
+        val presenter = PersonAccountEditPresenter(context,args,mockView, di, mockLifecycleOwner)
         presenter.onCreate(null)
-
         presenter.handleClickSave(person)
-
-        argumentCaptor<Boolean>().apply {
-            verify(mockView, timeout(timeoutInMill)).passwordRequiredErrorVisible = capture()
-            verify(mockView, timeout(timeoutInMill)).usernameRequiredErrorVisible = capture()
-            assertEquals("Required password field errors were shown", true, firstValue)
-            assertEquals("Required username field errors were shown", true, secondValue)
+        argumentCaptor<Boolean>{
+            verify(mockView, timeout(defaultTimeOut).atLeastOnce()).currentPasswordRequiredErrorVisible = capture()
+            assertEquals("Current password field error was shown", true, firstValue)
         }
-
     }
 
     @Test
-    fun givenPresenterCreatedInRegistrationMode_whenPasswordAndConfirmPasswordDoesNotMatchClickSave_shouldShowErrors() {
-        val args = mapOf(UstadView.ARG_WORKSPACE to Json.stringify(WorkSpace.serializer(), WorkSpace().apply {
-            registrationAllowed = true
-        }))
-
-        mockView = mock{
-            on{password}.thenReturn("password")
-            on{confirmedPassword}.thenReturn("password1")
-        }
-
-        val person = createPerson().apply {
-            username = "dummyUsername"
-        }
-
-        val presenter = PersonEditPresenter(context, args,mockView, di,mockLifecycleOwner)
-
+    fun givenPresenterCreated_whenNewPasswordIsNotFilledAndSaveClicked_thenShouldShowError(){
+        val person = createPerson(isAdmin = true)
+        val args = mapOf(UstadView.ARG_ENTITY_UID to person.personUid.toString())
+        val presenter = PersonAccountEditPresenter(context, args,mockView, di, mockLifecycleOwner)
         presenter.onCreate(null)
-
         presenter.handleClickSave(person)
-
-        argumentCaptor<Boolean>().apply {
-            verify(mockView, timeout(timeoutInMill)).noMatchPasswordErrorVisible = capture()
-            assertEquals("Password doesn't match field errors were shown", true, firstValue)
+        argumentCaptor<Boolean>{
+            verify(mockView, timeout(defaultTimeOut).atLeastOnce()).newPasswordRequiredErrorVisible = capture()
+            assertEquals("Password field error was shown", true, firstValue)
         }
-
     }
 
-
     @Test
-    fun givenPresenterCreatedInRegistrationMode_whenFormFilledAndClickSave_shouldRegisterAPerson() {
+    fun givenPresenterCreated_whenAllFieldsAreFilledAndSaveClicked_thenShouldChangePassword(){
 
         mockWebServer.enqueue(MockResponse()
                 .setHeader("Content-Type", "application/json")
                 .setBody(Buffer().write(Json.stringify(UmAccount.serializer(),
                         UmAccount(0L)).toByteArray())))
 
-
         mockView = mock{
-            on{password}.thenReturn("password")
-            on{confirmedPassword}.thenReturn("password")
+            on{currentPassword}.thenReturn("oldPassword")
+            on{newPassword}.thenReturn("password")
         }
-
-        val args = mapOf(UstadView.ARG_WORKSPACE to Json.stringify(WorkSpace.serializer(), WorkSpace().apply {
-            registrationAllowed = true
-        }), ARG_SERVER_URL to serverUrl)
-
-        val person = createPerson().apply {
-            username = "dummyUsername"
-        }
-        val presenter = PersonEditPresenter(context, args,mockView, di,mockLifecycleOwner)
-
+        val person = createPerson(isAdmin = true, withUsername = true)
+        val args = mapOf(UstadView.ARG_ENTITY_UID to person.personUid.toString())
+        val presenter = PersonAccountEditPresenter(context, args,mockView, di, mockLifecycleOwner)
         presenter.onCreate(null)
-
         presenter.handleClickSave(person)
-
-        argumentCaptor<Person>().apply {
-            verifyBlocking(accountManager, timeout(timeoutInMill)){
-                register(capture(), any(), eq(serverUrl), eq(false))
-                assertEquals("Person registration was done", person, firstValue)
-            }
-        }
-
-    }
-
-    @Test
-    fun givenPresenterCreatedInNonRegistrationMode_whenFormFilledAndClickSave_shouldSaveAPersonInDb() {
-        val args = mapOf(UstadView.ARG_WORKSPACE to Json.stringify(WorkSpace.serializer(), WorkSpace().apply {
-            registrationAllowed = false
-        }))
-
-        val person = createPerson().apply {
-            username = "dummyUsername"
-        }
-        val presenter = PersonEditPresenter(context, args,mockView, di,mockLifecycleOwner)
-
-        presenter.onCreate(null)
-
-        presenter.handleClickSave(person)
-
-        argumentCaptor<Person>().apply {
-            verifyBlocking(mockDao, timeout(timeoutInMill)){
-                insertAsync(capture())
-                assertEquals("Person saved in the db", person, firstValue)
+        argumentCaptor<String>{
+            verifyBlocking(accountManager, timeout(defaultTimeOut).atLeastOnce()){
+                changePassword(capture(), any(), any(), any())
+                assertEquals("Password change request was sent", person.username, firstValue)
             }
         }
     }
