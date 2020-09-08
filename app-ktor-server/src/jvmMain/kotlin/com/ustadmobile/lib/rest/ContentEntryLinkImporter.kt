30e7e87f67afe04360303406e520f4aaacc16a83
samih <samih@ustadmobile.com>
2020-09-08 11:08:19 +0400

    remove googleDrive from importer

--- app-ktor-server/src/jvmMain/kotlin/com/ustadmobile/lib/rest/ContentEntryLinkImporter.kt
+++ app-ktor-server/src/jvmMain/kotlin/com/ustadmobile/lib/rest/ContentEntryLinkImporter.kt
@@ -44,115 +44,38 @@ fun Route.ContentEntryLinkImporter() {
             tempDir.mkdir()
             val contentFile = File(tempDir, "url")
 
-            if (url.startsWith("https://drive.google.com/")) {
-
-                var fileIdLookUp = ""
-                if (url.startsWith("https://drive.google.com/file/d/")) {
-                    fileIdLookUp = url.substringAfter("https://drive.google.com/file/d/")
-                } else if (url.startsWith("https://drive.google.com/drive/folders/")) {
-                    fileIdLookUp = url.substringAfter("https://drive.google.com/drive/folders/")
-                }
-
-                val hasChar = fileIdLookUp.firstOrNull { it == '/' || it == '?' }
-                val fileId = if (hasChar == null) fileIdLookUp else fileIdLookUp.substringBefore(hasChar)
-                val apiCall = "https://www.googleapis.com/drive/v3/files/$fileId"
-
-                defaultHttpClient().get<HttpStatement>(apiCall) {
-                    parameter("key", "AIzaSyCoVemuuYfb3zT3Qe-CuCjATKPDVbmSzO0")
-                    parameter("fields", "id,modifiedTime,name,mimeType,description,thumbnailLink")
-                }.execute() {
-
-                    val status = it.status
-
-                    if (status.value == 404) {
-                        // Link is not shareable or not found
-                        this.call.respond(HttpStatusCode.BadRequest, "Google File/Folder not found")
-                        return@execute
-                    }
-
-                    val data = it.receive<GoogleFile>()
-                    val mimeType = data.mimeType ?: ""
-
-                    val supported = mimeTypeSupported.find { fileMimeType -> fileMimeType == mimeType }
-                    when {
-                        supported != null -> {
-
-                            defaultHttpClient().get<HttpStatement>(apiCall) {
-                                parameter("alt", "media")
-                                parameter("key", "AIzaSyCoVemuuYfb3zT3Qe-CuCjATKPDVbmSzO0")
-                            }.execute() { fileResponse ->
-
-                                val stream = fileResponse.receive<InputStream>()
-                                FileUtils.writeByteArrayToFile(contentFile, stream.readBytes())
-
-                                val metadata = extractContentEntryMetadataFromFile(contentFile.absolutePath, db)
-                                if (metadata == null) {
-                                    call.respond(HttpStatusCode.BadRequest, "Unsupported")
-                                } else {
-                                    // so contentEntryEdit calls downloadLink instead of file
-                                    metadata.fileUri = apiCall
-                                    call.respond(metadata)
-                                }
-                            }
-
-                        }
-                        mimeType == "application/vnd.google-apps.folder" -> {
-
-                            val entry = ContentEntryWithLanguage().apply {
-                                contentFlags = ContentEntry.FLAG_IMPORTED
-                                contentTypeFlag = ContentEntry.TYPE_COLLECTION
-                                licenseType = ContentEntry.LICENSE_TYPE_OTHER
-                                title = data.name
-                                description = data.description
-                                thumbnailUrl = data.thumbnailLink
-                                entryId = data.id
-                                leaf = false
-                            }
-
-                            call.respond(ImportedContentEntryMetaData(entry, data.mimeType!!, apiCall, GFOLDER))
-
-                        }
-                        else -> {
-                            this.call.respond(HttpStatusCode.BadRequest, "Unsupported")
-                            return@execute
-                        }
-                    }
-                }
-
-            } else {
-
-                val huc: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+            val huc: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
 
 
-                val mimeType = huc.contentType
-                val data = huc.inputStream
-                FileUtils.writeByteArrayToFile(contentFile, data.readBytes())
+            val mimeType = huc.contentType
+            val data = huc.inputStream
+            FileUtils.writeByteArrayToFile(contentFile, data.readBytes())
 
-                val supported = mimeTypeSupported.find { fileMimeType -> fileMimeType == mimeType }
-                if (supported != null) {
-                    val metadata = extractContentEntryMetadataFromFile(contentFile.toURI().toString(), db)
-                    if (metadata == null) {
-                        call.respond(HttpStatusCode.BadRequest, "Unsupported")
-                    } else {
-                        call.respond(metadata)
-                    }
+            val supported = mimeTypeSupported.find { fileMimeType -> fileMimeType == mimeType }
+            if (supported != null) {
+                val metadata = extractContentEntryMetadataFromFile(contentFile.toURI().toString(), db)
+                if (metadata == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Unsupported")
                 } else {
-                    this.call.respond(HttpStatusCode.BadRequest, "Unsupported")
+                    call.respond(metadata)
                 }
-
+            } else {
+                this.call.respond(HttpStatusCode.BadRequest, "Unsupported")
             }
 
         }
-        
+
+    }
 
 
-        post("downloadLink") {
 
-
-        }
+    post("downloadLink") {
 
 
     }
+
+
+}
 
 
 }
