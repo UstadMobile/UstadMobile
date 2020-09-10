e5af7c9fb00ce197bf3c8b58e36426558e10037d
Mike Dawson <mike@ustadmobile.com>
2020-09-11 00:14:37 +0400

    Implement filter

--- core/src/commonMain/kotlin/com/ustadmobile/core/view/ContainerMounter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/view/ContainerMounter.kt
@@ -5,7 +5,20 @@ package com.ustadmobile.core.view
  */
 interface ContainerMounter {
 
-    suspend fun mountContainer(endpointUrl: String, containerUid: Long): String
+    suspend fun mountContainer(endpointUrl: String, containerUid: Long, filterMode: Int = FILTER_MODE_NONE): String
 
     suspend fun unMountContainer(endpointUrl: String, mountPath: String)
+
+    companion object {
+        /**
+         * Do not filter any responses
+         */
+        const val FILTER_MODE_NONE = 0
+
+        /**
+         * Filter HTML mime types. A meta viewport tag and some basic responsive css will be
+         * automatically added to any HTML that does not already have it.
+         */
+        const val FILTER_MODE_EPUB = 1
+    }
 }
