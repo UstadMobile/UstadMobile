26d0e8037b271040bb15b1053b087f8e6f397c7e
samih <samih@ustadmobile.com>
2020-09-20 17:30:48 +0400

    generate uid if id is empty

--- sharedse/src/commonJvmMain/kotlin/com/ustadmobile/port/sharedse/contentformats/epub/EpubTypeFilePlugin.kt
+++ sharedse/src/commonJvmMain/kotlin/com/ustadmobile/port/sharedse/contentformats/epub/EpubTypeFilePlugin.kt
@@ -3,6 +3,7 @@ package com.ustadmobile.port.sharedse.contentformats.epub
 import com.ustadmobile.core.catalog.contenttype.EpubTypePlugin
 import com.ustadmobile.core.contentformats.epub.opf.OpfDocument
 import com.ustadmobile.core.impl.UstadMobileSystemImpl
+import com.ustadmobile.core.util.ext.alternative
 import com.ustadmobile.lib.db.entities.ContentEntryWithLanguage
 import com.ustadmobile.lib.db.entities.ContentEntry
 import com.ustadmobile.lib.db.entities.Language
@@ -19,6 +20,7 @@ import java.util.zip.ZipInputStream
 import com.ustadmobile.lib.db.entities.ContentEntry.Companion.LICENSE_TYPE_OTHER
 import com.ustadmobile.port.sharedse.contentformats.ContentTypeUtil.ZIPPED
 import java.net.URI
+import java.util.*
 
 /**
  * Class which handles EPUB content import tasks, creates content entry from the H5P file
@@ -45,7 +47,7 @@ class EpubTypeFilePlugin : EpubTypePlugin(), ContentTypeFilePlugin {
                         contentEntryVal.author = opfDocument.getCreator(0)?.creator
                         contentEntryVal.description = opfDocument.description
                         contentEntryVal.leaf = true
-                        contentEntryVal.entryId = opfDocument.id
+                        contentEntryVal.entryId = opfDocument.id.alternative(UUID.randomUUID().toString())
                         val languageCode = opfDocument.getLanguage(0)
                         if (languageCode != null) {
                             val language = Language()
