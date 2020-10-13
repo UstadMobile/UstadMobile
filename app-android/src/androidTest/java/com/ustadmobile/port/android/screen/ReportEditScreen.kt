3976bd1a94cb39509fb46df3162093a09f0061c2
samih <samih@ustadmobile.com>
2020-10-13 12:47:36 +0400

    report edit changed

---
+++ app-android/src/androidTest/java/com/ustadmobile/port/android/screen/ReportEditScreen.kt
@@ -0,0 +1,111 @@
+package com.ustadmobile.port.android.screen
+
+import android.content.Context
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.navigation.fragment.findNavController
+import com.agoda.kakao.edit.KTextInputLayout
+import com.agoda.kakao.text.KTextView
+import com.kaspersky.kaspresso.screens.KScreen
+import com.toughra.ustadmobile.R
+import com.ustadmobile.core.controller.ReportEditPresenter
+import com.ustadmobile.core.impl.UstadMobileSystemImpl
+import com.ustadmobile.core.networkmanager.defaultGson
+import com.ustadmobile.lib.db.entities.ContentEntry
+import com.ustadmobile.lib.db.entities.Person
+import com.ustadmobile.lib.db.entities.ReportWithFilters
+import com.ustadmobile.lib.db.entities.VerbDisplay
+import com.ustadmobile.port.android.view.ReportEditFragment
+import com.ustadmobile.test.port.android.util.setDateField
+import com.ustadmobile.test.port.android.util.setMessageIdOption
+
+object ReportEditScreen : KScreen<ReportEditScreen>() {
+
+    override val layoutId: Int?
+        get() = R.layout.fragment_report_edit
+    override val viewClass: Class<*>?
+        get() = ReportEditScreen::class.java
+
+    val reportTitleInput = KTextInputLayout { withId(R.id.fragment_report_edit_title_layout)}
+
+    val visualTypeValue = KTextView { withId(R.id.fragment_edit_report_dialog_visual_type_text)}
+
+    val yAxisValue = KTextView { withId(R.id.yaxis_value)}
+
+    val xAxisValue = KTextView { withId(R.id.fragment_edit_report_dialog_xaxis_text)}
+
+    val subGroupValue = KTextView { withId(R.id.fragment_edit_report_dialog_subgroup_text)}
+
+    val fromDateTextInput = KTextInputLayout { withId(R.id.activity_report_edit_fromDate_textinputlayout)}
+
+    val toDateTextInput = KTextInputLayout { withId(R.id.activity_report_edit_toDate_textinputlayout)}
+
+    fun fillFields(fragmentScenario: FragmentScenario<ReportEditFragment>? = null,
+                   report: ReportWithFilters,
+                   reportOnForm: ReportWithFilters? = ReportWithFilters(),
+                   setFieldsRequiringNavigation: Boolean = true, person: Person? = null,
+                   verbDisplay: VerbDisplay? = null, entry: ContentEntry? = null, impl: UstadMobileSystemImpl, context: Context) {
+
+        report.reportTitle?.takeIf { it != reportOnForm?.reportTitle }?.also {
+            reportTitleInput{
+                edit{
+                    clearText()
+                    typeText(it)
+                }
+            }
+        }
+
+        closeSoftKeyboard()
+
+        report.chartType.takeIf { it != reportOnForm?.chartType }?.also {
+            setMessageIdOption(visualTypeValue,
+                    impl.getString(ReportEditPresenter.ChartOptions.values().find { report -> report.optionVal == it }!!.messageId, context))
+        }
+
+        report.yAxis.takeIf { it != reportOnForm?.yAxis }?.also {
+            setMessageIdOption(yAxisValue,
+                    impl.getString(ReportEditPresenter.YAxisOptions.values().find { report -> report.optionVal == it }!!.messageId, context))
+        }
+
+        report.xAxis.takeIf { it != reportOnForm?.xAxis }?.also {
+            setMessageIdOption(xAxisValue,
+                    impl.getString(ReportEditPresenter.XAxisOptions.values().find { report -> report.optionVal == it }!!.messageId, context))
+        }
+
+        report.subGroup.takeIf { it != reportOnForm?.subGroup }?.also {
+            setMessageIdOption(subGroupValue,
+                    impl.getString(ReportEditPresenter.XAxisOptions.values().find { report -> report.optionVal == it }!!.messageId, context))
+        }
+
+        report.fromDate.takeIf { it != reportOnForm?.fromDate }?.also {
+            setDateField(R.id.activity_report_edit_fromDate_textinputlayout, it)
+        }
+        report.toDate.takeIf { it != reportOnForm?.toDate }?.also {
+            setDateField(R.id.activity_report_edit_toDate_textinputlayout, it)
+        }
+
+        if (!setFieldsRequiringNavigation) {
+            return
+        }
+
+        fragmentScenario?.onFragment { fragment ->
+            fragment.takeIf { verbDisplay != null }
+                    ?.findNavController()?.currentBackStackEntry?.savedStateHandle
+                    ?.set("VerbDisplay", defaultGson().toJson(listOf(verbDisplay)))
+        }
+
+        fragmentScenario?.onFragment { fragment ->
+            fragment.takeIf { person != null }
+                    ?.findNavController()?.currentBackStackEntry?.savedStateHandle
+                    ?.set("Person", defaultGson().toJson(listOf(person)))
+        }
+
+        fragmentScenario?.onFragment { fragment ->
+            fragment.takeIf { entry != null }
+                    ?.findNavController()?.currentBackStackEntry?.savedStateHandle
+                    ?.set("Content", defaultGson().toJson(listOf(entry)))
+        }
+
+    }
+
+
+}
