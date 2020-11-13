de293798ebae002a4823e214534a3a5f2886642c
Mike Dawson <mike@ustadmobile.com>
2020-11-13 11:44:13 +0400

    Fix DownloadJobConnectivityChangeTriggerEventsTest test failure. Fix ClazzLogScheduleWorker to use repo.

--- core/src/androidMain/kotlin/com/ustadmobile/core/schedule/ClazzLogScheduleWorker.kt
+++ core/src/androidMain/kotlin/com/ustadmobile/core/schedule/ClazzLogScheduleWorker.kt
@@ -5,7 +5,7 @@ import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.ustadmobile.core.account.Endpoint
 import com.ustadmobile.core.db.UmAppDatabase
-import com.ustadmobile.core.db.UmAppDatabase.Companion.TAG_DB
+import com.ustadmobile.core.db.UmAppDatabase.Companion.TAG_REPO
 import org.kodein.di.DI
 import org.kodein.di.android.di
 import org.kodein.di.instance
@@ -17,12 +17,12 @@ class ClazzLogScheduleWorker(context: Context, workerParams: WorkerParameters) :
 
     override fun doWork(): Result {
         val endpoint = inputData.getString(INPUT_ENDPOINTURL) ?: return Result.failure()
-        val db: UmAppDatabase by di.on(Endpoint(endpoint)).instance(tag = TAG_DB)
+        val repo: UmAppDatabase by di.on(Endpoint(endpoint)).instance(tag = TAG_REPO)
         val fromTime = inputData.getLong(INPUT_FROMTIME, Long.MAX_VALUE)
         val toTime = inputData.getLong(INPUT_TOTIME, 0L)
         val clazzUidFilter = inputData.getLong(INPUT_CLAZZUIDFILTER, 0)
         val matchLocalFromDay = inputData.getBoolean(INPUT_MATCH_LOCAL_FROM_DAY, false)
-        db.createClazzLogs(fromTime, toTime, clazzUidFilter, matchLocalFromDay)
+        repo.createClazzLogs(fromTime, toTime, clazzUidFilter, matchLocalFromDay)
 
         return Result.success()
     }
