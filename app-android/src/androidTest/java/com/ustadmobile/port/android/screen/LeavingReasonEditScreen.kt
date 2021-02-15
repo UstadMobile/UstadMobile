1ba7ab6e6825e21b75ac55ad80e5dac43694fb0e
samih <samih@ustadmobile.com>
2021-02-15 18:09:14 +0400

    remove fill fields from leavingReasonEditScreen

--- app-android/src/androidTest/java/com/ustadmobile/port/android/screen/LeavingReasonEditScreen.kt
+++ app-android/src/androidTest/java/com/ustadmobile/port/android/screen/LeavingReasonEditScreen.kt
@@ -18,39 +18,7 @@ object LeavingReasonEditScreen : KScreen<LeavingReasonEditScreen>() {
     override val viewClass: Class<*>?
         get() = LeavingReasonEditFragment::class.java
 
-    val LeavingReasonTitleInput = KTextInputLayout { withId(R.id.id_of_textfield_input_layout)}
-
-
-    fun fillFields(fragmentScenario: FragmentScenario<LeavingReasonEditFragment>,
-                   leavingReason: LeavingReason,
-                   leavingReasonOnForm: LeavingReason?,
-                   setFieldsRequiringNavigation: Boolean = true,
-                   impl: UstadMobileSystemImpl, context: Context,
-                   testContext: TestContext<Unit>) {
-        //TODO: set these values on the form using Espresso.
-
-        leavingReason.leavingReasonName?.takeIf {it != leavingReasonOnForm?.leavingReasonName }?.also {
-            LeavingReasonTitleInput{
-                edit{
-                    clearText()
-                    typeText(it)
-                }
-            }
-        }
-
-        if(!setFieldsRequiringNavigation) {
-            return
-        }
-
-        //TODO: if required, use the savedstatehandle to add link entities
-
-        fragmentScenario.onFragment { fragment ->
-            fragment.takeIf {leavingReason.relatedEntity != leavingReasonOnForm?.relatedEntity }
-                    ?.findNavController()?.currentBackStackEntry?.savedStateHandle
-                    ?.set("RelatedEntityName", defaultGson().toJson(listOf(leavingReason.relatedEntity)))
-        }
-
-    }
+   // val LeavingReasonTitleInput = KTextInputLayout { withId(R.id.id_of_textfield_input_layout)}
 
 
 }
