880ef2d3815843daff20609f25291d9da431cba6
samih <samih@ustadmobile.com>
2021-03-02 10:01:57 +0400

    missed one screen and added check for filter

---
+++ app-android/src/androidTest/java/com/ustadmobile/port/android/screen/ClazzEnrolmentEditScreen.kt
@@ -0,0 +1,25 @@
+package com.ustadmobile.port.android.screen
+
+import com.agoda.kakao.edit.KTextInputLayout
+import com.kaspersky.kaspresso.screens.KScreen
+import com.toughra.ustadmobile.R
+import com.ustadmobile.port.android.view.ClazzEnrolmentEditFragment
+
+object ClazzEnrolmentEditScreen: KScreen<ClazzEnrolmentEditScreen>() {
+
+    override val layoutId: Int?
+        get() = R.layout.fragment_clazz_enrolment
+    override val viewClass: Class<*>?
+        get() = ClazzEnrolmentEditFragment::class.java
+
+    val roleInputLayout = KTextInputLayout { withId(R.id.clazz_enrolment_edit_role_textinputlayout) }
+
+    val startDateLayout = KTextInputLayout {withId(R.id.clazz_enrolment_edit_fromDate_textinputlayout)}
+
+    val endDateLayout = KTextInputLayout {withId(R.id.clazz_enrolment_edit_toDate_textinputlayout)}
+
+    val statusLayout = KTextInputLayout {withId(R.id.clazz_enrolment_edit_status_textinputlayout)}
+
+    val leavingReasonLayout = KTextInputLayout {withId(R.id.clazz_enrolment_edit_reason_textinputlayout)}
+
+}
