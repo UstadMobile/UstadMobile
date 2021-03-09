97a15024ab317a6880aea3bc14ca1de39d3ea2e4
Varuna Singh <varuna.singh@gmail.com>
2021-03-09 21:24:13 +0400

    Content upload bug fix

--- lib-content-scrapers/src/jvmMain/kotlin/com/ustadmobile/lib/contentscrapers/abztract/Indexer.kt
+++ lib-content-scrapers/src/jvmMain/kotlin/com/ustadmobile/lib/contentscrapers/abztract/Indexer.kt
@@ -32,7 +32,7 @@ abstract class Indexer(val parentContentEntryUid: Long, val runUid: Int, val sqi
             contentEntry = db.contentEntryDao.findByUidAsync(contentEntryUid)
             scrapeQueueItem = db.scrapeQueueItemDao.findByUid(sqiUid)
         }
-        LanguageList().addAllLanguages()
+
     }
 
     fun createQueueItem(queueUrl: String, contentEntry: ContentEntry?, contentType: String, scraperType: Int, parentContentEntryUid: Long , priority: Int = 1) {
