f5138a7bf8415783204827d2549240c467ccbd63
samih <samih@ustadmobile.com>
2021-03-28 15:36:24 +0400

    not more than 15 length string

--- core/src/commonMain/kotlin/com/ustadmobile/core/util/graph/UidAndLabelFormatter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/util/graph/UidAndLabelFormatter.kt
@@ -1,5 +1,7 @@
 package com.ustadmobile.core.util.graph
 
+import kotlin.math.min
+
 class UidAndLabelFormatter(val uidMap: Map<Long, String?>) : LabelValueFormatter {
 
     override fun format(option: Any): String {
@@ -7,7 +9,15 @@ class UidAndLabelFormatter(val uidMap: Map<Long, String?>) : LabelValueFormatter
     }
 
     override fun formatAsList(option: List<Any>): List<String> {
-        return option.map { x -> format(x) }
+        return option.map { x ->
+            val str = format(x)
+            val min = min(15, str.length)
+            var newStr = str.substring(0, min)
+            if (min == 15 && str.length != 15) {
+                newStr += "..."
+            }
+            newStr
+        }
     }
 
 }
