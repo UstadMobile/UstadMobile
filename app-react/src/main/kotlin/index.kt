96c75fb37d1542957f76c18af21adabfeebdcdd0
Lukundo Kileha <lkileha@gmail.com>
2021-04-03 08:29:49 +0300

    Making sure default serializer is registered and app runs as expected

--- app-react/src/main/kotlin/index.kt
+++ app-react/src/main/kotlin/index.kt
@@ -1,14 +1,14 @@
-import react.dom.render
+import com.ustadmobile.core.impl.UstadMobileSystemImpl
+import com.ustadmobile.view.umBaseApp
 import kotlinx.browser.document
 import kotlinx.browser.window
-import styled.styledDiv
+import react.dom.render
 
 fun main() {
+    UstadMobileSystemImpl.instance.registerDefaultSerializer()
     window.onload = {
         render(document.getElementById("root")) {
-            styledDiv {
-                +"UstadMobile app running..."
-            }
+            umBaseApp()
         }
     }
 }
