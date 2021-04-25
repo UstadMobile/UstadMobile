55a949c8bb442de272538f347e560cc063956d02
samih <samih@ustadmobile.com>
2021-04-25 14:09:52 +0400

    dao for clazz Assignment list screen

--- lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/dao/ClazzAssignmentDao.kt
+++ lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/dao/ClazzAssignmentDao.kt
@@ -1,14 +1,49 @@
 package com.ustadmobile.core.db.dao
 
+import androidx.paging.DataSource
 import androidx.room.Dao
+import androidx.room.Query
 import com.ustadmobile.door.annotation.Repository
 import com.ustadmobile.lib.db.entities.ClazzAssignment
+import com.ustadmobile.lib.db.entities.ClazzWorkWithMetrics
 
 
 @Dao
 @Repository
 abstract class ClazzAssignmentDao : BaseDao<ClazzAssignment> {
 
+    @Query("""
+            SELECT ClazzAssignment.*, 
+            
+            
+            FROM ClazzAssignment
+        
+        
+            WHERE clazzAssignmentActive
+            AND (ClazzAssignment.clazzAssignmentTitle LIKE :searchText 
+                    OR ClazzAssignment.clazzAssignmentDescription :searchText)
+            ORDER BY CASE(:sortOrder)
+                WHEN $SORT_DEADLINE_ASC THEN ClazzAssignment.clazzAssignmentDeadlineDateTime
+                WHEN $SORT_SCORE_ASC THEN clazzAssignmentTotalScore
+                ELSE 0
+            END ASC,
+            CASE(:sortOrder)
+                WHEN $SORT_DEADLINE_DESC THEN ClazzAssignment.clazzAssignmentDeadlineDateTime
+                WHEN $SORT_SCORE_DESC THEN clazzAssignmentTotalScore
+                ELSE 0
+            END DESC,
+            CASE(:sortOrder)
+                WHEN $SORT_TITLE_ASC THEN ClazzAssignment.clazzAssignmentTitle
+                ELSE ''
+            END ASC,
+            CASE(:sortOrder)
+                WHEN $SORT_TITLE_DESC THEN ClazzAssignment.clazzAssignmentTitle
+                ELSE ''
+            END DESC
+    """)
+    abstract fun getAllAssignments(clazzUid: Long, today: Long,
+                                   sortOrder: Int, searchText: String? = "%"):
+            DataSource.Factory<Int, ClazzAssignment>
 
     companion object{
 
