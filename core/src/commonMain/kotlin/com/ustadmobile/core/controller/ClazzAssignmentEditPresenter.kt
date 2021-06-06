4babc37bd8c6ebeb27d85b6f2abf3e9bc369d83d
samih <samih@ustadmobile.com>
2021-06-06 13:13:45 +0400

    using createNavigateForResultListener

--- core/src/commonMain/kotlin/com/ustadmobile/core/controller/ClazzAssignmentEditPresenter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/controller/ClazzAssignmentEditPresenter.kt
@@ -9,6 +9,7 @@ import com.ustadmobile.core.util.ext.putEntityAsJson
 import com.ustadmobile.core.view.*
 import com.ustadmobile.core.view.UstadEditView.Companion.ARG_ENTITY_JSON
 import com.ustadmobile.core.view.UstadView.Companion.ARG_ENTITY_UID
+import com.ustadmobile.core.view.UstadView.Companion.ARG_LISTMODE
 import com.ustadmobile.door.DoorLifecycleOwner
 import com.ustadmobile.door.doorMainDispatcher
 import com.ustadmobile.lib.db.entities.*
@@ -50,37 +51,15 @@ class ClazzAssignmentEditPresenter(context: Any,
             ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer::class) {contentEntryUid = it}
 
 
-    val contentOneToManyJoinListener: OneToManyJoinEditListener<
-            ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer> = object :
-            OneToManyJoinEditListener<ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer> {
+    val contentOneToManyJoinListener = contentOneToManyJoinEditHelper.createNavigateForResultListener(
+            ContentEntryList2View.VIEW_NAME,
+            ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer.serializer(),
+            mutableMapOf(ContentEntryList2View.ARG_CLAZZ_ASSIGNMENT_FILTER to entity?.caUid.toString(),
+                    ContentEntryList2View.ARG_CONTENT_FILTER to ContentEntryList2View.ARG_LIBRARIES_CONTENT,
+                    UstadView.ARG_PARENT_ENTRY_UID to  UstadView.MASTER_SERVER_ROOT_ENTRY_UID.toString(),
+                    ContentEntryList2View.ARG_SELECT_FOLDER_VISIBLE to false.toString(),
+                    ARG_LISTMODE to ListViewMode.PICKER.toString()))
 
-        override fun onClickNew() {
-            val args = mutableMapOf<String, String>()
-            args[ContentEntryList2View.ARG_CLAZZ_ASSIGNMENT_FILTER] = entity?.caUid.toString()
-            args[ContentEntryList2View.ARG_CONTENT_FILTER] = ContentEntryList2View.ARG_LIBRARIES_CONTENT
-            args[UstadView.ARG_PARENT_ENTRY_UID] to  UstadView.MASTER_SERVER_ROOT_ENTRY_UID.toString()
-            args[ContentEntryList2View.ARG_SELECT_FOLDER_VISIBLE] to false.toString()
-
-            navigateToPickEntityFromList(
-                    NavigateForResultOptions(this@ClazzAssignmentEditPresenter,
-                            null, ContentEntryList2View.VIEW_NAME,
-                            ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer::class,
-                            ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer.serializer(),
-                            ARG_SAVEDSTATE_CONTENT,
-                            arguments = args)
-            )
-
-        }
-
-        override fun onClickEdit(joinedEntity: ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer) {
-            // do nothing, cant edit content here
-        }
-
-        override fun onClickDelete(joinedEntity: ContentEntryWithParentChildJoinAndStatusAndMostRecentContainer) {
-            contentOneToManyJoinEditHelper.onDeactivateEntity(joinedEntity)
-        }
-
-    }
 
     override fun onCreate(savedState: Map<String, String>?) {
         super.onCreate(savedState)
