d82e7ff7943f7fd5b63ba3debb6e294fc58c2e01
samih <samih@ustadmobile.com>
2021-06-20 17:16:01 +0400

    ui for new import

--- app-android/src/main/java/com/ustadmobile/port/android/view/ContentEntryAddOptionsBottomSheetFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/ContentEntryAddOptionsBottomSheetFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.toughra.ustadmobile.R
-import com.toughra.ustadmobile.databinding.FragmentContentEntryAddOptionsBinding
 import com.ustadmobile.core.impl.UMAndroidUtil
 import com.ustadmobile.core.view.ContentEntryAddOptionsView
+import com.toughra.ustadmobile.databinding.FragmentContentEntryAddOptionsBinding
 import com.ustadmobile.core.view.UstadView.Companion.ARG_LEAF
 import com.ustadmobile.core.view.UstadView.Companion.ARG_PARENT_ENTRY_UID
 
@@ -25,40 +25,54 @@ class ContentEntryAddOptionsBottomSheetFragment : BottomSheetDialogFragment(), C
 
     private var createFolderOptionView: View? = null
 
-    private var importContentOptionView: View? = null
+    private var addLinkOptionView: View? = null
+
+    private var addGalleryOptionView: View? = null
+
+    private var addFileOptionView: View? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+                              savedInstanceState: Bundle?): View? {
         return FragmentContentEntryAddOptionsBinding.inflate(inflater, container, false).also {
             createFolderOptionView = it.contentCreateFolder
-            importContentOptionView = it.contentImportContent
+            addLinkOptionView = it.contentAddLink
+            addGalleryOptionView = it.contentAddGallery
+            addFileOptionView = it.contentAddFile
             createFolderOptionView?.setOnClickListener(this)
-            importContentOptionView?.setOnClickListener(this)
+            addLinkOptionView?.setOnClickListener(this)
+            addFileOptionView?.setOnClickListener(this)
+            addGalleryOptionView?.setOnClickListener(this)
         }.root
     }
 
     override fun onClick(view: View?) {
         val isShowing = this.dialog?.isShowing
-       val contentType =  when(view?.id){
+        val contentType = when (view?.id) {
             R.id.content_create_folder -> false
-            R.id.content_import_content -> true
-           else -> -1
-       }
+            R.id.content_add_file -> true
+            R.id.content_add_link -> true
+            R.id.content_add_gallery -> true
+            else -> -1
+        }
 
         findNavController().navigate(R.id.content_entry_edit_dest, UMAndroidUtil.mapToBundle(mapOf(
                 ARG_PARENT_ENTRY_UID to arguments?.get(ARG_PARENT_ENTRY_UID).toString(),
                 ARG_LEAF to contentType.toString())))
-        if(isShowing != null && isShowing){
+        if (isShowing != null && isShowing) {
             dismiss()
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        importContentOptionView?.setOnClickListener(null)
+        addLinkOptionView?.setOnClickListener(null)
         createFolderOptionView?.setOnClickListener(null)
-        importContentOptionView = null
+        addGalleryOptionView?.setOnClickListener(null)
+        addFileOptionView?.setOnClickListener(null)
+        addLinkOptionView = null
         createFolderOptionView = null
+        addFileOptionView = null
+        addGalleryOptionView = null
     }
 
 }
