c6a9e766376f456be1a69d19b21814c601e8d1f6
samih <samih@ustadmobile.com>
2021-06-29 12:03:07 +0400

    handle unselected file

--- app-android/src/main/java/com/ustadmobile/port/android/view/SelectFileFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/SelectFileFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
 import com.ustadmobile.core.impl.UstadMobileSystemImpl
 import com.ustadmobile.core.util.ext.putFromOtherMapIfPresent
 import com.ustadmobile.core.util.ext.toStringMap
@@ -29,20 +30,23 @@ class SelectFileFragment(private val registry: ActivityResultRegistry? = null) :
         activityResultLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument(),
                 registry ?: requireActivity().activityResultRegistry) {
 
-            if (arguments?.containsKey(UstadView.ARG_RESULT_DEST_ID) == true) {
-                saveResultToBackStackSavedStateHandle(listOf(it.toString()))
-            } else {
-                val map = arguments.toStringMap()
-                val args = mutableMapOf<String, String>()
-                args[ARG_URI] = it.toString()
-                args.putFromOtherMapIfPresent(map, UstadView.ARG_LEAF)
-                args.putFromOtherMapIfPresent(map, UstadView.ARG_PARENT_ENTRY_UID)
-                systemImpl.go(ContentEntryEdit2View.VIEW_NAME,
-                        args, requireContext())
+            when {
+                it == null -> {
+                    findNavController().popBackStack()
+                }
+                arguments?.containsKey(UstadView.ARG_RESULT_DEST_ID) == true -> {
+                    saveResultToBackStackSavedStateHandle(listOf(it.toString()))
+                }
+                else -> {
+                    val map = arguments.toStringMap()
+                    val args = mutableMapOf<String, String>()
+                    args[ARG_URI] = it.toString()
+                    args.putFromOtherMapIfPresent(map, UstadView.ARG_LEAF)
+                    args.putFromOtherMapIfPresent(map, UstadView.ARG_PARENT_ENTRY_UID)
+                    systemImpl.go(ContentEntryEdit2View.VIEW_NAME,
+                            args, requireContext())
+                }
             }
-
-
-
 
         }
 
