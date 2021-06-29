3a3cc66b6fb2eb3dd4edc4820f5eab9b35a042d2
samih <samih@ustadmobile.com>
2021-06-29 10:46:34 +0400

    update plugin

--- core/src/commonMain/kotlin/com/ustadmobile/core/contentjob/ContentPlugin.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/contentjob/ContentPlugin.kt
@@ -2,15 +2,20 @@ package com.ustadmobile.core.contentjob
 
 import com.ustadmobile.door.DoorUri
 import com.ustadmobile.lib.db.entities.ContentEntry
+import com.ustadmobile.lib.db.entities.ContentEntryWithLanguage
 import com.ustadmobile.lib.db.entities.DownloadJobItem
 
-interface ContentJobItemWorker {
+interface ContentPlugin {
 
     val jobType: Int
 
+    val supportedMimeTypes: List<String>
+
+    val supportedFileExtensions: List<String>
+
     suspend fun canProcess(doorUri: DoorUri): Boolean
 
-    suspend fun extractMetadata(uri: DoorUri): ContentEntry?
+    suspend fun extractMetadata(uri: DoorUri): ContentEntryWithLanguage?
 
     //TODO: DownloadJobItem will be renamed to ContentJobItem
     //ContentJobItem will need fields for jobType, fromUri, toUri
