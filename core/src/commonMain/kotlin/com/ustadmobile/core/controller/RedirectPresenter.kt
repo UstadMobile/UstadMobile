339a9a201d9d1306c2505c2619e25e624ff085be
Mike Dawson <mike@ustadmobile.com>
2021-07-16 10:04:22 +0400

    Minor refactor of RedirectPresenter to convert if statement to when.

--- core/src/commonMain/kotlin/com/ustadmobile/core/controller/RedirectPresenter.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/controller/RedirectPresenter.kt
@@ -1,13 +1,10 @@
 package com.ustadmobile.core.controller
 
 import com.ustadmobile.core.account.UstadAccountManager
-import com.ustadmobile.core.impl.AppConfig
 import com.ustadmobile.core.impl.UstadMobileSystemImpl
 import com.ustadmobile.core.view.*
 import com.ustadmobile.core.view.UstadView.Companion.ARG_DEEPLINK
 import com.ustadmobile.core.view.UstadView.Companion.ARG_NEXT
-import com.ustadmobile.door.doorMainDispatcher
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.kodein.di.DI
 import org.kodein.di.instance
@@ -25,14 +22,20 @@ class RedirectPresenter(context: Any, arguments: Map<String, String>, view: Redi
         val nextViewArg = arguments[ARG_NEXT]
         val deepLink = arguments[ARG_DEEPLINK]
 
-        if(deepLink?.isNotEmpty() == true){
-            systemImpl.goToDeepLink(deepLink, accountManager, context)
-        }else {
-            if(nextViewArg != null) {
+        when {
+            deepLink?.isNotEmpty() == true -> {
+                systemImpl.goToDeepLink(deepLink, accountManager, context)
+            }
+
+            nextViewArg != null -> {
                 systemImpl.goToViewLink(nextViewArg, context)
-            }else if(accountManager.activeSession != null) {
+            }
+
+            accountManager.activeSession != null -> {
                 systemImpl.goToViewLink(ContentEntryListTabsView.VIEW_NAME, context)
-            }else {
+            }
+
+            else -> {
                 presenterScope.launch {
                     navigateToStartNewUserSession()
                 }
