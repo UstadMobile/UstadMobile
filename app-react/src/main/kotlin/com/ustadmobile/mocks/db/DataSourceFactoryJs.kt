788c65997c523157b4922f0774eb806754b2278d
Lukundo Kileha <lkileha@gmail.com>
2021-07-21 10:37:17 +0300

    Minor change on data factory data loading

--- app-react/src/main/kotlin/com/ustadmobile/mocks/db/DataSourceFactoryJs.kt
+++ app-react/src/main/kotlin/com/ustadmobile/mocks/db/DataSourceFactoryJs.kt
@@ -1,66 +1,10 @@
 package com.ustadmobile.mocks.db
 
 import androidx.paging.DataSource
-import com.ustadmobile.lib.db.entities.ClazzEnrolment
-import com.ustadmobile.lib.db.entities.ClazzWithListDisplayDetails
-import com.ustadmobile.lib.db.entities.EntityRoleWithNameAndRole
-import com.ustadmobile.lib.db.entities.PersonWithDisplayDetails
-import com.ustadmobile.mocks.db.ClazzDaoJs.Companion.CLAZZ_LIST
-import com.ustadmobile.util.Util.loadDataAsList
-import kotlinx.serialization.DeserializationStrategy
-import kotlin.js.Date
 
-class DataSourceFactoryJs<Key,Value, EXtra>(private val key:String? = null,
-                                            private val filterBy: Any,
-                                            private val sourcePath: String,
-                                            private val dStrategy: DeserializationStrategy<List<Value>>,
-                                            private val extraStrategy: DeserializationStrategy<List<EXtra>>? = null,
-                                            private val targetKey: String? = null,
-                                            private val relationKey: String? = null,
-                                            private val extraKey:String? = null,
-                                            private val extraSourcePath:String? = null
-): DataSource.Factory<Key,Value>() {
+class DataSourceFactoryJs<Key,Value>(private val values:List<Value>): DataSource.Factory<Key,Value>() {
 
     override suspend fun getData(offset: Int, limit: Int): List<Value> {
-        var dataSet = loadDataAsList(sourcePath,dStrategy)
-        console.log(dataSet)
-
-        if(sourcePath == "people"){
-            return listOf(PersonWithDisplayDetails().apply {
-                personUid = filterBy.toString().toLong()
-                username = "admin"
-                firstNames = "Admin"
-                admin = true
-                lastName = "Users"
-            } as Value)
-        }
-
-        if(sourcePath == "roles"){
-            return listOf(EntityRoleWithNameAndRole().apply {
-            } as Value)
-        }
-
-        if(sourcePath == "clazzList"){
-            return CLAZZ_LIST as List<Value>
-        }
-
-        if(key != null && dataSet.isNotEmpty()){
-            dataSet = dataSet.filter{it.asDynamic()[key].toString() == filterBy.toString()}
-        }
-
-        if(relationKey != null && extraSourcePath != null){
-            val extraDatSet = loadDataAsList(extraSourcePath,extraStrategy!!)
-            dataSet = dataSet.map { data ->
-                val found = extraDatSet.firstOrNull{ foundDataSet ->
-                    foundDataSet.asDynamic()[extraKey].toString() == data.asDynamic()[relationKey].toString()
-                }
-                if(found != null){
-                    data.asDynamic()[targetKey] = found
-                }
-                data
-            }.toMutableList()
-        }
-        return dataSet
+        return values
     }
-
 }
