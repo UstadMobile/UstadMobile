9f63c0282aec0bf3d75dfd6dbf0a59081ed1dc4b
samih <samih@ustadmobile.com>
2021-08-31 16:16:52 +0400

    temp torrent client test

---
+++ app-ktor-server/src/jvmMain/kotlin/com/ustadmobile/lib/rest/StartFile.kt
@@ -0,0 +1,49 @@
+package com.ustadmobile.lib.rest
+
+import com.ustadmobile.core.catalog.contenttype.EpubTypePluginCommonJvm
+import com.ustadmobile.core.catalog.contenttype.H5PTypePluginCommonJvm
+import com.ustadmobile.core.contentjob.ProcessContext
+import com.ustadmobile.core.db.UmAppDatabase
+import com.ustadmobile.core.util.DiTag
+import com.ustadmobile.door.DoorUri
+import com.ustadmobile.door.ext.DoorTag
+import com.ustadmobile.lib.db.entities.ContentJobItem
+import io.github.aakira.napier.Napier
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import org.kodein.di.DI
+import org.kodein.di.direct
+import org.kodein.di.instance
+import org.kodein.di.ktor.closestDI
+import org.kodein.di.on
+import java.io.File
+import java.nio.file.Files
+
+fun Route.StartFile(){
+
+    get("/startFile"){
+
+        val di : DI by closestDI()
+        val repo: UmAppDatabase = di.on(call).direct.instance(DoorTag.TAG_REPO)
+        val epubPlugin: H5PTypePluginCommonJvm = di.on(call).direct.instance()
+
+        val epub = File("/home/ustad/server/english.h5p")
+        val tempDir = Files.createTempDirectory("tmp").toFile()
+        val processContext = ProcessContext(DoorUri.parse(tempDir.toURI().toString()),
+                params = mutableMapOf())
+        val metadataResult = epubPlugin.extractMetadata(DoorUri.parse(epub.toURI().toString()),
+                processContext)
+        val containerFolder: File = closestDI().on(context).direct.instance(DiTag.TAG_DEFAULT_CONTAINER_DIR)
+        if(metadataResult != null){
+            val uid = repo.contentEntryDao.insert(metadataResult.entry)
+            val job = ContentJobItem(fromUri = epub.toURI().toString(),
+                    toUri = containerFolder.toURI().toString(),
+                    cjiContentEntryUid = uid)
+            epubPlugin.processJob(job, processContext){
+            }
+            call.respond("OK")
+        }
+    }
+
+}
---
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/torrent/TestTorrentClient.kt
@@ -0,0 +1,89 @@
+package com.ustadmobile.core.torrent
+
+import com.ustadmobile.core.account.Endpoint
+import com.ustadmobile.core.account.UstadAccountManager
+import com.ustadmobile.core.contentjob.ProcessContext
+import com.ustadmobile.core.util.DiTag
+import com.ustadmobile.core.util.UstadTestRule
+import com.ustadmobile.door.DoorUri
+import com.ustadmobile.lib.db.entities.ContentJobItem
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.kodein.di.*
+import java.io.File
+import java.net.InetAddress
+
+class TestTorrentClient {
+
+    private lateinit var localDi: DI
+
+    @JvmField
+    @Rule
+    val temporaryFolder = TemporaryFolder()
+
+
+    @JvmField
+    @Rule
+    var ustadTestRule = UstadTestRule()
+
+
+    @Before
+    fun setup() {
+
+        val clientFolder = temporaryFolder.newFolder("clientFolder")
+        val torrentClientFolder =  File(clientFolder, "torrent")
+        torrentClientFolder.mkdirs()
+
+        val containerClientFolder =  File(clientFolder, "container")
+        containerClientFolder.mkdirs()
+
+        localDi = DI {
+            import(ustadTestRule.diModule)
+            bind<File>(tag = DiTag.TAG_TORRENT_DIR) with scoped(ustadTestRule.endpointScope).singleton {
+                torrentClientFolder
+            }
+            bind<File>(tag = DiTag.TAG_DEFAULT_CONTAINER_DIR) with scoped(ustadTestRule.endpointScope).singleton {
+                containerClientFolder
+            }
+            bind<UstadTorrentManager>() with scoped(ustadTestRule.endpointScope).singleton {
+                UstadTorrentManagerImpl(endpoint = context, di = di)
+            }
+            bind<UstadCommunicationManager>() with singleton {
+                UstadCommunicationManager()
+            }
+            bind<ContainerTorrentDownloadJob>() with scoped(ustadTestRule.endpointScope).singleton {
+                ContainerTorrentDownloadJob(endpoint = context, di = di)
+            }
+            onReady {
+                instance<UstadCommunicationManager>().start(InetAddress.getByName("0.0.0.0"))
+            }
+        }
+
+    }
+
+    @Test
+    fun test(){
+
+        val accountManager: UstadAccountManager by localDi.instance()
+        accountManager.activeEndpoint = Endpoint("http://65.108.52.205:8087/")
+        val containerDownloadJob: ContainerTorrentDownloadJob = localDi.on(accountManager.activeEndpoint).direct.instance()
+        val seedManager: UstadTorrentManager = localDi.on(accountManager.activeEndpoint).direct.instance()
+        GlobalScope.launch {
+            seedManager.start()
+        }
+
+        runBlocking {
+            containerDownloadJob.processJob(ContentJobItem(cjiContainerUid = 225779602304692224),
+                    ProcessContext(DoorUri.parse(""), params = mutableMapOf())){
+            }
+        }
+
+    }
+
+
+}
