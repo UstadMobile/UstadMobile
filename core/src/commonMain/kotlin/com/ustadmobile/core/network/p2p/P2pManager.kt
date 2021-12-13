a83b53454db0a1fbe0b0e82c5467c5b0ae72b8e4
Mike Dawson <mike@ustadmobile.com>
2021-12-13 11:30:02 +0400

    Add initial interface

---
+++ core/src/commonMain/kotlin/com/ustadmobile/core/network/p2p/P2pManager.kt
@@ -0,0 +1,14 @@
+
+interface P2pManager {
+
+    //Check the queue when something is added to watchlist, when a new node is discovered, or
+    //when an executor finishes.
+
+    /**
+     * When a presenter or other component (e.g. ContentJobRunner) wants to monitor the status of
+     * a given cotnainer, it should call this function. Availability information will be stored
+     * in the database and accessed as LiveData or RateLimitedLiveData
+     */
+    fun addToWatchList(contianerUids: List<Long>)
+
+}
