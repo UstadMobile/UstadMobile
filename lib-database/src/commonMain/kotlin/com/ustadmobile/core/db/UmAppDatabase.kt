33c9ddc6993dd7906675cad27ad989b2e62868ee
Mike Dawson <mike@ustadmobile.com>
2021-12-25 20:47:04 +0400

    Add migration for contentjobitem changes on cancelation branch.

--- lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/UmAppDatabase.kt
+++ lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/UmAppDatabase.kt
@@ -5248,6 +5248,19 @@ abstract class UmAppDatabase : DoorDatabase(), SyncableDoorDatabase {
             listOf("ALTER TABLE ContentJobItem ADD COLUMN cjiUploadSessionUid TEXT")
         }
 
+        val MIGRATION_89_90 = DoorMigrationStatementList(89, 90) { db ->
+            if(db.dbType() == DoorDbType.SQLITE) {
+                listOf(
+                    "ALTER TABLE ContentJobItem ADD COLUMN cjiContentDeletedOnCancellation INTEGER NOT NULL DEFAULT 0",
+                    "ALTER TABLE ContentJobItem ADD COLUMN cjiContainerProcessed INTEGER NOT NULL DEFAULT 0")
+            }else {
+                listOf(
+                    "ALTER TABLE ContentJobItem ADD COLUMN cjiContentDeletedOnCancellation BOOL NOT NULL DEFAULT false",
+                    "ALTER TABLE ContentJobItem ADD COLUMN cjiContainerProcessed BOOL NOT NULL DEFAULT false"
+                )
+            }
+        }
+
 
 
 fun migrationList(nodeId: Int) = listOf<DoorMigration>(
@@ -5265,7 +5278,7 @@ fun migrationList(nodeId: Int) = listOf<DoorMigration>(
     MIGRATION_76_77, MIGRATION_77_78, MIGRATION_78_79, MIGRATION_78_79,
     MIGRATION_79_80, MIGRATION_80_81, MIGRATION_81_82, MIGRATION_82_83, MIGRATION_83_84,
     MIGRATION_84_85, MIGRATION_85_86, MIGRATION_86_87, MIGRATION_87_88,
-    MIGRATION_88_89
+    MIGRATION_88_89, MIGRATION_89_90
 )
 
 internal fun migrate67to68(nodeId: Int)= DoorMigrationSync(67, 68) { database ->
