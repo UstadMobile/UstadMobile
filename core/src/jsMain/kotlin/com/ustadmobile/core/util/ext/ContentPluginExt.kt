a40c9f2e6e4923f080a751d378888f51bc169218
kileha3 <lkileha@gmail.com>
2021-12-28 14:06:58 +0300

    Clean up

--- core/src/jsMain/kotlin/com/ustadmobile/core/util/ext/ContentPluginExt.kt
+++ core/src/jsMain/kotlin/com/ustadmobile/core/util/ext/ContentPluginExt.kt
@@ -3,23 +3,21 @@ package com.ustadmobile.core.util.ext
 import com.ustadmobile.core.account.Endpoint
 import com.ustadmobile.core.contentjob.ContentPlugin
 import com.ustadmobile.core.db.UmAppDatabase
-import com.ustadmobile.core.util.DiTag
 import com.ustadmobile.door.ext.DoorTag
 import com.ustadmobile.lib.db.entities.ContainerEntryFile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.withContext
 import org.kodein.di.DI
-import org.kodein.di.direct
 import org.kodein.di.instance
 import org.kodein.di.on
-import java.io.File
 
 actual suspend fun ContentPlugin.withWifiLock(context: Any, block: suspend () -> Unit) {
     block.invoke()
 }
 
 actual suspend fun deleteFilesForContentJob(jobId: Long, di: DI, endpoint: Endpoint): Int{
+    //TODO Finish up this when local files are handled
     val db:UmAppDatabase by di.on(endpoint).instance(tag = DoorTag.TAG_DB)
     val repo: UmAppDatabase by di.on(endpoint).instance(tag = DoorTag.TAG_REPO)
     var numberOfFailedDeletion = 0
@@ -38,9 +36,7 @@ actual suspend fun deleteFilesForContentJob(jobId: Long, di: DI, endpoint: Endpo
                 zombieEntryFilesList = db.containerEntryFileDao.findZombieEntries()
                 zombieEntryFilesList.forEach {
                     val filePath = it.cefPath
-                    if (filePath == null || !File(filePath).delete()) {
-                        numberOfFailedDeletion++
-                    }
+                    //delete file and increment numberOfFailedDeletion
                 }
 
                 db.containerEntryFileDao.deleteListOfEntryFiles(zombieEntryFilesList)
@@ -48,9 +44,7 @@ actual suspend fun deleteFilesForContentJob(jobId: Long, di: DI, endpoint: Endpo
 
             val jobItems = db.contentJobItemDao.findAllByJobId(jobId)
             val job = db.contentJobDao.findByUid(jobId)
-            val defaultContainerDir: File = di.on(endpoint).direct.instance(tag = DiTag.TAG_DEFAULT_CONTAINER_DIR)
-            val containerFolderUri = job?.toUri
-                ?: defaultContainerDir.toURI().toString()
+
             jobItems.forEach {
                 //delete all files under containerFolderUri
             }
