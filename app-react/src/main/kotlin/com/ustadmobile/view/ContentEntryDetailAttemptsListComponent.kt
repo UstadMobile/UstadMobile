a37c4f911a7da627d414edc124fdb0dd566c943d
kileha3 <lkileha@gmail.com>
2022-01-23 03:45:48 +0300

    Add missing screen

---
+++ app-react/src/main/kotlin/com/ustadmobile/view/ContentEntryDetailAttemptsListComponent.kt
@@ -0,0 +1,46 @@
+package com.ustadmobile.view
+
+import com.ustadmobile.core.controller.ContentEntryDetailAttemptsListPresenter
+import com.ustadmobile.core.controller.UstadListPresenter
+import com.ustadmobile.core.view.ContentEntryDetailAttemptsListView
+import com.ustadmobile.lib.db.entities.PersonWithAttemptsSummary
+import com.ustadmobile.util.UmProps
+import com.ustadmobile.view.ext.createPersonWithAttemptProgress
+import react.RBuilder
+
+
+class ContentEntryDetailAttemptsListComponent(props: UmProps): UstadListComponent<PersonWithAttemptsSummary,
+        PersonWithAttemptsSummary>(props), ContentEntryDetailAttemptsListView {
+
+    private var mPresenter: ContentEntryDetailAttemptsListPresenter? = null
+
+    override val listPresenter: UstadListPresenter<*, in PersonWithAttemptsSummary>?
+        get() = mPresenter
+
+    override val displayTypeRepo: Any?
+        get() = dbRepo?.statementDao
+
+    override val viewNames: List<String>
+        get() = listOf(ContentEntryDetailAttemptsListView.VIEW_NAME)
+
+    override fun onCreateView() {
+        super.onCreateView()
+        fabManager?.visible = false
+        mPresenter = ContentEntryDetailAttemptsListPresenter(this, arguments, this, di, this)
+        mPresenter?.onCreate(mapOf())
+    }
+
+    override fun RBuilder.renderListItem(item: PersonWithAttemptsSummary) {
+        createPersonWithAttemptProgress(item, systemImpl)
+    }
+
+    override fun handleClickEntry(entry: PersonWithAttemptsSummary) {
+        mPresenter?.onClickPersonWithStatementDisplay(entry)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mPresenter?.onDestroy()
+        mPresenter = null
+    }
+}
