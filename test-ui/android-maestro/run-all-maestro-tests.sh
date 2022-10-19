526297b9502fb83fcd34b0011be328e33d73ebcc
pooja <pooja@ustadmobile.com>
2022-10-19 16:39:00 +0400

    Maestro UI Test - Tests that admin is able to upload content or not to ustad mobile app

--- test-ui/android-maestro/run-all-maestro-tests.sh
+++ test-ui/android-maestro/run-all-maestro-tests.sh
@@ -3,7 +3,7 @@
 
 #Parse command line arguments as per
 # /usr/share/doc/util-linux/examples/getopt-example.bash
-TEMP=$(getopt -o 's:u:p:r::' --long 'serial1:,username:,password::' -n 'run-all-maestro-tests.sh' -- "$@")
+TEMP=$(getopt -o 's:u:p:e::' --long 'serial1:,username:,password::' -n 'run-all-maestro-tests.sh' -- "$@")
 
 eval set -- "$TEMP"
 unset TEMP
@@ -26,8 +26,8 @@ while true; do
                         shift 2
                        continue
                 ;;
-              '-r'|'--result')
-                    RESULT=$2
+              '-e'|'--endpoint')
+                    ENDPOINT=$2
                       shift 2
                      continue
                ;;
@@ -42,10 +42,17 @@ done
 
 BASEDIR=$(pwd)
 TESTS=$(ls ./tests/*/runtest.sh)
-RESULT = $(ls ./tests/*.png)
+
 echo "Serial1=$SERIAL"
 echo "Username=$USERNAME"
 
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){​3}​[0-9]*).*/\2/p' | head -n 1
+IPADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){​3}​[0-9]*).*/\2/p' | head -n 1)
+if [ "$ENDPOINT" == "" ]; then
+     IPADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1)
+    ENDPOINT="http://$IPADDR:8087/"
+fi
+echo "ENDPOINT=$ENDPOINT"
 for TESTFILE in $TESTS; do
   cd $(dirname $TESTFILE)
     source $TESTFILE
--- test-ui/android-maestro/tests/001_001_admin_can_add_content/runtest.sh
+++ test-ui/android-maestro/tests/001_001_admin_can_add_content/runtest.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 echo "Run admin can add content test"
-
+echo "ENDPOINT=$ENDPOINT"
+echo "Username=$USERNAME"
+echo "PASSWORD=$PASSWORD"
 INDEX=0
-
+ ../../../../runserver.sh --password testpass  --clear --background --nobuild
  for FILENAME in $(ls ../Content/*); do
   FILEBASENAME=$(basename $FILENAME)
   adb push $FILENAME /sdcard/Download/$FILEBASENAME
   CONTENTNAME="TESTCONTENT"$INDEX
-  maestro --platform android test -e USERNAME=$USERNAME -e PASSWORD=$PASSWORD -e TESTFILENAME=$FILEBASENAME -e TESTCONTENTNAME=$CONTENTNAME admin_can_add_content.yaml
+
+  maestro --platform android test -e ENDPOINT=$ENDPOINT -e USERNAME=$USERNAME -e PASSWORD=$PASSWORD -e TESTFILENAME=$FILEBASENAME -e TESTCONTENTNAME=$CONTENTNAME admin_can_add_content.yaml
+
+  TESTRESULT=$?
+  if [ "$TESTRESULT" != "0" ]; then
+     echo "fail" > results/result
+  elif [ ! -f results/result ]; then
+     echo "pass" > results/result
+  fi
+
   INDEX=$((INDEX+1))
  done
-
+  ../../../../runserver.sh --stop
