092ea9e540f83a190e82c57c530e7e0d5b4dd136
pooja <pooja@ustadmobile.com>
2022-10-26 11:32:22 +0400

    Maestro UI Test - Tests that admin is able to upload content or not to ustad mobile app

--- test-ui/android-maestro/tests/002_001_admin_can_create_course/runtest.sh
+++ test-ui/android-maestro/tests/002_001_admin_can_create_course/runtest.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo "Run admin can add content test"
-echo "ENDPOINT=$ENDPOINT"
-echo "Username=$USERNAME"
-echo "PASSWORD=$PASSWORD"
-#INDEX=0
+echo "Run admin can create course test"
 
- #for FILENAME in $(ls ../Content/*); do
-  #FILEBASENAME=$(basename $FILENAME)
-  #adb push $FILENAME /sdcard/Download/$FILEBASENAME
-  #CONTENTNAME="ContentUploadDownloadTest"$INDEX
  ../../../../runserver.sh --password testpass  --clear --background --nobuild
  ../../start-screenrecord.sh emulator-5554 $COURSENAME.mp4
   maestro --platform android test -e ENDPOINT=$ENDPOINT -e USERNAME=admin -e PASSWORD=testpass -e COURSENAME=$COURSENAME admin_can_create_course.yaml
@@ -22,6 +14,5 @@ echo "PASSWORD=$PASSWORD"
      echo "pass" > results/result
   fi
 
-  #INDEX=$((INDEX+1))
- #done
+
 
