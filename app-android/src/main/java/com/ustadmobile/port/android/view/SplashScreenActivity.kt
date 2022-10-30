d09125f2f096669321060cbf9f1ac31eeff22503
ParimSsme <parima.ssme@gmail.com>
2022-10-31 02:04:11 +0430

    fix(SplashScreenActivity): fix starting new activity

--- app-android/src/main/java/com/ustadmobile/port/android/view/SplashScreenActivity.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -13,37 +14,65 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.savedstate.SavedStateRegistryOwner
 import com.example.ustadmobile.ui.ImageCompose
 import com.toughra.ustadmobile.R
+import com.ustadmobile.core.impl.UstadMobileSystemImpl
+import com.ustadmobile.core.impl.nav.SavedStateHandleAdapter
+import com.ustadmobile.core.view.OnBoardingView
+import com.ustadmobile.core.viewmodel.OnBoardingViewModel
 import com.ustadmobile.port.android.ui.compose.TextHeader1
 import com.ustadmobile.port.android.ui.compose.TextBody1
 import com.ustadmobile.port.android.ui.theme.ui.theme.UstadMobileTheme
 import com.ustadmobile.port.android.ui.theme.ui.theme.gray
-import com.ustadmobile.port.android.util.AppPreferences
 import kotlinx.coroutines.delay
+import org.kodein.di.DI
+import org.kodein.di.android.closestDI
+import org.kodein.di.direct
+import org.kodein.di.instance
 
 class SplashScreenActivity : ComponentActivity() {
+
+    val di: DI by closestDI()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launchWhenCreated {
             delay(2000L)
-            startActivity(this@SplashScreenActivity)
+            startActivity(this@SplashScreenActivity, di)
             finish()
         }
         setContent {
             UstadMobileTheme {
-                splashScreen()
+                SplashScreen()
+            }
+        }
+    }
+
+    companion object {
+        fun provideFactory(
+            di: DI,
+            owner: SavedStateRegistryOwner,
+            defaultArgs: Bundle? = null,
+        ): AbstractSavedStateViewModelFactory = object: AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+            override fun <T : ViewModel?> create(
+                key: String,
+                modelClass: Class<T>,
+                handle: SavedStateHandle
+            ): T {
+                return OnBoardingViewModel(di, SavedStateHandleAdapter(handle)) as T
             }
         }
     }
 }
 
 @Composable
-@Preview
-private fun splashScreen(){
+private fun SplashScreen(){
     Column(
         verticalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxSize()
@@ -69,11 +98,22 @@ private fun splashScreen(){
     }
 }
 
-private fun startActivity(context: Context) {
-    val appPreferences = AppPreferences()
-    var intent = Intent(context, OnboardingActivity::class.java)
-    if(appPreferences.getIsLoggedIn(context) == true) {
-        intent = Intent(context, MainActivity::class.java)
+@Composable
+@Preview
+private fun SplashScreenPreview(){
+    SplashScreen()
+}
+
+private fun startActivity(context: Context, di: DI) {
+
+    val systemImpl: UstadMobileSystemImpl = di.direct.instance()
+
+    val activityClass = if(systemImpl.getAppPref(OnBoardingView.PREF_TAG, "false").toBoolean()) {
+        MainActivity::class.java
+    }else {
+        OnboardingActivity::class.java
     }
+
+    var intent = Intent(context, activityClass)
     context.startActivity(intent)
 }
