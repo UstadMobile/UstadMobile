c329eebf6d6e892b1c5a77e755a599d35e860d16
Mike Dawson <mike@ustadmobile.com>
2022-11-02 19:07:18 +0400

    Remove unused changes

--- core/src/commonMain/kotlin/com/ustadmobile/core/shared/OnBoardingModel.kt
+++
@@ -1,8 +0,0 @@
-package com.ustadmobile.core.shared
-
-import kotlinx.coroutines.CoroutineScope
-
-expect abstract class OnBoardingModel() {
-    val viewModelScope: CoroutineScope
-    protected open fun onCleared()
-}
