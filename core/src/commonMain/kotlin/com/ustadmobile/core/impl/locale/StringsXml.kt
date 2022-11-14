f534043dde03501998eab337279ad236e250fe3b
Mike Dawson <mike@ustadmobile.com>
2022-11-14 16:38:02 +0400

    Add mapLookup function for StringsXml

--- core/src/commonMain/kotlin/com/ustadmobile/core/impl/locale/StringsXml.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/impl/locale/StringsXml.kt
@@ -83,6 +83,21 @@ class StringsXml (
     }
 
     /**
+     * This is a simple shorthand to lookup the MessageId from a map using a key. An exception will
+     * be thrown if the given lookupKey is not in the map. This is often used to map constants of
+     * some kind (e.g. filter chip flags, sort flags, etc) to a MessageID that is used in the
+     * user interface
+     *
+     * @param lookupKey the key to lookup in the given map
+     * @param map map of key to message id
+     */
+    fun <K> mapLookup(lookupKey: K, map: Map<K, Int>): String {
+        val messageId = map[lookupKey] ?:
+            throw IllegalArgumentException("StringsXml: mapLookup: key $lookupKey not found in map provided ")
+        return get(messageId)
+    }
+
+    /**
      * If comments were enabled, this function will return the comment
      * for the given message id. This is not needed in normal usage, but
      * might be needed when processing files for translation tools etc.
