63858605dce16c4964bdd7c6e94ebd4016cfd182
ParimSsme <parima.ssme@gmail.com>
2022-11-26 08:40:58 +0430

    "merge div-mvvm-primary into dev-mvvm-contentEntryDetailOverview"

--- app-android/src/main/java/com/ustadmobile/port/android/view/SiteTermsDetailFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/SiteTermsDetailFragment.kt
@@ -2,28 +2,15 @@ package com.ustadmobile.port.android.view
 
 import android.os.Bundle
 import android.view.*
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.composethemeadapter.MdcTheme
 import com.toughra.ustadmobile.R
 import com.toughra.ustadmobile.databinding.FragmentSiteTermsDetailBinding
 import com.ustadmobile.core.controller.UstadDetailPresenter
 import com.ustadmobile.core.controller.SiteTermsDetailPresenter
 import com.ustadmobile.core.util.ext.toStringMap
 import com.ustadmobile.core.view.SiteTermsDetailView
-import com.ustadmobile.core.viewmodel.SiteTermsDetailUiState
 import com.ustadmobile.lib.db.entities.SiteTerms
 import com.ustadmobile.port.android.util.ext.currentBackStackEntrySavedStateMap
-import com.ustadmobile.port.android.view.binding.loadHtmlData
 
 
 class SiteTermsDetailFragment: UstadDetailFragment<SiteTerms>(), SiteTermsDetailView {
@@ -95,47 +82,4 @@ class SiteTermsDetailFragment: UstadDetailFragment<SiteTerms>(), SiteTermsDetail
 
 
 
-}
-
-@Composable
-fun SiteTermsDetailScreen(
-    uiState: SiteTermsDetailUiState = SiteTermsDetailUiState()
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState()),
-    )  {
-
-        AndroidView(factory = {
-            WebView(it).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
-                )
-                webViewClient = WebViewClient()
-                loadHtmlData(uiState.siteTerms?.termsHtml)
-                setTag(R.id.tag_webview_html, uiState.siteTerms?.termsHtml)
-            }},
-            update = {
-                if(uiState.siteTerms?.termsHtml != it.getTag(R.id.tag_webview_html))
-                    it.loadHtmlData(uiState.siteTerms?.termsHtml)
-            }
-        )
-    }
-}
-
-@Composable
-@Preview
-fun SiteTermsDetailScreenPreview() {
-    val uiState = SiteTermsDetailUiState(
-        siteTerms = SiteTerms().apply {
-            termsHtml = "https://www.ustadmobile.com"
-        },
-    )
-
-    MdcTheme {
-        SiteTermsDetailScreen(uiState)
-    }
 }
--- app-react/src/main/kotlin/com/ustadmobile/view/RegisterAgeRedirectScreen.kt
+++ app-react/src/main/kotlin/com/ustadmobile/view/RegisterAgeRedirectScreen.kt
@@ -1,37 +1,19 @@
 package com.ustadmobile.view
 
-import com.ustadmobile.core.generated.locale.MessageID
 import com.ustadmobile.core.hooks.useStringsXml
-import com.ustadmobile.core.viewmodel.RegisterAgeRedirectUiState
-import com.ustadmobile.mui.components.UstadDateEditField
 import csstype.px
-import mui.material.Button
-import mui.material.ButtonVariant
-import mui.material.Typography
-import mui.system.Container
+import mui.material.*
 import mui.system.Stack
 import mui.system.StackDirection
 import mui.system.responsive
 import react.FC
 import react.Props
-import react.useState
 
 external interface RegisterAgeRedirectProps : Props {
-
-    var uiState: RegisterAgeRedirectUiState
-
-    var onSetDate: (Long) -> Unit
-
-    var onClickNext: () -> Unit
-
 }
 
 val RegisterAgeRedirectPreview = FC<Props> {
-    val uiStateVal by useState {
-        RegisterAgeRedirectUiState()
-    }
     RegisterAgeRedirectComponent2 {
-        uiState = uiStateVal
     }
 }
 
@@ -44,24 +26,8 @@ val RegisterAgeRedirectComponent2 = FC<RegisterAgeRedirectProps> { props ->
 
         Stack {
             direction = responsive(StackDirection.column)
-            spacing = responsive(15.px)
+            spacing = responsive(10.px)
 
-            Typography {
-                + strings[MessageID.what_is_your_date_of_birth]
-            }
-
-            UstadDateEditField {
-                timeInMillis = props.uiState.dateOfBirth ?: 0
-                label = strings[MessageID.birthday]
-                onChange = { props.onSetDate(it) }
-            }
-
-            Button {
-                variant = ButtonVariant.contained
-                onClick = { props.onClickNext }
-
-                + strings[MessageID.next].uppercase()
-            }
         }
     }
 }
