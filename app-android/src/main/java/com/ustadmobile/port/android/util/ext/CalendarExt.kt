0407b9427d7fcbe40bf7a4966798b79bb99cfecd
Mike Dawson <mike@ustadmobile.com>
2022-12-01 20:30:37 +0400

    Update Jetpack Compose DateAndTime functions to make timezone id mandatory. Handle timezone correctly on all time calculations.

--- app-android/src/main/java/com/ustadmobile/port/android/util/ext/CalendarExt.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/util/ext/CalendarExt.kt
@@ -6,6 +6,10 @@ const val MS_PER_HOUR = (60 * 60 * 1000)
 
 const val MS_PER_MIN = (60 * 1000)
 
+/**
+ * Shorthand to get the time of day (e.g. time since midnight) in milliseconds from a calendar
+ * instance.
+ */
 val Calendar.timeOfDayInMs: Int
     get() {
         return (get(Calendar.HOUR_OF_DAY) * MS_PER_HOUR) + (get(Calendar.MINUTE) * MS_PER_MIN) +
