f0e1e3b8e3168a01512499cea1935587c50410fd
ParimSsme <parima.ssme@gmail.com>
2022-12-02 17:56:03 +0430

    merge branch dev-mvvm-primary into dev-mvvm-ScheduleEdit

--- app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
+++ app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
@@ -36,6 +36,8 @@ fun useUstadScreens(): UstadScreens {
                 SiteTermsDetailScreenPreview),
             UstadScreen(RegisterMinorWaitForParentView.VIEW_NAME, "RegisterMinorWaitForParent Preview",
                 RegisterMinorWaitForParentPreview),
+            UstadScreen(ScheduleEditView.VIEW_NAME, "ScheduleEdit Preview",
+                ScheduleEditScreenPreview),
         )
     }
 }
