3deb7a4303d45caefe2c6811bd9d917980a4ba88
ParimSsme <parima.ssme@gmail.com>
2022-12-24 02:07:00 +0430

    "remove unrelated changes"

--- lib-database/src/commonMain/kotlin/com/ustadmobile/lib/db/entities/ext/ReportFilterExt.kt
+++
@@ -1,9 +0,0 @@
-package com.ustadmobile.lib.db.entities.ext
-
-import com.ustadmobile.door.annotation.ShallowCopy
-import com.ustadmobile.lib.db.entities.ReportFilter
-
-@ShallowCopy
-expect fun ReportFilter.shallowCopy(
-    block: ReportFilter.() -> Unit,
-): ReportFilter
