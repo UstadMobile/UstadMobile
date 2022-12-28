f8a2304560ba27bb717497a1e3c81038cc6dcc6f
ParimSsme <parima.ssme@gmail.com>
2022-12-28 15:39:26 +0430

    complete ReportFilterEdit screen with jetpack compose and react

--- app-android/src/main/java/com/ustadmobile/port/android/view/ReportFilterEditFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/ReportFilterEditFragment.kt
@@ -7,25 +7,47 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.toughra.ustadmobile.R
 import com.toughra.ustadmobile.databinding.FragmentReportFilterEditBinding
 import com.toughra.ustadmobile.databinding.ItemUidlabelFilterListBinding
 import com.ustadmobile.core.controller.ReportFilterEditPresenter
 import com.ustadmobile.core.controller.UstadEditPresenter
+import com.ustadmobile.core.impl.locale.entityconstants.ConditionConstants
+import com.ustadmobile.core.impl.locale.entityconstants.ContentCompletionStatusConstants
+import com.ustadmobile.core.impl.locale.entityconstants.FieldConstants
 import com.ustadmobile.core.util.IdOption
 import com.ustadmobile.core.util.MessageIdOption
 import com.ustadmobile.core.util.ext.toStringMap
 import com.ustadmobile.core.view.ReportFilterEditView
+import com.ustadmobile.core.viewmodel.ReportFilterEditUiState
 import com.ustadmobile.door.lifecycle.LiveData
-import com.ustadmobile.lib.db.entities.ReportFilter
-import com.ustadmobile.lib.db.entities.UidAndLabel
+import com.ustadmobile.lib.db.entities.*
+import com.ustadmobile.lib.db.entities.ext.shallowCopy
 import com.ustadmobile.port.android.util.ext.currentBackStackEntrySavedStateMap
+import com.ustadmobile.port.android.view.composable.UstadMessageIdOptionExposedDropDownMenuField
+import com.ustadmobile.port.android.view.composable.UstadTextEditField
 
 
 interface ReportFilterEditFragmentEventHandler {
@@ -271,5 +293,174 @@ class ReportFilterEditFragment : UstadEditFragment<ReportFilter>(), ReportFilter
             }
         }
 
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun ReportFilterEditScreen(
+    uiState: ReportFilterEditUiState = ReportFilterEditUiState(),
+    onClickNewItemFilter: () -> Unit = {},
+    onReportFilterChanged: (ReportFilter?) -> Unit = {},
+    onClickEditFilter: (UidAndLabel?) -> Unit = {},
+    onClickRemoveFilter: (UidAndLabel?) -> Unit = {},
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )  {
+
+        item {
+            UstadMessageIdOptionExposedDropDownMenuField(
+                value = uiState.reportFilter?.reportFilterField ?: 0,
+                label = stringResource(R.string.report_filter_edit_field),
+                options = FieldConstants.FIELD_MESSAGE_IDS,
+                error = uiState.fieldError,
+                enabled = uiState.fieldsEnabled,
+                onOptionSelected = {
+                    onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                        reportFilterField = it.value
+                    })
+                },
+            )
+        }
+
+        item {
+            Row {
+                UstadMessageIdOptionExposedDropDownMenuField(
+                    modifier = Modifier.weight(0.5F),
+                    value = uiState.reportFilter?.reportFilterCondition ?: 0,
+                    label = stringResource(R.string.report_filter_edit_condition),
+                    options = ConditionConstants.CONDITION_MESSAGE_IDS,
+                    error = uiState.conditionsError,
+                    enabled = uiState.fieldsEnabled,
+                    onOptionSelected = {
+                        onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                            reportFilterCondition = it.value
+                        })
+                    },
+                )
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                UstadMessageIdOptionExposedDropDownMenuField(
+                    modifier = Modifier.weight(0.5F),
+                    value = uiState.reportFilter?.reportFilterDropDownValue ?: 0,
+                    label = stringResource(R.string.report_filter_edit_values),
+                    options = ContentCompletionStatusConstants.CONTENT_COMPLETION_STATUS_MESSAGE_IDS,
+                    error = uiState.valuesError,
+                    enabled = uiState.fieldsEnabled,
+                    onOptionSelected = {
+                        onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                            reportFilterDropDownValue = it.value
+                        })
+                    },
+                )
+            }
+        }
+
+        item {
+            UstadTextEditField(
+                value = uiState.reportFilter?.reportFilterValue ?: "",
+                label = stringResource(id = R.string.report_filter_edit_values),
+                error = uiState.valuesError,
+                enabled = uiState.fieldsEnabled,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                onValueChange = {
+                    onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                        reportFilterValue = it
+                    })
+                }
+            )
+        }
+
+        item {
+            Row {
+                UstadTextEditField(
+                    modifier = Modifier.weight(0.5F),
+                    value = uiState.reportFilter?.reportFilterValueBetweenX ?: "",
+                    label = stringResource(id = R.string.from),
+                    error = uiState.valuesError,
+                    enabled = uiState.fieldsEnabled,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = {
+                        onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                            reportFilterValueBetweenX = it
+                        })
+                    }
+                )
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                UstadTextEditField(
+                    modifier = Modifier.weight(0.5F),
+                    value = uiState.reportFilter?.reportFilterValueBetweenY ?: "",
+                    label = stringResource(id = R.string.toC),
+                    error = uiState.valuesError,
+                    enabled = uiState.fieldsEnabled,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = {
+                        onReportFilterChanged(uiState.reportFilter?.shallowCopy{
+                            reportFilterValueBetweenY = it
+                        })
+                    }
+                )
+            }
+        }
+
+        items(
+            items = uiState.uidAndLabelList
+        ){ uidAndLabel ->
+            ListItem(
+                text = {
+                    TextButton(onClick = { onClickEditFilter(uidAndLabel)  }) {
+                        Text(uidAndLabel.labelName ?: "")
+                    }
+                },
+                trailing = {
+                    IconButton(onClick = { onClickRemoveFilter(uidAndLabel) }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = stringResource(id = R.string.delete),
+                        )
+                    }
+                }
+            )
+        }
+
+        item {
+            ListItem(
+                modifier = Modifier.clickable {
+                    onClickNewItemFilter()
+                },
+                text = { Text(uiState.createNewFilter) },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = stringResource(id = R.string.add),
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ReportFilterEditScreenPreview() {
+    val uiStateVal = ReportFilterEditUiState(
+        uidAndLabelList = listOf(
+            UidAndLabel().apply {
+                labelName = "First Filter"
+            },
+            UidAndLabel().apply {
+                labelName = "Second Filter"
+            }
+        ),
+        createNewFilter = "Create new filter"
+    )
+    MdcTheme {
+        ReportFilterEditScreen(uiStateVal)
     }
 }
--- app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
+++ app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
@@ -56,6 +56,10 @@ fun useUstadScreens(): UstadScreens {
             UstadScreen("EasySort", "Easy Sort", EasySortPreview),
             UstadScreen(ErrorReportView.VIEW_NAME, name = "ErrorReport Preview", ErrorReportPreview),
             UstadScreen(LanguageEditView.VIEW_NAME, "LanguageEdit Preview", LanguageEditPreview),
+            UstadScreen(ReportFilterEditView.VIEW_NAME,
+                "ReportFilterEdit Preview",
+                ReportFilterEditScreenPreview
+            ),
         )
     }
 }
