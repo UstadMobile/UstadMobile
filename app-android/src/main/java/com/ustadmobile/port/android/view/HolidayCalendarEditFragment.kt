5204de98206b44c29d39cd47557601dec372c11e
Munireh <m.hosseinzada10@gmail.com>
2022-12-28 16:30:47 +0430

    Adding HolidayCalendarEdit page

--- app-android/src/main/java/com/ustadmobile/port/android/view/HolidayCalendarEditFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/HolidayCalendarEditFragment.kt
@@ -4,6 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
@@ -18,10 +33,14 @@ import com.ustadmobile.core.controller.UstadEditPresenter
 import com.ustadmobile.core.util.ext.toBundle
 import com.ustadmobile.core.util.ext.toStringMap
 import com.ustadmobile.core.view.HolidayCalendarEditView
+import com.ustadmobile.core.viewmodel.HolidayCalendarEditUiState
 import com.ustadmobile.door.lifecycle.LiveData
 import com.ustadmobile.lib.db.entities.Holiday
 import com.ustadmobile.lib.db.entities.HolidayCalendar
+import com.ustadmobile.lib.db.entities.ext.shallowCopy
+import com.ustadmobile.port.android.util.compose.rememberFormattedDate
 import com.ustadmobile.port.android.util.ext.currentBackStackEntrySavedStateMap
+import com.ustadmobile.port.android.view.composable.UstadTextEditField
 
 class HolidayCalendarEditFragment() : UstadEditFragment<HolidayCalendar>(), HolidayCalendarEditView{
 
@@ -145,4 +164,93 @@ class HolidayCalendarEditFragment() : UstadEditFragment<HolidayCalendar>(), Holi
     }
 
 
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun HolidayCalendarEditScreen(
+    uiState: HolidayCalendarEditUiState,
+    onHolidayCalendarChange: (HolidayCalendar?) -> Unit = {},
+    onClickAddItem: () -> Unit = {},
+    onDeleteItemClick: (Holiday) -> Unit = {},
+    onItemClick: (Holiday?) -> Unit = {}
+){
+    Column (
+        modifier = Modifier
+            .padding(8.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        UstadTextEditField(
+            value = uiState.holidayCalendar?.umCalendarName ?: "",
+            label = stringResource(id = R.string.name),
+            enabled = uiState.fieldsEnabled,
+            onValueChange = {
+                onHolidayCalendarChange(uiState.holidayCalendar?.shallowCopy {
+                    umCalendarName = it
+                })
+            }
+        )
+
+        ListItem(
+            modifier = Modifier.clickable {
+                onClickAddItem()
+            },
+            icon = {
+                Icon(
+                    Icons.Filled.Add,
+                    contentDescription = null
+                )
+            },
+            text = { Text(stringResource(id = R.string.add_a_holiday)) }
+        )
+
+        uiState.holidayList?.forEach { holiday ->
+
+            val holidayStart = rememberFormattedDate(timeInMillis = holiday.holStartTime, timeZoneId = "UTC")
+            val holidayEnd = rememberFormattedDate(timeInMillis = holiday.holEndTime, timeZoneId = "UTC")
+
+            ListItem(
+                modifier = Modifier
+                    .clickable {
+                        onItemClick(holiday)
+                    },
+                text = { Text(text = holiday.holName ?: "") },
+                secondaryText = {
+                    Text(text = "$holidayStart - $holidayEnd")
+                },
+                trailing = {
+                    IconButton(onClick = {
+                        onDeleteItemClick(holiday)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = stringResource(R.string.delete)
+                        )
+                    }
+                }
+            )
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun HolidayCalendarEditPreview(){
+    HolidayCalendarEditScreen(
+        uiState = HolidayCalendarEditUiState(
+            holidayCalendar = HolidayCalendar().apply {
+                umCalendarName = "my cal"
+            },
+            holidayList = listOf(
+                Holiday().apply {
+                    holName = "Eid"
+                    holStartTime = 1352958816
+                    holEndTime = 1352958818
+                }
+            )
+        )
+    )
 }
---
+++ app-react/src/main/kotlin/com/ustadmobile/view/HolidayCalendarEditScreen.kt
@@ -0,0 +1,123 @@
+package com.ustadmobile.view
+
+import com.ustadmobile.core.generated.locale.MessageID
+import com.ustadmobile.core.hooks.useStringsXml
+import com.ustadmobile.core.viewmodel.HolidayCalendarEditUiState
+import com.ustadmobile.hooks.useFormattedDate
+import com.ustadmobile.lib.db.entities.Holiday
+import com.ustadmobile.lib.db.entities.HolidayCalendar
+import com.ustadmobile.lib.db.entities.ext.shallowCopy
+import com.ustadmobile.mui.components.UstadTextEditField
+import kotlinx.css.px
+import mui.icons.material.Add
+import mui.icons.material.Delete
+import mui.material.*
+import mui.material.styles.TypographyVariant
+import mui.system.responsive
+import react.FC
+import react.Props
+import react.ReactNode
+import react.create
+
+external interface HolidayCalendarEditProps: Props {
+    var uiState: HolidayCalendarEditUiState
+    var onAddItemClick: () -> Unit
+    var onHolidayCalendarChange: (HolidayCalendar?) -> Unit
+    var onDeleteItemClick: (Holiday?) -> Unit
+    var onItemClick: (Holiday) -> Unit
+}
+
+val HolidayCalendarEditComponent2 = FC<HolidayCalendarEditProps> { props ->
+
+    val strings = useStringsXml()
+
+    Container{
+        maxWidth = "lg"
+
+        Stack{
+            direction = responsive(mui.material.StackDirection.column)
+            spacing = responsive(10.px)
+
+            UstadTextEditField {
+                value = props.uiState.holidayCalendar?.umCalendarName ?: ""
+                label = strings[MessageID.name]
+                enabled = props.uiState.fieldsEnabled
+                onChange = {
+                    props.onHolidayCalendarChange(
+                        props.uiState.holidayCalendar?.shallowCopy {
+                            umCalendarName = it
+                        })
+                }
+            }
+
+            ListItem {
+
+                disablePadding = true
+
+                ListItemButton{
+                    ListItemIcon {
+                        Add{}
+                    }
+
+                    ListItemText{
+                        primary = ReactNode((strings[MessageID.add_a_holiday]))
+                    }
+
+                    onClick = {
+                        props.onAddItemClick()
+                    }
+                }
+            }
+
+            List{
+                props.uiState.holidayList?.forEach { holiday ->
+                    ListItem {
+
+                        disablePadding = true
+
+                        val holidayStart = useFormattedDate(holiday.holStartTime, "UTC")
+                        val holidayEnd = useFormattedDate(holiday.holEndTime, "UTC")
+
+                        ListItemButton{
+
+                            onClick = {
+                                props.onItemClick(holiday)
+                            }
+
+                            ListItemText{
+                                primary = ReactNode((holiday.holName ?: ""))
+                                secondary = ReactNode("$holidayStart - $holidayEnd")
+                            }
+                        }
+
+                        secondaryAction = IconButton.create {
+                            onClick = {
+                                props.onDeleteItemClick(holiday)
+                            }
+                            Delete{}
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+
+val HolidayCalendarEditPreview = FC<Props> {
+    HolidayCalendarEditComponent2{
+        uiState = HolidayCalendarEditUiState(
+            holidayCalendar = HolidayCalendar().apply {
+                umCalendarName = "my cal"
+            },
+            holidayList = listOf(
+                Holiday().apply {
+                    holName = "Eid"
+                    holStartTime = 1352958816
+                    holEndTime = 1352958818
+                }
+            )
+        )
+    }
+}
