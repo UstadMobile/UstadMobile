d48ed21b9b3ea0db449c1a9a8f8f3e25d911d03b
Mike Dawson <mike@ustadmobile.com>
2023-01-02 20:57:15 +0400

    Tweaks for UstadListSortHeader . Remove use of UiState (only has two fields). Update sizes.

--- app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadListSortHeader.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadListSortHeader.kt
@@ -2,6 +2,9 @@ package com.ustadmobile.port.android.view.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -9,41 +12,51 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.toughra.ustadmobile.R
 import com.ustadmobile.core.db.dao.ClazzDaoCommon
 import com.ustadmobile.core.generated.locale.MessageID
 import com.ustadmobile.core.util.SortOrderOption
-import com.ustadmobile.core.viewmodel.SortHeaderUiState
 import com.ustadmobile.port.android.util.compose.messageIdResource
 
 @Composable
 fun UstadListSortHeader(
-    uiState: SortHeaderUiState,
+    activeSortOrderOption: SortOrderOption,
+    enabled: Boolean = true,
     onClickSort: () -> Unit = {}
 ){
-
-    val sortIcon = if(uiState.sortOption?.order == true)
-        Icons.Default.ArrowDownward
-    else
-        Icons.Default.ArrowUpward
-
     Surface(
         modifier = Modifier
             .clickable(
-                uiState.fieldsEnabled,
+                enabled = enabled,
                 onClick = { onClickSort() }
             )
     ) {
-        Row {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
 
-            Text(messageIdResource(id = uiState.sortOption?.fieldMessageId
-                ?: MessageID.field_person_age))
+            Text(messageIdResource(id = activeSortOrderOption.fieldMessageId))
+
+            Spacer(Modifier.width(8.dp))
 
             Icon(
-               sortIcon,
-               contentDescription = ""
+                imageVector = if(activeSortOrderOption.order)
+                    Icons.Default.ArrowDownward
+                else
+                    Icons.Default.ArrowUpward,
+                contentDescription = stringResource(if(activeSortOrderOption.order) {
+                    R.string.ascending
+                }else {
+                    R.string.descending
+                }),
+                modifier = Modifier.size(16.dp)
             )
+
         }
     }
 }
@@ -51,12 +64,9 @@ fun UstadListSortHeader(
 @Composable
 @Preview
 private fun UstadListSortHeaderPreview() {
-    val uiState = SortHeaderUiState(
-        sortOption = SortOrderOption(
-            MessageID.name,
-            ClazzDaoCommon.SORT_CLAZZNAME_ASC,
-            true
-        )
-    )
-    UstadListSortHeader(uiState)
+    UstadListSortHeader(SortOrderOption(
+        MessageID.name,
+        ClazzDaoCommon.SORT_CLAZZNAME_ASC,
+        true
+    ))
 }
--- app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadListSortHeader.kt
+++ app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadListSortHeader.kt
@@ -4,19 +4,24 @@ import com.ustadmobile.core.db.dao.ClazzDaoCommon
 import com.ustadmobile.core.generated.locale.MessageID
 import com.ustadmobile.core.hooks.useStringsXml
 import com.ustadmobile.core.util.SortOrderOption
-import com.ustadmobile.core.viewmodel.SortHeaderUiState
+import csstype.JustifyContent
+import csstype.pct
+import csstype.px
 import mui.icons.material.ArrowDownward
 import mui.icons.material.ArrowUpward
 import mui.material.*
+import mui.system.sx
 import react.FC
 import react.Props
 import react.create
 
 external interface UstadListSortHeaderProps : Props {
 
-    var uiState: SortHeaderUiState
+    var activeSortOrderOption: SortOrderOption
 
-    var onClickSort: Unit
+    var enabled: Boolean?
+
+    var onClickSort: () -> Unit
 
 }
 
@@ -24,19 +29,23 @@ val UstadListSortHeader = FC<UstadListSortHeaderProps> { props ->
 
     val strings = useStringsXml()
 
-    val sortIcon = if(props.uiState.sortOption?.order == true)
+    val sortIcon = if(props.activeSortOrderOption.order)
         ArrowDownward.create()
     else
         ArrowUpward.create()
 
     ButtonBase {
-        disabled = !props.uiState.fieldsEnabled
+        sx {
+            justifyContent = JustifyContent.start
+            width = 100.pct
+            padding = 16.px
+        }
+        disabled = props.enabled == false
 
         onClick = { props.onClickSort }
 
         Typography {
-            + strings[props.uiState.sortOption?.fieldMessageId
-                ?: MessageID.field_person_age]
+            + strings[props.activeSortOrderOption.fieldMessageId]
         }
 
         Icon {
@@ -48,12 +57,12 @@ val UstadListSortHeader = FC<UstadListSortHeaderProps> { props ->
 val UstadListSortHeaderPreview = FC<Props> {
 
     UstadListSortHeader {
-        uiState = SortHeaderUiState(
-            sortOption = SortOrderOption(
+        activeSortOrderOption = SortOrderOption(
                 MessageID.name,
                 ClazzDaoCommon.SORT_CLAZZNAME_ASC,
                 true
             )
-        )
+        enabled = true
+        onClickSort = { }
     }
 }
