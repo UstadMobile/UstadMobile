ebdf92171e51164b76e8e416ca0f5751d7fa0442
Munireh <m.hosseinzada10@gmail.com>
2023-01-11 17:49:30 +0430

    Adding Person List page

--- app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
+++ app-react/src/main/kotlin/com/ustadmobile/hooks/useUstadScreens.kt
@@ -83,6 +83,7 @@ fun useUstadScreens(): UstadScreens {
                 ClazzLogEditScreenPreview),
             UstadScreen(CourseGroupSetEditView.VIEW_NAME, "CourseGroupSetEdit Preview",
                 CourseGroupSetEditScreenPreview),
+            UstadScreen(PersonListView.VIEW_NAME, "PersonList Preview", PersonListScreenPreview)
         )
     }
 }
