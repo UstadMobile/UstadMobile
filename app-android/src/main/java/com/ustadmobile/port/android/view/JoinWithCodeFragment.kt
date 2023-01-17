1e1be6d160f0b48fffabe2ed91530e8563742439
ParimSsme <parima.ssme@gmail.com>
2023-01-17 09:45:31 +0430

    create JoinWithCode screen with react and jetpack compose

--- app-android/src/main/java/com/ustadmobile/port/android/view/JoinWithCodeFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/JoinWithCodeFragment.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.toughra.ustadmobile.R
 import com.toughra.ustadmobile.databinding.FragmentJoinWithCodeBinding
