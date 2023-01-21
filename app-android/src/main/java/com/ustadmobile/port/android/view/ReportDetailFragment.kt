3ef2e47f0cf8b1bafe47fe33f8480957ef20fd5b
ParimSsme <parima.ssme@gmail.com>
2023-01-21 21:56:58 +0430

    work on creating ReportDetail screen with jetpack compose

--- app-android/src/main/java/com/ustadmobile/port/android/view/ReportDetailFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/ReportDetailFragment.kt
@@ -8,16 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
@@ -36,11 +29,9 @@ import com.ustadmobile.core.account.UstadAccountManager
 import com.ustadmobile.core.controller.ReportDetailPresenter
 import com.ustadmobile.core.controller.UstadDetailPresenter
 import com.ustadmobile.core.db.UmAppDatabase
-import com.ustadmobile.core.generated.locale.MessageID
 import com.ustadmobile.core.util.ext.ChartData
 import com.ustadmobile.core.util.ext.toStringMap
 import com.ustadmobile.core.view.ReportDetailView
-import com.ustadmobile.core.viewmodel.LoginUiState
 import com.ustadmobile.core.viewmodel.ReportDetailUiState
 import com.ustadmobile.door.ext.DoorTag
 import com.ustadmobile.door.ext.asRepositoryLiveData
@@ -48,10 +39,8 @@ import com.ustadmobile.lib.db.entities.ReportWithSeriesWithFilters
 import com.ustadmobile.lib.db.entities.StatementEntityWithDisplayDetails
 import com.ustadmobile.port.android.util.ext.currentBackStackEntrySavedStateMap
 import com.ustadmobile.port.android.util.ext.defaultScreenPadding
-import com.ustadmobile.port.android.view.composable.UstadTextEditField
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import org.kodein.di.direct
 import org.kodein.di.instance
@@ -366,7 +355,6 @@ class ReportDetailFragment : UstadDetailFragment<ReportWithSeriesWithFilters>(),
 @Composable
 private fun ReportDetailScreen(
     uiState: ReportDetailUiState = ReportDetailUiState(),
-    onClickLogin: () -> Unit = {},
 ) {
     Column(
         modifier = Modifier
