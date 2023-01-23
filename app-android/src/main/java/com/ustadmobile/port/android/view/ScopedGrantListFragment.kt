479ebf24da6656188cba1a24bde04c8d0dbc9eb0
ParimSsme <parima.ssme@gmail.com>
2023-01-23 17:48:53 +0430

    create ScopedGrantList screen with jetpack compose

--- app-android/src/main/java/com/ustadmobile/port/android/view/ScopedGrantListFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/ScopedGrantListFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.ustadmobile.core.controller.ScopedGrantListPresenter
 import com.ustadmobile.core.util.ext.toStringMap
 import com.ustadmobile.core.view.ScopedGrantListView
 import com.ustadmobile.lib.db.entities.ScopedGrant
-import com.ustadmobile.lib.db.entities.ScopedGrantAndName
 import com.ustadmobile.core.controller.UstadListPresenter
 import com.toughra.ustadmobile.R
+import com.ustadmobile.core.viewmodel.ScopedGrantListUiState
 import com.ustadmobile.lib.db.entities.ScopedGrantWithName
+import com.ustadmobile.port.android.util.ext.defaultScreenPadding
 import com.ustadmobile.port.android.view.util.ListHeaderRecyclerViewAdapter
 
 
@@ -52,4 +61,38 @@ class ScopedGrantListFragment(
     override val displayTypeRepo: Any?
         get() = dbRepo?.scopedGrantDao
 
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun ScopedGrantListScreen(
+    uiState: ScopedGrantListUiState = ScopedGrantListUiState(),
+    onClickScopedGrant: (ScopedGrantWithName) -> Unit = {},
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .defaultScreenPadding()
+    )  {
+
+        items(
+            items = uiState.scopedGrantList,
+            key = { scopedGrant -> scopedGrant.sgUid }
+        ){ scopedGrant ->
+            ListItem(
+                modifier = Modifier.clickable {
+                    onClickScopedGrant(scopedGrant)
+                },
+                text = { Text(scopedGrant.name ?: "") },
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ScopedGrantListScreenPreview() {
+    MdcTheme {
+        ScopedGrantListScreen()
+    }
 }
