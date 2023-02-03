bcdd8a9124e309b00cc42d3956f1c6839725656a
poojaustad <78338755+poojaustad@users.noreply.github.com>
2023-02-03 12:01:18 +0400

    Dev test maestro checklist 003 course blocks (#480)
    * MAESTRO CHECKLIST NO _003 Course block editing
    
    * MAESTRO CHECKLIST NO _003 Course block editing
    
    * MAESTRO CHECKLIST NO _003 Course block editing
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added pdf content
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added testserial and testname variables
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added testserial and testname variables
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added testserial and testname variables
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added testserial and testname variables
    
    * MAESTRO CHECKLIST NO _003 Course block editing-added assertion to test 003_003
    
    * MAESTRO CHECKLIST NO _003 Course block editing-removed select option and added content name
    
    * removed id from class content name as text
    
    * if login flow not successful, test will retry the flow on test-001_007
    
    * checklist- 004 - Assignment blocks
    
    * checklist- 003- changes
    
    * checklist- 003- changes
    
    * checklist- 002- changes
    
    * Add one comment to Maestro flow on module collapse.
    
    ---------
    
    Co-authored-by: Mike Dawson <mike@ustadmobile.com>

--- test-end-to-end/android-maestro/common/startserver.js
+++ test-end-to-end/android-maestro/common/startserver.js
@@ -1,3 +1,2 @@
-var response = http.get(CONTROLSERVER +"start?device=" + TESTSERIAL + "&testName=" + TESTNAME
-    + "&adbRecord=true");
+var response = http.get(CONTROLSERVER +"start?device=" + TESTSERIAL + "&testName=" + TESTNAME + "&adbRecord=true");
 output.result = "started"
