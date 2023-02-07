042280029aa16b8b236acb247b0b14eaa0ac28d3
Mike Dawson <mike@ustadmobile.com>
2023-02-07 16:53:39 +0400

    Add functions for duration formatting.

---
+++ app-android/src/main/java/com/ustadmobile/port/android/util/compose/RememberFormattedDuration.kt
@@ -0,0 +1,40 @@
+package com.ustadmobile.port.android.util.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.res.pluralStringResource
+import com.toughra.ustadmobile.R
+import com.ustadmobile.port.android.util.ext.MS_PER_HOUR
+import com.ustadmobile.port.android.util.ext.MS_PER_MIN
+
+/**
+ * Create a human readable string for a duration. E.g. "1 hour 49 minutes"
+ *
+ * @param timeInMillis the duration in milliseconds
+ * @return formatted string e.g. 1 hour 49 minutes
+ */
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun rememberFormattedDuration(
+    timeInMillis: Long,
+): String {
+
+    val hours = (timeInMillis / MS_PER_HOUR)
+    val mins = timeInMillis.mod(MS_PER_HOUR) / MS_PER_MIN
+
+    val hoursStr = pluralStringResource(id = R.plurals.duration_hours, count = hours.toInt())
+    val minsStr = pluralStringResource(id = R.plurals.duration_minutes, count = mins)
+
+    return remember(timeInMillis) {
+        var str = ""
+        if(hours > 0)
+            str += "$hoursStr "
+
+        if(mins > 0)
+            str += minsStr
+
+        str
+    }
+}
+
