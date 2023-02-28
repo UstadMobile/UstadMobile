a47f4a97aa54e1d709d9284c50ae175f47a06592
ParimSsme <parima.ssme@gmail.com>
2023-02-28 14:16:14 +0430

    work on ClazzAssignmentDetailStudentProgress screen with jetpack compose

--- app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadAssignmentFileSubmissionListItem.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadAssignmentFileSubmissionListItem.kt
@@ -2,18 +2,19 @@ package com.ustadmobile.port.android.view.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.Book
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.toughra.ustadmobile.R
 import com.ustadmobile.core.viewmodel.UstadAssignmentFileSubmissionListItemUiState
 import com.ustadmobile.lib.db.entities.*
+import com.ustadmobile.port.android.util.compose.rememberFormattedDateTime
 import java.util.*
 
 
@@ -22,8 +23,14 @@ import java.util.*
 fun UstadAssignmentFileSubmissionListItem(
     modifier: Modifier = Modifier,
     uiState: UstadAssignmentFileSubmissionListItemUiState,
-    onClickOpenSubmission: (CourseAssignmentSubmissionWithAttachment) -> Unit = {}
+    onClickOpenSubmission: (CourseAssignmentSubmissionWithAttachment) -> Unit = {},
+    onClickDeleteSubmission: (CourseAssignmentSubmissionWithAttachment) -> Unit = {}
 ){
+
+    val formattedDateTime = rememberFormattedDateTime(
+        uiState.fileSubmission.casTimestamp,
+        timeZoneId = TimeZone.getDefault().id
+    )
 
     ListItem(
         modifier = modifier
@@ -33,13 +40,27 @@ fun UstadAssignmentFileSubmissionListItem(
 
         icon = {
             Icon(
-                Icons.Default.Book,
+                Icons.Outlined.Book,
                 contentDescription = "",
                 modifier = Modifier.size(70.dp)
             )
         },
         text = { Text(uiState.fileNameText) },
-        secondaryText = {}
+        secondaryText = {
+            if (uiState.isSubmitted){
+               Text("${stringResource(R.string.submitted_cap)}:$formattedDateTime")
+            }
+        },
+        trailing = {
+            IconButton(
+                onClick = { onClickDeleteSubmission(uiState.fileSubmission) }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "",
+                )
+            }
+        }
     )
 }
 
@@ -50,7 +71,8 @@ private fun UstadAssignmentFileSubmissionListItemPreview() {
 
     UstadAssignmentFileSubmissionListItem(
         uiState = UstadAssignmentFileSubmissionListItemUiState(
-
+            fileNameText = "Content Title",
+            isSubmitted = true
         )
     )
 }
