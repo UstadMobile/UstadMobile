cdf83df606898d1e7e8c9e552f5e91bbd7d75da0
ParimSsme <parima.ssme@gmail.com>
2023-03-01 17:14:06 +0430

    UstadAssignmentFileSubmissionHeader.kt: fix design problem

--- app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadAssignmentFileSubmissionHeader.kt
+++ app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadAssignmentFileSubmissionHeader.kt
@@ -12,7 +12,6 @@ import mui.icons.material.Done
 import mui.icons.material.EmojiEvents
 import mui.material.SvgIconProps
 import mui.material.Typography
-import mui.system.Stack
 import react.*
 import react.dom.html.ReactHTML.span
 
@@ -27,50 +26,46 @@ val UstadAssignmentFileSubmissionHeader = FC<UstadAssignmentFileSubmissionHeader
 
     val strings = useStringsXml()
 
-    Stack{
+    var imageId: ReactElement<SvgIconProps>? = null
+    if (props.uiState.submissionStatusIconVisible) {
+        imageId = (ASSIGNMENT_STATUS_MAP[props.uiState.assignmentStatus] ?: Done.create())
+    }
 
-        var imageId: ReactElement<SvgIconProps>? = null
-        if (props.uiState.submissionStatusIconVisible) {
-            imageId = (ASSIGNMENT_STATUS_MAP[props.uiState.assignmentStatus] ?: Done.create())
-        }
+    UstadDetailField {
+        valueText = ReactNode(strings[
+                SubmissionConstants.STATUS_MAP
+                        [props.uiState.assignmentStatus] ?: MessageID.not_submitted_cap])
+        labelText = strings[MessageID.status]
+        icon = imageId
+    }
 
-        UstadDetailField {
-            valueText = ReactNode(strings[
-                    SubmissionConstants.STATUS_MAP
-                            [props.uiState.assignmentStatus] ?: MessageID.not_submitted_cap])
-            labelText = strings[MessageID.status]
-            icon = imageId
-        }
+    val pointsElement = Typography.create {
+        + ("${props.uiState.assignmentMark?.averageScore ?: 0}" +
+                "/${props.uiState.assignment?.block?.cbMaxPoints ?: 0}" +
+                strings[MessageID.points])
 
-        val pointsElement = Typography.create {
-            + ("${props.uiState.assignmentMark?.averageScore ?: 0}" +
-                    "/${props.uiState.assignment?.block?.cbMaxPoints ?: 0}" +
-                    strings[MessageID.points])
+        + " "
 
-            + " "
-
-            if (props.uiState.latePenaltyVisible) {
-                span { style = jso {
-                    color = rgba(255, 0,0, 1.0)
-                }
-                    child(ReactNode(
-                        strings[MessageID.late_penalty]
-                            .replace("%1\$s", (
-                                    props.uiState.assignment?.block?.cbLateSubmissionPenalty ?: 0)
-                                .toString())
-                    ))
-                }
+        if (props.uiState.latePenaltyVisible) {
+            span { style = jso {
+                color = rgba(255, 0,0, 1.0)
+            }
+                child(ReactNode(
+                    strings[MessageID.late_penalty]
+                        .replace("%1\$s", (
+                                props.uiState.assignment?.block?.cbLateSubmissionPenalty ?: 0)
+                            .toString())
+                ))
             }
         }
+    }
 
-        if (props.uiState.showPoints){
-            UstadDetailField{
-                valueText = pointsElement
-                labelText = strings[MessageID.xapi_result_header]
-                icon = EmojiEvents.create()
-            }
+    if (props.uiState.showPoints){
+        UstadDetailField{
+            valueText = pointsElement
+            labelText = strings[MessageID.xapi_result_header]
+            icon = EmojiEvents.create()
         }
-
     }
 }
 
