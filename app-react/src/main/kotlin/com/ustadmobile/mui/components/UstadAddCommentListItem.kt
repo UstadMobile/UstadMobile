daa07eeb78fde02751423df7f4f03727f6dea2cf
Mike Dawson <mike@ustadmobile.com>
2023-03-02 21:58:07 +0400

    Change UstadAddComment for web to use inputtext field.

--- app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadAddCommentListItem.kt
+++ app-react/src/main/kotlin/com/ustadmobile/mui/components/UstadAddCommentListItem.kt
@@ -1,27 +1,39 @@
 package com.ustadmobile.mui.components
 
-import csstype.*
+import com.ustadmobile.core.generated.locale.MessageID
+import com.ustadmobile.core.hooks.useStringsXml
+import js.core.jso
 import mui.icons.material.AccountCircle
+import mui.icons.material.Send
 import mui.material.*
-import mui.system.sx
 import react.FC
 import react.Props
-import react.ReactNode
 import react.create
+import react.dom.aria.ariaLabel
+import react.dom.onChange
+import react.useState
 
 external interface UstadAddCommentListItemProps: Props {
 
     var text: String
 
+    var placeholder: String
+
     var enabled: Boolean?
 
     var personUid: Long?
 
-    var onClickAddComment: () -> Unit
+    var id: String
+
+    var onChange: (String) -> Unit
+
+    var onClickSubmit: () -> Unit
 
 }
 
 val UstadAddCommentListItem = FC<UstadAddCommentListItemProps> { props ->
+
+    val strings = useStringsXml()
 
     ListItem {
 
@@ -29,22 +41,30 @@ val UstadAddCommentListItem = FC<UstadAddCommentListItemProps> { props ->
             + AccountCircle.create()
         }
 
-        ListItemButton{
-
-            disabled = !(props.enabled ?: true)
-            sx {
-                backgroundColor = rgba(0, 0,0, 0.11)
-                borderRadius = 8.px
+        TextField {
+            fullWidth = true
+            id = props.id
+            onChange = {
+                val currentVal = it.target.asDynamic().value
+                props.onChange(currentVal?.toString() ?: "")
             }
-            ListItemText{
-                primary = ReactNode(props.text)
-                sx {
-                    color = rgba(0, 0,0, 0.57)
+            placeholder = props.placeholder
+
+            if(props.text.isNotBlank()) {
+                asDynamic().InputProps = jso<InputBaseProps> {
+                    endAdornment = InputAdornment.create {
+                        position = InputAdornmentPosition.end
+
+                        IconButton {
+                            ariaLabel =  strings[MessageID.submit]
+                            onClick = {
+                                props.onClickSubmit()
+                            }
+
+                            Send { }
+                        }
+                    }
                 }
-            }
-
-            onClick = {
-                props.onClickAddComment()
             }
         }
     }
@@ -52,10 +72,17 @@ val UstadAddCommentListItem = FC<UstadAddCommentListItemProps> { props ->
 
 val UstadAddCommentListItemPreview = FC<Props> {
 
+    var commentText by useState { "" }
+
     UstadAddCommentListItem {
-        text = "Add"
+        text = commentText
         enabled = true
         personUid = 0
-        onClickAddComment = {}
+        onClickSubmit = {}
+        placeholder = "Add a comment"
+        id = "addcomment"
+        onChange = {
+            commentText = it
+        }
     }
 }
