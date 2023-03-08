6d4723427961a85a844eb89d0d22d8ae1f4557fd
ParimSsme <parima.ssme@gmail.com>
2023-03-08 15:00:46 +0430

    ClazzAssignmentEditFragment.kt: fix design problems

--- app-android/src/main/java/com/ustadmobile/port/android/view/ClazzAssignmentEditFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/ClazzAssignmentEditFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.CompoundButton
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -13,6 +15,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +44,6 @@ import com.ustadmobile.port.android.util.ext.defaultScreenPadding
 import com.ustadmobile.port.android.view.binding.isSet
 import com.ustadmobile.port.android.view.composable.*
 import com.ustadmobile.port.android.view.composable.UstadMessageIdOptionExposedDropDownMenuField
-import com.ustadmobile.port.android.view.composable.UstadTextEditField
 
 
 class ClazzAssignmentEditFragment: UstadEditFragment<CourseBlockWithEntity>(), ClazzAssignmentEditView,
@@ -314,6 +318,15 @@ private fun ClazzAssignmentEditScreen(
     onChangedAllowClassComments: (Boolean) -> Unit = {},
     onChangedAllowPrivateCommentsFromStudents: (Boolean) -> Unit = {},
 ) {
+
+    val submissionTypeSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    val isSubmissionTypePressed = submissionTypeSource.collectIsPressedAsState().value
+    LaunchedEffect(isSubmissionTypePressed){
+        if(isSubmissionTypePressed) {
+            onClickSubmissionType()
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -322,10 +335,10 @@ private fun ClazzAssignmentEditScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     )  {
 
-        UstadTextEditField(
+        OutlinedTextField(
             value = uiState.entity?.assignment?.caTitle ?: "",
-            label = stringResource(id = R.string.title),
-            error = uiState.caTitleError,
+            label = { Text(stringResource(id = R.string.title)) },
+            isError = uiState.caTitleError != null,
             enabled = uiState.fieldsEnabled,
             onValueChange = {
                 onChangeCourseBlockWithEntity(
@@ -337,9 +350,10 @@ private fun ClazzAssignmentEditScreen(
             },
         )
 
-        UstadTextEditField(
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = uiState.entity?.assignment?.caDescription ?: "",
-            label = stringResource(id = R.string.description).addOptionalSuffix(),
+            label = { Text(stringResource(id = R.string.description).addOptionalSuffix()) },
             enabled = uiState.fieldsEnabled,
             onValueChange = {
                 onChangeCourseBlockWithEntity(
@@ -356,12 +370,12 @@ private fun ClazzAssignmentEditScreen(
             onCourseBlockChange = onChangeCourseBlock
         )
 
-        UstadTextEditField(
+        OutlinedTextField(
             value = uiState.groupSet?.cgsName ?: "",
-            label = stringResource(id = R.string.submission_type),
+            label = { Text(stringResource(id = R.string.submission_type)) },
             enabled = uiState.groupSetEnabled,
             onValueChange = {},
-            onClick = onClickSubmissionType,
+            interactionSource = submissionTypeSource,
         )
 
         UstadSwitchField(
@@ -387,9 +401,9 @@ private fun ClazzAssignmentEditScreen(
                 },
             )
 
-            UstadTextEditField(
+            OutlinedTextField(
                 value = (uiState.entity?.assignment?.caSizeLimit ?: 0).toString(),
-                label = stringResource(id = R.string.size_limit),
+                label = { Text(stringResource(id = R.string.size_limit)) },
                 enabled = uiState.fieldsEnabled,
                 onValueChange = { newString ->
                     onChangeCourseBlockWithEntity(
@@ -401,9 +415,9 @@ private fun ClazzAssignmentEditScreen(
                 },
             )
 
-            UstadTextEditField(
+            OutlinedTextField(
                 value = (uiState.entity?.assignment?.caNumberOfFiles ?: 0).toString(),
-                label = stringResource(id = R.string.number_of_files),
+                label = { Text(stringResource(id = R.string.number_of_files)) },
                 enabled = uiState.fieldsEnabled,
                 onValueChange = { newString ->
                     onChangeCourseBlockWithEntity(
@@ -441,9 +455,9 @@ private fun ClazzAssignmentEditScreen(
                 },
             )
 
-            UstadTextEditField(
+            OutlinedTextField(
                 value = (uiState.entity?.assignment?.caTextLimit ?: 0).toString(),
-                label = stringResource(id = R.string.maximum),
+                label = { Text(stringResource(id = R.string.maximum)) },
                 enabled = uiState.fieldsEnabled,
                 onValueChange = { newString ->
                     onChangeCourseBlockWithEntity(
