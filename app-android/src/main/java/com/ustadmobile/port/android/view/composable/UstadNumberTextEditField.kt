9053b9393e643b8d21acc3c5b0d576f4838453ce
ParimSsme <parima.ssme@gmail.com>
2023-03-15 08:41:34 +0430

    add UstadNumberTextEditField.kt

--- app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadNumberTextEditField.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadNumberTextEditField.kt
@@ -1,16 +1,18 @@
 package com.ustadmobile.port.android.view.composable
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.ustadmobile.core.util.StringAndSerialNum
 
 @Composable
 fun UstadNumberTextEditField(
@@ -19,36 +21,54 @@ fun UstadNumberTextEditField(
     enabled: Boolean,
     modifier: Modifier = Modifier,
     error: String? = null,
-    errorString: StringAndSerialNum? = null,
-    onClick: () -> Unit,
-    suffixText: String? = null,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    onValueChange: (String) -> Unit,
 ) {
-    var errorText by remember(errorString) {
-        mutableStateOf(errorString?.toString())
+
+    var valueRemember by remember {
+        mutableStateOf(value)
     }
 
-    val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
-    val isPressed = interactionSource.collectIsPressedAsState().value
-    LaunchedEffect(isPressed){
-        if(isPressed) {
-            onClick()
+    var isError by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = { newText ->
+                valueRemember = newText
+                isError = false
+                onValueChange(newText)
+            },
+            label = { Text(text = label) },
+            placeholder = { Text(text = label) },
+            isError = error != null,
+            enabled = enabled,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    // validate here
+                    isError = validateAge(inputText = value)
+                }
+            )
+        )
+
+        if (error != null) {
+            UstadErrorText(error = error)
         }
     }
+}
 
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        label = { Text(label) },
-        value = value,
-        onValueChange = { },
-        isError = errorText != null,
-        enabled = enabled,
-        interactionSource = interactionSource,
-        readOnly = true,
-        keyboardActions = keyboardActions,
-        keyboardOptions = keyboardOptions,
-    )
+private fun validateAge(inputText: String): Boolean {
+    return inputText.toInt() < 18
 }
 
 @Preview
@@ -59,6 +79,6 @@ fun UstadNumberTextEditFieldPreview() {
         label = "Email",
         error =null,
         enabled = true,
-        onClick = {}
+        onValueChange = {}
     )
 }
