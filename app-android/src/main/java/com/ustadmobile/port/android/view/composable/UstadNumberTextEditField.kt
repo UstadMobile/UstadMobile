b0be7132e494bcefa3eee31234dc627b8d3df448
ParimSsme <parima.ssme@gmail.com>
2023-03-15 20:43:08 +0430

    made changes from pull request comments

--- app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadNumberTextEditField.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/composable/UstadNumberTextEditField.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun UstadNumberTextEditField(
@@ -18,56 +16,31 @@ fun UstadNumberTextEditField(
     enabled: Boolean,
     modifier: Modifier = Modifier,
     error: String? = null,
-    suffixText: String? = null,
+    trailingIcon: (() -> Unit)? = null,
     onValueChange: (Int) -> Unit,
 ) {
-
-    var errorText by remember {
-        mutableStateOf(error)
-    }
 
     var strValue: String by remember {
         mutableStateOf(if(value != 0) value.toString() else "")
     }
 
-    Column(
+    OutlinedTextField(
+        value = strValue,
         modifier = modifier,
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        OutlinedTextField(
-            value = strValue,
-            modifier = modifier,
-            onValueChange = { text ->
-                errorText = null
-                strValue = text
-                val intVal = text.filter { it.isDigit() }.toIntOrNull() ?: 0
-                onValueChange(intVal)
-            },
-            label = { Text(text = label) },
-            placeholder = { Text(text = label) },
-            isError = errorText != null,
-            enabled = enabled,
-            trailingIcon = if(suffixText != null) {
-                {
-                    Text(
-                        text = suffixText,
-                        modifier = Modifier.padding(end = 16.dp)
-                    )
-                }
-            }else {
-                null
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-            ),
-        )
-
-        if (errorText != null) {
-            UstadErrorText(error = error ?: "")
-        }
-    }
+        onValueChange = { text ->
+            strValue = text
+            val intVal = text.filter { it.isDigit() }.toIntOrNull() ?: 0
+            onValueChange(intVal)
+        },
+        isError = error != null,
+        label = { Text(text = label) },
+        placeholder = { Text(text = label) },
+        enabled = enabled,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+        ),
+    )
 }
 
 @Preview
@@ -83,6 +56,5 @@ fun UstadNumberTextEditFieldPreview() {
         onValueChange = {
 
         },
-        suffixText = "points"
     )
 }
