8757090eaf92bed33f9c362541a2379e724df908
Mike Dawson <mike@ustadmobile.com>
2023-03-24 17:34:51 +0400

    Update Shrinker - basic skeleton for test

--- core/src/jvmMain/kotlin/com/ustadmobile/core/shrinker/VideoShrinkerJvm.kt
+++ core/src/jvmMain/kotlin/com/ustadmobile/core/shrinker/VideoShrinkerJvm.kt
@@ -4,14 +4,19 @@ import com.ustadmobile.core.shrink.ShrinkConfig
 import com.ustadmobile.core.shrink.ShrinkProgressListener
 import com.ustadmobile.core.shrink.Shrinker
 import com.ustadmobile.door.DoorUri
+import com.ustadmobile.door.ext.toFile
 
 class VideoShrinkerJvm: Shrinker {
     override suspend fun shrink(
         srcUri: DoorUri,
         destinationUri: DoorUri,
         config: ShrinkConfig,
-        progressListener: ShrinkProgressListener?
+        progressListener: ShrinkProgressListener? = null,
     ) {
+        val srcFile = srcUri.toFile()
+        val destFile = srcUri.toFile()
+
+        //TODO here: use ffmpeg to compress
         TODO("Not yet implemented")
     }
 }
--- core/src/jvmTest/kotlin/com/ustadmobile/core/shrink/JvmVideoShrinkerTest.kt
+++ core/src/jvmTest/kotlin/com/ustadmobile/core/shrink/JvmVideoShrinkerTest.kt
@@ -1,11 +1,41 @@
 package com.ustadmobile.core.shrink
 
+import com.ustadmobile.core.shrinker.VideoShrinkerJvm
+import com.ustadmobile.door.ext.toDoorUri
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import kotlin.test.assertTrue
 
 class JvmVideoShrinkerTest {
 
+    @JvmField
+    @Rule
+    val tempFolder = TemporaryFolder()
+
     @Test
     fun givenValidVideo_whenShrinkCalled_thenShouldShrink() {
+        val inTmpFile = tempFolder.newFile()
+        val outTmpFile = tempFolder.newFile()
+
+        //need to put video in test-resources/com/ustadmobile/core/contenttype/
+        inTmpFile.outputStream().use {
+            this::class.java.getResourceAsStream("/com/ustadmobile/core/container/BigBuckBunny.mp4").copyTo(it)
+            it.flush()
+        }
+
+        val videoShrinker = VideoShrinkerJvm()
+
+
+        runBlocking {
+            videoShrinker.shrink(inTmpFile.toDoorUri(), outTmpFile.toDoorUri(), ShrinkConfig())
+        }
+
+        //Assert that theres a valid video file in outTmpFile
+        assertTrue(outTmpFile.exists())
+
 
     }
 
