7edc545afe2db18eda7d9d82e044af6900a14117
ParimSsme <parima.ssme@gmail.com>
2023-03-28 13:42:36 +0430

    work on NetworkNodeList screen with jetpack compose

--- app-android/src/main/java/com/ustadmobile/port/android/view/NetworkNodeListFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/NetworkNodeListFragment.kt
@@ -2,12 +2,19 @@ package com.ustadmobile.port.android.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Message
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.material.composethemeadapter.MdcTheme
+import com.toughra.ustadmobile.R
 import com.ustadmobile.core.viewmodel.NetworkNodeListUiState
 import com.ustadmobile.port.android.util.ext.defaultScreenPadding
+import com.ustadmobile.port.android.view.composable.UstadDetailField
 
 
 @Composable
@@ -20,6 +27,13 @@ private fun NetworkNodeListScreen(
             .defaultScreenPadding()
     ) {
 
+        item {
+            UstadDetailField(
+                valueText = "+12341231",
+                labelText = stringResource(R.string.device),
+                imageId = R.drawable.ic_phone_black_24dp,
+            )
+        }
     }
 }
 
