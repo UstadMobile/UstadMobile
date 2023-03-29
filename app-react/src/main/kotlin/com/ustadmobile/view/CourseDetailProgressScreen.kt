621cc6ca10e5535ba85b66d2d7d37e7856c9bdf0
ParimSsme <parima.ssme@gmail.com>
2023-03-29 13:40:38 +0430

    work on CourseDetailProgress screen with react

--- app-react/src/main/kotlin/com/ustadmobile/view/CourseDetailProgressScreen.kt
+++ app-react/src/main/kotlin/com/ustadmobile/view/CourseDetailProgressScreen.kt
@@ -1,6 +1,12 @@
 package com.ustadmobile.view
 
+import com.ustadmobile.core.generated.locale.MessageID
 import com.ustadmobile.core.hooks.useStringsXml
+import com.ustadmobile.core.viewmodel.CourseDetailProgressUiState
+import com.ustadmobile.core.viewmodel.SchoolDetailOverviewUiState
+import com.ustadmobile.lib.db.entities.Clazz
+import com.ustadmobile.lib.db.entities.Person
+import com.ustadmobile.mui.components.Header
 import csstype.px
 import mui.material.*
 import mui.system.Stack
@@ -8,14 +14,107 @@ import mui.system.StackDirection
 import mui.system.responsive
 import react.FC
 import react.Props
+import react.ReactNode
 
 external interface CourseDetailProgressProps : Props {
+
+    var uiState: CourseDetailProgressUiState
+
+    var onClickStudent: (Person) -> Unit
 
 }
 
 val CourseDetailProgressScreenPreview = FC<Props> {
     CourseDetailProgressScreenComponent2 {
-
+        uiState = CourseDetailProgressUiState(
+            students = listOf(
+                Person().apply {
+                    personUid = 1
+                    firstNames = "Bart"
+                    lastName = "Simpson"
+                },
+                Person().apply {
+                    personUid = 2
+                    firstNames = "Shelly"
+                    lastName = "Mackleberry"
+                },
+                Person().apply {
+                    personUid = 3
+                    firstNames = "Tracy"
+                    lastName = "Mackleberry"
+                },
+                Person().apply {
+                    personUid = 4
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 5
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 6
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 7
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 8
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 9
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 10
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 11
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 12
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 8
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 9
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 10
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 11
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                },
+                Person().apply {
+                    personUid = 12
+                    firstNames = "Nelzon"
+                    lastName = "Muntz"
+                }
+            )
+        )
     }
 }
 
@@ -29,6 +128,42 @@ val CourseDetailProgressScreenComponent2 = FC<CourseDetailProgressProps> { props
         Stack {
             direction = responsive(StackDirection.column)
             spacing = responsive(10.px)
+
+
+            Header {
+
+                Typography {
+                    + strings[MessageID.discussion_board]
+                }
+
+                Typography {
+                    + strings[MessageID.module]
+                }
+
+                Typography {
+                    + strings[MessageID.video]
+                }
+
+                Typography {
+                    + strings[MessageID.clazz_assignment]
+                }
+            }
+
+            List{
+                props.uiState.students.forEach { student ->
+                    ListItem{
+                        ListItemButton {
+                            onClick = {
+                                props.onClickStudent(student)
+                            }
+
+                            ListItemText {
+                                primary = ReactNode(student.fullName())
+                            }
+                        }
+                    }
+                }
+            }
 
         }
     }
