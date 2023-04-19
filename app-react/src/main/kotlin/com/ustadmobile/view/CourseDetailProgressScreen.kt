4f5a4e2ec69aef85d85d53ba81eb5de439b2e939
ParimSsme <parima.ssme@gmail.com>
2023-04-19 15:55:14 +0430

    work on CourseDetailProgress screen

--- app-react/src/main/kotlin/com/ustadmobile/view/CourseDetailProgressScreen.kt
+++ app-react/src/main/kotlin/com/ustadmobile/view/CourseDetailProgressScreen.kt
@@ -5,27 +5,17 @@ import com.ustadmobile.core.hooks.useStringsXml
 import com.ustadmobile.core.paging.ListPagingSource
 import com.ustadmobile.core.viewmodel.CourseDetailProgressUiState
 import com.ustadmobile.core.viewmodel.PersonWithResults
+import com.ustadmobile.core.viewmodel.StudentResult
 import com.ustadmobile.hooks.useMuiAppState
 import com.ustadmobile.hooks.usePagingSource
 import com.ustadmobile.lib.db.entities.CourseBlock
 import com.ustadmobile.lib.db.entities.Person
+import com.ustadmobile.mui.common.justifyContent
 import com.ustadmobile.view.components.UstadPersonAvatar
 import com.ustadmobile.view.components.virtuallist.VirtualList
 import com.ustadmobile.view.components.virtuallist.VirtualListOutlet
 import com.ustadmobile.view.components.virtuallist.virtualListContent
-import csstype.Overflow
-import csstype.pct
-import csstype.rotate
-import csstype.deg
-import csstype.px
-import csstype.Color
-import csstype.unaryMinus
-import csstype.Contain
-import csstype.translatex
-import csstype.Position
-import csstype.Height
-import csstype.Display
-import csstype.AlignItems
+import csstype.*
 import js.core.jso
 import mui.icons.material.CheckBoxOutlined
 import mui.icons.material.Message
@@ -53,7 +43,32 @@ val CourseDetailProgressScreenPreview = FC<Props> { props ->
         uiState = CourseDetailProgressUiState(
             students = { ListPagingSource(listOf(
                 PersonWithResults(
-                    results = listOf(),
+                    results = listOf(
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = false
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        )
+                    ),
                     person = Person().apply {
                         personUid = 1
                         firstNames = "Bart"
@@ -61,7 +76,32 @@ val CourseDetailProgressScreenPreview = FC<Props> { props ->
                     }
                 ),
                 PersonWithResults(
-                    results = listOf(),
+                    results = listOf(
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = false
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = false
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = false
+                        )
+                    ),
                     person = Person().apply {
                         personUid = 2
                         firstNames = "Shelly"
@@ -69,7 +109,32 @@ val CourseDetailProgressScreenPreview = FC<Props> { props ->
                     }
                 ),
                 PersonWithResults(
-                    results = listOf(),
+                    results = listOf(
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        )
+                    ),
                     person = Person().apply {
                         personUid = 3
                         firstNames = "Tracy"
@@ -77,7 +142,32 @@ val CourseDetailProgressScreenPreview = FC<Props> { props ->
                     }
                 ),
                 PersonWithResults(
-                    results = listOf(),
+                    results = listOf(
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = false
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        ),
+                        StudentResult(
+                            personUid = 0,
+                            courseBlockUid = 0,
+                            clazzUid = 0,
+                            completed = true
+                        )
+                    ),
                     person = Person().apply {
                         personUid = 4
                         firstNames = "Nelzon"
@@ -152,6 +242,18 @@ val CourseDetailProgressScreenPreview = FC<Props> { props ->
             courseBlocks = listOf(
                 CourseBlock().apply {
                     cbTitle = strings[MessageID.discussion_board]
+                },
+                CourseBlock().apply {
+                    cbTitle = strings[MessageID.module]
+                },
+                CourseBlock().apply {
+                    cbTitle = strings[MessageID.video]
+                },
+                CourseBlock().apply {
+                    cbTitle = strings[MessageID.audio]
+                },
+                CourseBlock().apply {
+                    cbTitle = strings[MessageID.document]
                 }
             )
         )
@@ -197,6 +299,7 @@ val CourseDetailProgressScreenComponent2 = FC<CourseDetailProgressProps> { props
 
                 Stack.create {
                     direction = responsive(StackDirection.row)
+                    justifyContent = JustifyContent.spaceBetween
 
                     Stack {
                         direction = responsive(StackDirection.row)
@@ -224,19 +327,22 @@ val CourseDetailProgressScreenComponent2 = FC<CourseDetailProgressProps> { props
                         }
                     }
 
-                    (0..60).forEachIndexed { index, item ->
-                        Icon {
+                    Stack {
+                        direction = responsive(StackDirection.row)
+                        person?.results?.forEachIndexed { index, item ->
+                            Icon {
 
-                            sx {
-                                width = 60.px
-                                if (scrollX > index){
+                                sx {
+                                    width = 60.px
+                                    if (scrollX > index){
 //                                        position = Position.absolute
 //                                        marginLeft = -(44*index).px
-                                    color = Color("#006400")
+                                        color = Color("#006400")
+                                    }
                                 }
-                            }
 
-                            + CheckBoxOutlined.create()
+                                + CheckBoxOutlined.create()
+                            }
                         }
                     }
 
@@ -259,6 +365,7 @@ val CourseDetailProgressScreenComponent2 = FC<CourseDetailProgressProps> { props
             Container {
                 Stack {
                     direction = responsive(StackDirection.row)
+                    justifyContent = JustifyContent.spaceBetween
 
                     sx {
                         width = 100.pct
@@ -275,22 +382,27 @@ val CourseDetailProgressScreenComponent2 = FC<CourseDetailProgressProps> { props
                         }
                     }
 
-            props.uiState.courseBlocks.forEachIndexed { index, item ->
+                    Stack {
+                        direction = responsive(StackDirection.row)
 
-                Typography {
-                    sx {
-                        width = 44.px
-                        transform = rotate(270.deg)
+                        props.uiState.courseBlocks.forEachIndexed { index, item ->
+
+                            Typography {
+                                sx {
+                                    width = 600.px
+                                    transform = rotate(270.deg)
+                                    textOverflow = TextOverflow.ellipsis
 //                        if (scrollX > index){
 //                            position = Position.absolute
 //                            marginLeft = -(44*index).px
 //                            color = Color("000000")
 //                        }
-                    }
+                                }
 
-                    + item
-                }
-            }
+                                + item.cbTitle
+                            }
+                        }
+                    }
                 }
             }
 
