6112f620237a7b66756e30858d874eb1d8699a12
ParimSsme <parima.ssme@gmail.com>
2023-04-24 16:23:41 +0430

    work on CourseDetailProgress screen

--- app-android/src/main/java/com/ustadmobile/port/android/view/CourseDetailProgressFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/CourseDetailProgressFragment.kt
@@ -1,7 +1,18 @@
 package com.ustadmobile.port.android.view
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.paging.compose.items
 import androidx.compose.material.ExperimentalMaterialApi
