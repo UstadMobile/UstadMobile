cfe935e27b4f58bc46ea8d321fddf9c179aaf829
ParimSsme <parima.ssme@gmail.com>
2023-05-15 16:52:54 +0430

    work on QR code scanner

--- app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
