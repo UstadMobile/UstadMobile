85578bf4ddd0b726774a492c188d8fce371a6604
ParimSsme <parima.ssme@gmail.com>
2023-05-21 13:07:52 +0430

    work on QR code scanner

--- app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
@@ -17,12 +18,16 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -48,11 +53,18 @@ import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.themeadapter.material.MdcTheme
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.ustadmobile.core.viewmodel.QRCodeScannerViewModel
+import com.ustadmobile.port.android.view.bar.BarcodeImageAnalyzer
+import com.ustadmobile.port.android.view.bar.BarcodeResultBoundaryAnalyzer
 import com.ustadmobile.port.android.view.bar.BarcodeScannerScreen
+import com.ustadmobile.port.android.view.bar.BarcodeScanningDecorationLayout
+import com.ustadmobile.port.android.view.bar.handleBarcodeResults
+import com.ustadmobile.port.android.view.bar.model.BarCodeResult
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -84,6 +96,103 @@ class QRCodeScannerFragment: UstadBaseMvvmFragment(){
 private fun QRCodeScannerScreen(viewModel: QRCodeScannerViewModel) {
 
     BarcodeScannerScreen()
+}
+
+@Composable
+@androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
+fun BarcodeScannerScreen() {
+
+    val barcodeResultBoundaryAnalyzer = BarcodeResultBoundaryAnalyzer()
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var preview by remember { mutableStateOf<androidx.camera.core.Preview?>(null) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val width = remember { mutableStateOf(0) }
+        val height = remember { mutableStateOf(0) }
+        AndroidView(
+            factory = { AndroidViewContext ->
+                PreviewView(AndroidViewContext).apply {
+                    this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                    )
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }
+            },
+            modifier = Modifier.fillMaxSize().onGloballyPositioned { layoutCoordinates ->
+                width.value = layoutCoordinates.size.width
+                height.value = layoutCoordinates.size.height
+                barcodeResultBoundaryAnalyzer.onCameraBoundaryReady(
+                    RectF(
+                        0F,
+                        0F,
+                        width.value.toFloat(),
+                        height.value.toFloat()
+                    )
+                )
+            },
+            update = { previewView ->
+                val cameraSelector: CameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+                val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+                val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                    ProcessCameraProvider.getInstance(context)
+
+                cameraProviderFuture.addListener({
+                    preview = androidx.camera.core.Preview.Builder().build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+                    val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                    val barcodeAnalyser = BarcodeImageAnalyzer()
+                    barcodeAnalyser.setProcessListener(
+                        listener = object : BarcodeImageAnalyzer.ProcessListenerAdapter() {
+                            override fun onSucceed(results: List<Barcode>, inputImage: InputImage) {
+                                super.onSucceed(results, inputImage)
+                                if (results.isNotEmpty()){
+                                    handleBarcodeResults(results, inputImage, barcodeResultBoundaryAnalyzer)
+                                }
+
+                            }
+                        }
+                    )
+                    val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build()
+                        .also {
+                            it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                        }
+
+                    try {
+                        cameraProvider.unbindAll()
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            preview,
+                            imageAnalysis
+                        )
+                    } catch (e: Exception) {
+                        Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
+                    }
+                }, ContextCompat.getMainExecutor(context))
+            }
+        )
+
+        BarcodeScanningDecorationLayout(
+            width = width.value,
+            height = height.value,
+            onScanningAreaReady = {
+                barcodeResultBoundaryAnalyzer.onBarcodeScanningAreaReady(it)
+            },
+            scanningResult = BarCodeResult()
+        )
+    }
 }
 
 
