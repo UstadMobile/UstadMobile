d235ca48f4fa69d295b13cb6500a71b37d5a0f0b
ParimSsme <parima.ssme@gmail.com>
2023-05-21 13:15:16 +0430

    work on QR code scanner

--- app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
@@ -1,8 +1,5 @@
 package com.ustadmobile.port.android.view
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.graphics.PointF
 import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
@@ -10,34 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview as CameraPreview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetState
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathOperation
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
@@ -46,28 +23,20 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.themeadapter.material.MdcTheme
 import com.google.common.util.concurrent.ListenableFuture
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.ustadmobile.core.viewmodel.QRCodeScannerViewModel
 import com.ustadmobile.port.android.view.bar.BarcodeImageAnalyzer
 import com.ustadmobile.port.android.view.bar.BarcodeResultBoundaryAnalyzer
-import com.ustadmobile.port.android.view.bar.BarcodeScannerScreen
 import com.ustadmobile.port.android.view.bar.BarcodeScanningDecorationLayout
-import com.ustadmobile.port.android.view.bar.handleBarcodeResults
 import com.ustadmobile.port.android.view.bar.model.BarCodeResult
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 class QRCodeScannerFragment: UstadBaseMvvmFragment(){
 
@@ -95,12 +64,16 @@ class QRCodeScannerFragment: UstadBaseMvvmFragment(){
 @androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
 private fun QRCodeScannerScreen(viewModel: QRCodeScannerViewModel) {
 
-    BarcodeScannerScreen()
+    BarcodeScannerScreen(
+        onQRCodeDetected = viewModel::onQRCodeDetected
+    )
 }
 
 @Composable
 @androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
-fun BarcodeScannerScreen() {
+fun BarcodeScannerScreen(
+    onQRCodeDetected: (String) -> Unit = {}
+) {
 
     val barcodeResultBoundaryAnalyzer = BarcodeResultBoundaryAnalyzer()
 
@@ -156,7 +129,10 @@ fun BarcodeScannerScreen() {
                             override fun onSucceed(results: List<Barcode>, inputImage: InputImage) {
                                 super.onSucceed(results, inputImage)
                                 if (results.isNotEmpty()){
-                                    handleBarcodeResults(results, inputImage, barcodeResultBoundaryAnalyzer)
+                                    val scanningResult = barcodeResultBoundaryAnalyzer.analyze(results, inputImage)
+                                    if (scanningResult.message.equals("PerfectMatch")) {
+                                        scanningResult.barCode?.rawValue?.let { onQRCodeDetected(it) }
+                                    }
                                 }
 
                             }
