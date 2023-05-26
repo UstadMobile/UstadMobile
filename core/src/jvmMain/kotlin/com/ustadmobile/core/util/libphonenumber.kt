359f84064ec498ccb220664dfb42aea9943e1263
ParimSsme <parima.ssme@gmail.com>
2023-05-26 18:32:41 +0430

    add libphonenumber library in android

--- core/src/jvmMain/kotlin/com/ustadmobile/core/util/libphonenumber.kt
+++ core/src/jvmMain/kotlin/com/ustadmobile/core/util/libphonenumber.kt
@@ -1,8 +1,16 @@
 package com.ustadmobile.core.util
 
+import com.google.i18n.phonenumbers.NumberParseException
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 
 
 actual fun isValidPhoneNumber(str: String): Boolean {
+    val phoneUtil = PhoneNumberUtil.getInstance()
+    try {
+        val swissNumberProto = phoneUtil.parse(str, "CH")
+        return phoneUtil.isValidNumber(swissNumberProto)
+    } catch (e: NumberParseException) {
+        System.err.println("NumberParseException was thrown: $e")
+    }
     return false
 }
