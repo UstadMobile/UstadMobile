904f5d0e86b1dc35d15c1ec99fea0f5e464857ff
ParimSsme <parima.ssme@gmail.com>
2023-06-19 10:33:26 +0430

    add international phone number validation for react

--- core/src/jsMain/kotlin/com/ustadmobile/core/util/libphonenumber.kt
+++ core/src/jsMain/kotlin/com/ustadmobile/core/util/libphonenumber.kt
@@ -3,19 +3,12 @@ package com.ustadmobile.core.util
 import org.kodein.di.DI
 
 
-@JsModule("libphonenumber-js")
-@JsNonModule
-external fun parsePhoneNumber(number: String, countryCode: String): dynamic
-
-@JsModule("libphonenumber-js")
-@JsNonModule
-external fun isValidNumber(number: String): Boolean
-
 actual fun isValidPhoneNumber(di: DI, str: String): Boolean {
 
-    val phoneNumber = parsePhoneNumber(str, "CH")
-    if (!(phoneNumber as? String).isNullOrBlank()) {
-        return phoneNumber.isValid() == true
+    val regex = "^\\+(?:[0-9]●?){6,14}[0-9]\$"
+
+    if (str.toRegex().matches(regex)) {
+        return true
     }
     return false
 }
