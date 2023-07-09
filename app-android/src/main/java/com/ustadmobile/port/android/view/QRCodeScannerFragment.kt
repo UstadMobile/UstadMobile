0822a5ff68550b25c3797595f9132725579b2670
ParimSsme <parima.ssme@gmail.com>
2023-07-09 21:40:56 +0430

    scan barcode in android

--- app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/QRCodeScannerFragment.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
@@ -23,8 +15,6 @@ import com.google.accompanist.themeadapter.material.MdcTheme
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import com.ustadmobile.core.viewmodel.QRCodeScannerViewModel
-import com.ustadmobile.port.android.util.ext.defaultItemPadding
-import com.ustadmobile.port.android.util.ext.defaultScreenPadding
 
 class QRCodeScannerFragment : UstadBaseMvvmFragment() {
 
@@ -67,38 +57,17 @@ private fun BarcodeScannerScreen(
     onQRCodeDetected: (String) -> Unit = {}
 ) {
 
-    val context = LocalContext.current
-
     val barcodeLauncher:ActivityResultLauncher<ScanOptions> = rememberLauncherForActivityResult(
         ScanContract()
     ) { result ->
-        if(result.contents == null) {
-            Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_LONG).show();
+        if(result.contents != null) {
+            onQRCodeDetected(result.contents)
         }
     }
 
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .defaultScreenPadding(),
-    ) {
-
-        item {
-            TextButton(
-                modifier = Modifier
-                    .defaultItemPadding(),
-                onClick = {
-                    barcodeLauncher.launch(ScanOptions())
-                }
-            ) {
-                Text("Scan Barcode")
-            }
-        }
+    LaunchedEffect(Unit) {
+        barcodeLauncher.launch(ScanOptions())
     }
-
 }
 
 
