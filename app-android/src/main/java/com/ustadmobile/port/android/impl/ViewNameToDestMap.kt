1cd3879484c2f7119b85b9d9c4fcb2681154c92f
ParimSsme <parima.ssme@gmail.com>
2023-08-02 16:52:12 +0430

    remove QRCodeScanner fragment and viewModel

--- app-android/src/main/java/com/ustadmobile/port/android/impl/ViewNameToDestMap.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/impl/ViewNameToDestMap.kt
@@ -9,7 +9,6 @@ import com.ustadmobile.core.view.*
 import com.ustadmobile.core.viewmodel.courseblock.edit.CourseBlockEditViewModel
 import com.ustadmobile.core.viewmodel.HtmlEditViewModel
 import com.ustadmobile.core.viewmodel.timezone.TimeZoneListViewModel
-import com.ustadmobile.core.viewmodel.QRCodeScannerViewModel
 import com.ustadmobile.port.android.view.PanicButtonSettingsView
 import com.ustadmobile.core.viewmodel.clazz.detail.ClazzDetailViewModel
 import com.ustadmobile.core.viewmodel.clazzassignment.submitterdetail.ClazzAssignmentSubmitterDetailViewModel
@@ -190,8 +189,7 @@ class ViewNameToDestMap: DestinationProvider {
                 actionBarScrollBehavior = SCROLL_FLAG_NO_SCROLL),
             HtmlEditViewModel.DEST_NAME to UstadDestination(R.id.html_edit_dest,
                     actionBarScrollBehavior = SCROLL_FLAG_NO_SCROLL),
-            TimeZoneListViewModel.DEST_NAME to UstadDestination(R.id.time_zone_list_dest),
-            QRCodeScannerViewModel.DEST_NAME to UstadDestination(R.id.qr_code_scan_dest)
+            TimeZoneListViewModel.DEST_NAME to UstadDestination(R.id.time_zone_list_dest)
     )
 
     override val navControllerViewId: Int
