1c61f2f037f8cf84bcbc1593c62e660e0488cdc3
ParimSsme <parima.ssme@gmail.com>
2023-08-05 11:57:59 +0430

    add LeavingReasonList screen in jetpack compose

---
+++ app-android/src/main/java/com/ustadmobile/port/android/view/leavingreason/list/LeavingReasonListFragment.kt
@@ -0,0 +1,132 @@
+package com.ustadmobile.port.android.view.leavingreason.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import com.google.accompanist.themeadapter.material.MdcTheme
+import com.ustadmobile.core.viewmodel.leavingreason.list.LeavingReasonListUiState
+import com.ustadmobile.core.viewmodel.leavingreason.list.LeavingReasonListViewModel
+import com.ustadmobile.lib.db.entities.LeavingReason
+import com.ustadmobile.port.android.util.ext.defaultScreenPadding
+import com.ustadmobile.port.android.view.UstadBaseMvvmFragment
+
+class LeavingReasonListFragment() : UstadBaseMvvmFragment() {
+
+    private val viewModel: LeavingReasonListViewModel by ustadViewModels(::LeavingReasonListViewModel)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewLifecycleOwner.lifecycleScope.launchNavigatorCollector(viewModel)
+        viewLifecycleOwner.lifecycleScope.launchAppUiStateCollector(viewModel)
+
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                MdcTheme {
+                    LeavingReasonListScreen(viewModel)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun LeavingReasonListScreen(
+    viewModel: LeavingReasonListViewModel
+) {
+    val uiState by viewModel.uiState.collectAsState(LeavingReasonListUiState())
+
+    LeavingReasonListScreen(
+        uiState = uiState
+    )
+}
+
+@Composable
+fun LeavingReasonListScreen(
+    uiState: LeavingReasonListUiState = LeavingReasonListUiState(),
+    onClickLeavingReason: (LeavingReason) -> Unit = {},
+) {
+
+    val leavingReasonListPager = remember(uiState.leavingReasonList) {
+        Pager(
+            pagingSourceFactory = uiState.leavingReasonList,
+            config = PagingConfig(pageSize = 20, enablePlaceholders = true)
+        )
+    }
+
+    val leavingReasonListItems = leavingReasonListPager.flow.collectAsLazyPagingItems()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .defaultScreenPadding()
+    ) {
+
+
+        items(
+            items = leavingReasonListItems,
+            key = { it.leavingReasonUid }
+        ){ pendingStudent ->
+            LeavingReasonListItem(
+                leavingReason = pendingStudent,
+                onClick = onClickLeavingReason
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun LeavingReasonListItem(
+    leavingReason: LeavingReason?,
+    onClick: (LeavingReason) -> Unit,
+){
+
+    ListItem (
+        modifier = Modifier.clickable {
+            leavingReason?.also(onClick)
+        },
+        text = {
+            Text(text = leavingReason?.leavingReasonTitle ?: "")
+        }
+    )
+}
+
+@Composable
+@Preview
+fun LeavingReasonListScreenPreview() {
+    val uiStateVal = LeavingReasonListUiState()
+
+    MdcTheme {
+        LeavingReasonListScreen(
+            uiState = uiStateVal
+        )
+    }
+}
--- core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/leavingreason/list/LeavingReasonListViewModel.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/leavingreason/list/LeavingReasonListViewModel.kt
@@ -1,9 +1,43 @@
 package com.ustadmobile.core.viewmodel.leavingreason.list
 
+import com.ustadmobile.core.impl.nav.UstadSavedStateHandle
+import com.ustadmobile.core.viewmodel.ListPagingSourceFactory
+import com.ustadmobile.core.viewmodel.UstadListViewModel
+import com.ustadmobile.core.viewmodel.person.list.EmptyPagingSource
 import com.ustadmobile.lib.db.entities.ClazzEnrolmentWithLeavingReason
+import com.ustadmobile.lib.db.entities.LeavingReason
+import com.ustadmobile.lib.db.entities.PersonWithClazzEnrolmentDetails
+import org.kodein.di.DI
 
 data class LeavingReasonListUiState(
 
-    val leavingReasonList: List<ClazzEnrolmentWithLeavingReason> = emptyList(),
+    val leavingReasonList: ListPagingSourceFactory<LeavingReason> = { EmptyPagingSource() },
 
 )
+
+class LeavingReasonListViewModel(
+    di: DI,
+    savedStateHandle: UstadSavedStateHandle,
+) : UstadListViewModel<LeavingReasonListUiState>(
+    di, savedStateHandle, LeavingReasonListUiState(), DEST_NAME
+) {
+
+    init {
+
+    }
+
+    companion object {
+
+        const val DEST_NAME = "LeavingReasonList"
+
+    }
+
+    override fun onUpdateSearchResult(searchText: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickAdd() {
+        TODO("Not yet implemented")
+    }
+
+}
