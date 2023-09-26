dfeb53e9aafe72ee1c70b55f7ea49110a0647d5c
ParimSsme <parima.ssme@gmail.com>
2023-09-26 12:37:06 +0430

    remove unused imports from TimeZoneListFragment

--- app-android/src/main/java/com/ustadmobile/port/android/view/timezone/TimeZoneListFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/timezone/TimeZoneListFragment.kt
@@ -2,32 +2,13 @@ package com.ustadmobile.port.android.view.timezone
 
 import android.os.Bundle
 import android.view.*
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.themeadapter.material.MdcTheme
-import com.ustadmobile.core.util.ext.formattedString
 import com.ustadmobile.core.viewmodel.timezone.TimeZoneListViewModel
-import com.ustadmobile.core.viewmodel.timezone.TimezoneListUiState
 import com.ustadmobile.libuicompose.view.timezone.TimeZoneListScreenForViewModel
 import com.ustadmobile.port.android.view.UstadBaseMvvmFragment
-import kotlinx.datetime.Clock
-import java.util.*
-import kotlinx.datetime.TimeZone as TimeZoneKt
 
 class TimeZoneListFragment : UstadBaseMvvmFragment() {
 
