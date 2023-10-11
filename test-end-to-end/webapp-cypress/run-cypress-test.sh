1eb4a5132578cfb82918d196590dcc2e5c9fe2fd
pooja <pooja@ustadmobile.com>
2023-10-11 16:39:56 +0400

    Shell script added

--- test-end-to-end/webapp-cypress/run-cypress-test.sh
+++ test-end-to-end/webapp-cypress/run-cypress-test.sh
@@ -2,7 +2,7 @@
 
 #Parse command line arguments as per
 # /usr/share/doc/util-linux/examples/getopt-example.bash
-TEMP=$(getopt -o 's:u:p:e:c:r' --long 'serial1:,username:,password:,endpoint:,console-output,spec:' -n 'run-cypress-test.sh' -- "$@")
+TEMP=$(getopt -o 'u:p:e:r:' --long 'username:,password:,endpoint:,spec:' -n 'run-cypress-test.sh' -- "$@")
 
 eval set -- "$TEMP"
 unset TEMP
@@ -11,18 +11,11 @@ TESTUSER="admin"
 TESTPASS="testpass"
 WORKDIR=$(pwd)
 SCRIPTDIR=$(realpath $(dirname $0))
-CONTROLSERVER=""
-USECONSOLEOUTPUT=0
 SPEC=""
 
 while true; do
         case "$1" in
-             '-s'|'--serial1')
-                	TESTSERIAL=$2
-                        shift 2
-                        continue
-                ;;
-             '-u'|'--username')
+            '-u'|'--username')
                 	TESTUSER=$2
                         shift 2
                         continue
@@ -37,13 +30,7 @@ while true; do
                       shift 2
                      continue
                ;;
-              '-c'|'--console-output')
-                     echo "Use console output"
-                     USECONSOLEOUTPUT=1
-                     shift 1
-                     continue
-                ;;
-              '-r'|'--spec')
+             '-r'|'--spec')
                       echo "Set spec to $2"
                       SPEC=$2
                       shift 2
@@ -57,40 +44,9 @@ while true; do
 	esac
 done
 
-IPADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1)
-if [ "$ENDPOINT" = "" ]; then
-    ENDPOINT="http://$IPADDR:8087/"
-fi
-
-if [ "$CONTROLSERVER" = "" ]; then
-  CONTROLSERVER="http://localhost:8075/"
-fi
-
-if [ ! -e $SCRIPTDIR/results ]; then
-  mkdir $SCRIPTDIR/results
-fi
-
-# Start control server
-$SCRIPTDIR/../../testserver-controller/start.sh
-
-SPECARG=$SPEC
-if [ "$SPEC" != "" ]; then
-  SPECARG="$SCRIPTDIR/cypress/e2e/$SPEC.cy.js"
-else
-  SPECARG="$SCRIPTDIR/cypress/e2e/"
-fi
-
-OUTPUTARGS=" --format junit --output $SCRIPTDIR/results/report.xml "
-if [ "$USECONSOLEOUTPUT" == "1" ]; then
-  OUTPUTARGS=""
-fi
 
 cd $SCRIPTDIR
  cypress run cypress run
-#--reporter junit \
---reporter-options "mochaFile=results/my-test-output.xml,toConsole=true" \
-#--spec $SPECARG
-
-$SCRIPTDIR/../../testserver-controller/stop.sh
+ --reporter-options "mochaFile=results/my-test-output.xml,toConsole=true" \
 
 
