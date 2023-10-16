2291585cffbb5d4efbb5176c19e5bf2b9cacab6d
ParimSsme <parima.ssme@gmail.com>
2023-10-16 15:12:02 +0430

    remove preview and unused imports from PersonEditFragment

--- app-android/src/main/java/com/ustadmobile/port/android/view/person/edit/PersonEditFragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/person/edit/PersonEditFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.themeadapter.material.MdcTheme
-import com.ustadmobile.core.viewmodel.person.edit.PersonEditUiState
 import com.ustadmobile.core.viewmodel.person.edit.PersonEditViewModel
 import com.ustadmobile.libuicompose.view.person.edit.PersonEditScreenForViewModel
 import com.ustadmobile.port.android.view.UstadBaseMvvmFragment
@@ -41,11 +38,4 @@ class PersonEditFragment: UstadBaseMvvmFragment() {
     }
 
 
-}
-
-@Preview
-@Composable
-private fun PersonEditPreview() {
-    val uiState = PersonEditUiState()
-//    PersonEditScreen(uiState)
 }
