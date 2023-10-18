b5930ca65baff038daa6947aa6a3f2fcf2929cae
Mike Dawson <mike@ustadmobile.com>
2023-10-18 13:08:42 +0400

    Add multiplatform paging dependencies to libuicompose.

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/helloworld/HelloWorld.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/helloworld/HelloWorld.kt
@@ -3,6 +3,7 @@ package com.ustadmobile.libuicompose.helloworld
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,8 +12,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.paging.PagingConfig
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import app.cash.paging.Pager
+import app.cash.paging.PagingSource
 import dev.icerock.moko.resources.compose.stringResource
 import com.ustadmobile.core.MR
+import app.cash.paging.compose.*
 
 @Composable
 fun HelloWorld() {
@@ -31,6 +38,32 @@ fun HelloWorld() {
                 count.value = 0
             }) {
             Text("Reset")
+        }
+    }
+}
+
+
+@Composable
+fun HelloPaging() {
+    lateinit var pagingSourceFactory: () -> PagingSource<Int, String>
+
+    val pager = remember {
+        Pager(
+            config = PagingConfig(pageSize = 20, enablePlaceholders = true, maxSize = 200),
+            pagingSourceFactory = pagingSourceFactory,
+        )
+    }
+
+    val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
+
+    LazyColumn(Modifier.fillMaxSize()) {
+        items(
+            count = lazyPagingItems.itemCount,
+            key = lazyPagingItems.itemKey { it.hashCode() },
+            contentType = lazyPagingItems.itemContentType()
+        ) { index ->
+            val item = lazyPagingItems[index]
+            Text(item ?: "")
         }
     }
 }
