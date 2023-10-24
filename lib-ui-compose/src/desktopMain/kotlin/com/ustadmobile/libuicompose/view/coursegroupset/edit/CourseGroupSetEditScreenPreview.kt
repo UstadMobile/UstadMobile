a56bbc510725750e20749f81f6cfbd55b9121163
ParimSsme <parima.ssme@gmail.com>
2023-10-24 14:49:52 +0430

    move CourseGroupSetEditScreen to lib-ui-compose

---
+++ lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/coursegroupset/edit/CourseGroupSetEditScreenPreview.kt
@@ -0,0 +1,43 @@
+package com.ustadmobile.libuicompose.view.coursegroupset.edit
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+
+@Composable
+@Preview
+fun CourseGroupSetEditScreenPreview(){
+//    CourseGroupSetEditScreen(
+//        uiState = CourseGroupSetEditUiState(
+//            courseGroupSet = CourseGroupSet().apply {
+//                cgsName = "ttl"
+//                cgsTotalGroups = 6
+//            },
+//            membersList = listOf(
+//                CourseGroupMemberAndName(
+//                    cgm = CourseGroupMember().apply {
+//                        cgmGroupNumber = 1
+//                    },
+//                    name = "Bart Simpson"
+//                ),
+//                CourseGroupMemberAndName(
+//                    cgm = CourseGroupMember().apply {
+//                        cgmGroupNumber = 2
+//                    },
+//                    name = "Shelly Mackleberry"
+//                ),
+//                CourseGroupMemberAndName(
+//                    cgm = CourseGroupMember().apply {
+//                        cgmGroupNumber = 2
+//                    },
+//                    name = "Tracy Mackleberry"
+//                ),
+//                CourseGroupMemberAndName(
+//                    cgm = CourseGroupMember().apply {
+//                        cgmGroupNumber = 1
+//                    },
+//                    name = "Nelzon Muntz"
+//                )
+//            )
+//        )
+//    )
+}
