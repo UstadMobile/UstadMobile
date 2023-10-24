3efed553e9e52c00113285bb2413904c5b704a6a
ParimSsme <parima.ssme@gmail.com>
2023-10-24 13:43:29 +0430

    remove unused imports from ContentEntryEdit2Fragment

--- app-android/src/main/java/com/ustadmobile/port/android/view/contententry/edit/ContentEntryEdit2Fragment.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/view/contententry/edit/ContentEntryEdit2Fragment.kt
@@ -4,35 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.themeadapter.material.MdcTheme
-import com.toughra.ustadmobile.R
-import com.ustadmobile.core.contentjob.MetadataResult
-import com.ustadmobile.core.impl.ContainerStorageDir
-import com.ustadmobile.core.impl.locale.entityconstants.LicenceConstants
-import com.ustadmobile.core.viewmodel.contententry.edit.ContentEntryEditUiState
 import com.ustadmobile.core.viewmodel.contententry.edit.ContentEntryEditViewModel
-import com.ustadmobile.core.viewmodel.courseblock.edit.CourseBlockEditUiState
-import com.ustadmobile.lib.db.entities.*
-import com.ustadmobile.lib.db.entities.ext.shallowCopy
 import com.ustadmobile.libuicompose.view.contententry.edit.ContentEntryEditScreenForViewModel
 import com.ustadmobile.port.android.view.UstadBaseMvvmFragment
-import com.ustadmobile.port.android.view.composable.*
-import com.ustadmobile.core.R as CR
 
 
 interface ContentEntryEdit2FragmentEventHandler {
