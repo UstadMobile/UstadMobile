c87e9fa5d4f3c5a4cd77e29ac0088dd08da307c5
ParimSsme <parima.ssme@gmail.com>
2023-11-03 18:30:47 +0430

    move UstadAssignmentSubmissionHeader to lib-ui-compose

--- lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/clazzassignment/UstadAssignmentSubmissionHeaderPreview.kt
+++ lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/clazzassignment/UstadAssignmentSubmissionHeaderPreview.kt
@@ -6,12 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.*
 import com.ustadmobile.core.viewmodel.clazzassignment.UstadAssignmentSubmissionHeaderUiState
 import com.ustadmobile.lib.db.entities.CourseAssignmentSubmission
+import com.ustadmobile.lib.db.entities.AverageCourseAssignmentMark
+
 @Composable
 @Preview
 private fun UstadAssignmentSubmissionHeaderPreview() {
     UstadAssignmentSubmissionHeader(
         uiState = UstadAssignmentSubmissionHeaderUiState(
-            assignmentStatus = CourseAssignmentSubmission.NOT_SUBMITTED
+            assignmentStatus = CourseAssignmentSubmission.NOT_SUBMITTED,
+            assignmentMark = AverageCourseAssignmentMark().apply {
+                averagePenalty = 3
+            },
         )
     )
 }
