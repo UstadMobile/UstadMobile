c1c9904331a19f53b19f08021710dce3c51fcfe8
ParimSsme <parima.ssme@gmail.com>
2023-11-11 17:55:30 +0430

    add UstadContentEntryListItem preview in lib-ui-compose

--- lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/contententry/UstadContentEntryListItemPreview.kt
+++ lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/contententry/UstadContentEntryListItemPreview.kt
@@ -1,12 +1,13 @@
 package com.ustadmobile.libuicompose.view.contententry
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.paging.*
-import com.toughra.ustadmobile.databinding.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
 import com.ustadmobile.lib.db.entities.*
+import com.ustadmobile.lib.db.entities.StatementEntity.Companion.RESULT_SUCCESS
 
 @Composable
 @Preview
