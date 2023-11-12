c9111f9c321cc6439c9faeb05ce88800e6cf2e5c
ParimSsme <parima.ssme@gmail.com>
2023-11-12 09:20:07 +0430

    move UstadEditHeader to lib-ui-compose

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/components/UstadEditHeader.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/components/UstadEditHeader.kt
@@ -3,12 +3,12 @@ package com.ustadmobile.libuicompose.components
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.ustadmobile.libuicompose.util.ext.defaultItemPadding
 
 @Composable
 fun UstadEditHeader(
     text: String,
     modifier: Modifier = Modifier,
 ){
-                                   //  TODO error
-//    Text(text, modifier = modifier.defaultItemPadding())
+    Text(text, modifier = modifier.defaultItemPadding())
 }
