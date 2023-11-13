36b9b5f0726c86f77404336e2182431257ce423a
ParimSsme <parima.ssme@gmail.com>
2023-11-13 09:52:58 +0430

    update ScheduleEditScreen in lib-ui-compose

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/schedule/edit/ScheduleEditScreen.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/schedule/edit/ScheduleEditScreen.kt
@@ -3,12 +3,8 @@ package com.ustadmobile.libuicompose.view.schedule.edit
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
@@ -17,7 +13,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.ustadmobile.core.impl.locale.entityconstants.ScheduleConstants
 import com.ustadmobile.core.viewmodel.schedule.edit.ScheduleEditUiState
 import com.ustadmobile.core.viewmodel.schedule.edit.ScheduleEditViewModel
