9768bbbc76ed24855bc7ea5963a1d672152f52e2
ParimSsme <parima.ssme@gmail.com>
2023-11-28 12:29:57 +0430

    update UstadAddCommentListItem.kt in lib-ui-compose

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/components/UstadAddCommentListItem.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/components/UstadAddCommentListItem.kt
@@ -1,51 +1,121 @@
 package com.ustadmobile.libuicompose.components
 
-import  androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.ustadmobile.core.MR
+import dev.icerock.moko.resources.compose.stringResource
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UstadAddCommentListItem(
-    text: String,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    personUid: Long,
-    commentInBottomSheet: Boolean = false, //Will be true on Android, false on desktop
-    onCommentTextChanged: (String) -> Unit = { },
-    onClickAddComment: () -> Unit = {  },
+    commentText: String,
+    commentLabel: String,
+    onCommentChanged: (String) -> Unit = { },
+    currentUserPersonUid: Long,
+    onSubmitComment: () -> Unit = {  },
+    editCommentInBottomSheet: Boolean = !isDesktop(), //Will be true on Android, false on desktop
 ){
-    //If using bottom, show in bottom sheet modal, otherwise, use "normal" outlined text field.
+
+    var bottomSheetExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    if(editCommentInBottomSheet && bottomSheetExpanded) {
+        ModalBottomSheet(
+            onDismissRequest= {
+                bottomSheetExpanded = false
+            }
+        ) {
+            ListItem(
+                modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                leadingContent = {
+                    UstadPersonAvatar(
+                        personUid = currentUserPersonUid,
+                        modifier = Modifier.size(40.dp)
+                    )
+                },
+                headlineContent = {
+                    OutlinedTextField(
+                        value = commentText,
+                        onValueChange = {
+                            onCommentChanged(it)
+                         },
+                        modifier = Modifier.fillMaxWidth(),
+                        label = { Text(commentLabel) },
+                        enabled = enabled,
+                    )
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = {
+                            onSubmitComment()
+                            bottomSheetExpanded = false
+                        },
+                        enabled = enabled && commentText.isNotEmpty(),
+                    ) {
+                        Icon(
+                            Icons.Filled.Send,
+                            contentDescription = stringResource(MR.strings.send)
+                        )
+                    }
+                }
+            )
+        }
+    }
 
     ListItem(
         modifier = modifier,
-        icon = {
-//            UstadPersonAvatar(
-//                personUid = personUid,
-//                modifier = Modifier.size(40.dp)
-//            )
+        leadingContent = {
+            UstadPersonAvatar(
+                personUid = currentUserPersonUid,
+                modifier = Modifier.size(40.dp)
+            )
         },
-        text = {
-            TextButton(
-                onClick = onClickAddComment,
-                modifier = Modifier.fillMaxWidth(),
-                border = BorderStroke(0.dp, Color.Transparent),
-                enabled = enabled,
-//                colors = ButtonDefaults.buttonColors(
-//                    backgroundColor = colorResource(id = org.wordpress.aztec.R.color.grey_a_40),
-//                )
+        headlineContent = {
+            OutlinedTextField(
+                value = commentText,
+                onValueChange = {
+                    onCommentChanged(it)
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .clickable(
+                        onClick = {
+                            if (editCommentInBottomSheet) bottomSheetExpanded = !bottomSheetExpanded
+                        }),
+                label = { Text(commentLabel) },
+                enabled = enabled && !editCommentInBottomSheet,
+            )
+        },
+        trailingContent = {
+            if(!editCommentInBottomSheet) IconButton(
+                onClick = {
+                    onSubmitComment()
+                },
+                enabled = enabled && commentText.isNotEmpty(),
             ) {
-                Text(
-                    text = text,
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier.fillMaxWidth(),
-//                    color = contentColorFor(
-//                        colorResource(id = org.wordpress.aztec.R.color.grey_a_40))
+                Icon(
+                    Icons.Filled.Send,
+                    contentDescription = stringResource(MR.strings.send)
                 )
             }
         }
