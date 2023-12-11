51dfe9426ca2b637133070673c57a94aa7db03e1
ParimSsme <parima.ssme@gmail.com>
2023-12-11 08:38:00 +0430

    work on ConversationListScreen design

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/message/conversationlist/ConversationListScreen.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/message/conversationlist/ConversationListScreen.kt
@@ -1,26 +1,45 @@
 package com.ustadmobile.libuicompose.view.message.conversationlist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import app.cash.paging.Pager
 import app.cash.paging.PagingConfig
 import com.ustadmobile.core.viewmodel.message.conversationlist.ConversationListUiState
 import com.ustadmobile.core.viewmodel.message.conversationlist.ConversationListViewModel
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import com.ustadmobile.libuicompose.components.ustadPagedItems
 import com.ustadmobile.libuicompose.util.ext.defaultItemPadding
 import com.ustadmobile.libuicompose.util.ext.defaultScreenPadding
-import com.ustadmobile.libuicompose.components.ustadPagedItems
+
+
+val PurpleGrey80 = Color(0xFFCCC2DC)
 
 @Composable
 fun ConversationListScreen(
@@ -28,17 +47,17 @@ fun ConversationListScreen(
 ) {
     val uiState: ConversationListUiState by viewModel.uiState.collectAsState(ConversationListUiState())
 
-    MessageDetailScreen(
+    ConversationListScreen(
         uiState = uiState,
     )
 }
 
 @Composable
-fun MessageDetailScreen(
+fun ConversationListScreen(
     uiState: ConversationListUiState,
 ){
 
-    val pager = remember(uiState.conversations) {
+    val pager = remember(10) {
         Pager(
             config = PagingConfig(pageSize = 20, enablePlaceholders = true, maxSize = 200),
             pagingSourceFactory = uiState.conversations
@@ -60,25 +79,94 @@ fun MessageDetailScreen(
             pagingItems = lazyPagingItems,
             key = { it.message?.messageUid ?: 0 },
         ) {  message ->
-//            ListItem(
-//                headlineContent = { Text(text = "${message?.message?.messageText}") },
-//                leadingContent = {
-//                    UstadPersonAvatar(
-//                        message?.senderPerson?.personUid ?: 0,
-//                    )
-//                },
-//            )
-        }
+//            ChatItem(item)
         }
 
-        Row (
-            modifier = Modifier.defaultItemPadding()
-        ){
-            IconButton(
-                onClick = {},
-                content = { Icon(Icons.Default.Add, contentDescription = null) }
+            item {
+                ChatItem("Hi Mary, How are you?", true)
+            }
+
+            item {
+                ChatItem("Hi Branch, good. You?", false)
+            }
+        }
+
+        ChatBox(
+            {  },
+            modifier = Modifier
+                .fillMaxWidth()
+
+        )
+    }
+
+}
+
+@Composable
+fun ChatItem(message: String, isFromMe: Boolean) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(4.dp)) {
+        Box(
+            modifier = Modifier
+                .align(if (isFromMe) Alignment.End else Alignment.Start)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 48f,
+                        topEnd = 48f,
+                        bottomStart = if (isFromMe) 48f else 0f,
+                        bottomEnd = if (isFromMe) 0f else 48f
+                    )
+                )
+                .background(PurpleGrey80)
+                .padding(16.dp)
+        ) {
+            Text(text = message)
+        }
+    }
+}
+
+@Composable
+fun ChatBox(
+    onSendChatClickListener: (String) -> Unit,
+    modifier: Modifier
+) {
+    var chatBoxValue by remember { mutableStateOf(TextFieldValue("")) }
+    Row(modifier = modifier.padding(16.dp)) {
+        TextField(
+            value = chatBoxValue,
+            onValueChange = { newText ->
+                chatBoxValue = newText
+            },
+            modifier = Modifier
+                .weight(1f)
+                .padding(4.dp),
+            shape = RoundedCornerShape(24.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            placeholder = {
+                Text(text = "Type something")
+            }
+        )
+        IconButton(
+            onClick = {
+                val msg = chatBoxValue.text
+                if (msg.isBlank()) return@IconButton
+                onSendChatClickListener(chatBoxValue.text)
+                chatBoxValue = TextFieldValue("")
+            },
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(color = PurpleGrey80)
+                .align(Alignment.CenterVertically)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Send,
+                contentDescription = "Send",
+                modifier = Modifier.fillMaxSize().padding(8.dp)
             )
         }
     }
-
 }
