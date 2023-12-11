f83ef1847f9458a59bf95385b615293bde84ee86
ParimSsme <parima.ssme@gmail.com>
2023-12-11 10:31:04 +0430

    remove unused imports from MessageListViewModel

--- core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/message/messagelist/MessageListViewModel.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/message/messagelist/MessageListViewModel.kt
@@ -2,46 +2,19 @@ package com.ustadmobile.core.viewmodel.message.messagelist
 
 import app.cash.paging.PagingSource
 import com.ustadmobile.core.MR
-import com.ustadmobile.core.impl.appstate.ActionBarButtonUiState
-import com.ustadmobile.core.impl.appstate.LoadingUiState
 import com.ustadmobile.core.impl.nav.UstadSavedStateHandle
 import com.ustadmobile.core.util.SortOrderOption
-import com.ustadmobile.core.util.UMFileUtil
-import com.ustadmobile.core.util.ext.replaceOrAppend
 import com.ustadmobile.core.view.UstadView
 import com.ustadmobile.core.viewmodel.UstadListViewModel
-import com.ustadmobile.core.viewmodel.clazz.edit.ClazzEditViewModel
 import com.ustadmobile.core.viewmodel.message.conversationlist.ConversationListViewModel
-import com.ustadmobile.core.viewmodel.person.PersonViewModelConstants
-import com.ustadmobile.core.viewmodel.person.detail.PersonDetailViewModel
-import com.ustadmobile.core.viewmodel.person.edit.PersonEditViewModel
 import com.ustadmobile.core.viewmodel.person.list.EmptyPagingSource
 import com.ustadmobile.core.viewmodel.person.list.PersonListViewModel
-import com.ustadmobile.core.viewmodel.schedule.edit.ScheduleEditViewModel
-import com.ustadmobile.door.ext.doorPrimaryKeyManager
-import com.ustadmobile.door.util.systemTimeInMillis
-import com.ustadmobile.lib.db.composites.ContentEntryBlockLanguageAndContentJob
-import com.ustadmobile.lib.db.composites.CourseBlockAndEditEntities
 import com.ustadmobile.lib.db.composites.MessageAndSenderPerson
 import com.ustadmobile.lib.db.entities.ChatWithLatestMessageAndCount
-import com.ustadmobile.lib.db.entities.Clazz
-import com.ustadmobile.lib.db.entities.ClazzWithHolidayCalendarAndSchoolAndTerminology
-import com.ustadmobile.lib.db.entities.CourseBlock
-import com.ustadmobile.lib.db.entities.CourseTerminology
 import com.ustadmobile.lib.db.entities.Person
 import com.ustadmobile.lib.db.entities.Role
-import com.ustadmobile.lib.db.entities.Schedule
-import com.ustadmobile.lib.db.entities.ext.shallowCopy
-import com.ustadmobile.lib.util.getDefaultTimeZoneId
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.encodeToString
 import org.kodein.di.DI
 
 data class MessageListUiState(
