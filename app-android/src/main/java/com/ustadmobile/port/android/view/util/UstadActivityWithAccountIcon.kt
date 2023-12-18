7232937fdb4775309aae3105d6123806c6f4e385
Mike Dawson <mike@ustadmobile.com>
2023-12-18 17:55:17 +0400

    Delete obsolete (ViewBinding) Android code

--- app-android/src/main/java/com/ustadmobile/port/android/view/util/UstadActivityWithAccountIcon.kt
+++
@@ -1,7 +0,0 @@
-package com.ustadmobile.port.android.view.util
-
-interface UstadActivityWithAccountIcon {
-
-    var accountIconVisible: Boolean?
-
-}
--- app-android/src/main/java/com/ustadmobile/port/android/view/util/UstadActivityWithBottomNavigation.kt
+++
@@ -1,9 +0,0 @@
-package com.ustadmobile.port.android.view.util
-
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-interface UstadActivityWithBottomNavigation {
-
-    val bottomNavigationView: BottomNavigationView
-
-}
