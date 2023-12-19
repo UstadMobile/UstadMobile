cad8831972f122048480058b368ef373bbf8ed16
ParimSsme <parima.ssme@gmail.com>
2023-12-19 09:23:31 +0430

    work on MessageLisScreen in app-react

--- app-react/src/jsMain/kotlin/com/ustadmobile/view/message/list/MessageListScreen.kt
+++ app-react/src/jsMain/kotlin/com/ustadmobile/view/message/list/MessageListScreen.kt
@@ -1,5 +1,6 @@
 package com.ustadmobile.view.message.list
 
+import com.ustadmobile.core.MR
 import com.ustadmobile.core.hooks.collectAsState
 import com.ustadmobile.core.hooks.useStringProvider
 import com.ustadmobile.hooks.useUstadViewModel
@@ -8,6 +9,11 @@ import com.ustadmobile.core.viewmodel.message.messagelist.MessageListViewModel
 import com.ustadmobile.hooks.useMuiAppState
 import com.ustadmobile.hooks.usePagingSource
 import com.ustadmobile.lib.db.entities.Message
+import com.ustadmobile.mui.components.ThemeContext
+import com.ustadmobile.mui.components.UstadLinkify
+import com.ustadmobile.mui.components.UstadTextField
+import com.ustadmobile.util.ext.onTextChange
+import com.ustadmobile.view.components.virtuallist.VirtualListOutlet
 import com.ustadmobile.view.components.virtuallist.VirtualList
 import com.ustadmobile.view.components.virtuallist.virtualListContent
 import web.cssom.Contain
@@ -15,14 +21,21 @@ import web.cssom.Height
 import web.cssom.Overflow
 import web.cssom.pct
 import js.core.jso
+import kotlinx.css.px
+import mui.icons.material.Send
 import mui.material.*
+import mui.system.responsive
 import mui.system.sx
 import react.FC
 import react.Props
+import react.ReactNode
 import react.create
+import react.dom.aria.ariaLabel
+import react.useRequiredContext
 import react.useState
 import web.cssom.BackgroundClip
 import web.cssom.Display
+import web.cssom.ShapeOutside
 
 
 external interface MessageListScreenProps : Props {
@@ -37,8 +50,6 @@ external interface MessageListScreenProps : Props {
 
 
 private val MessageListScreenComponent2 = FC<MessageListScreenProps> { props ->
-
-    val strings = useStringProvider()
 
     val infiniteQueryResult = usePagingSource(
         props.uiState.messages, true, 50
@@ -64,10 +75,18 @@ private val MessageListScreenComponent2 = FC<MessageListScreenProps> { props ->
                     activeUserUid = props.uiState.activePersonUid
                 }
             }
+
+            item {
+                NewMessageBox.create {
+                    text = props.uiState.newMessageText
+                    onChangeNewMessageText = props.onChangeNewMessageText
+                    onClickSend = props.onClickSend
+                }
+            }
         }
 
         Container {
-            com.ustadmobile.view.components.virtuallist.VirtualListOutlet()
+            VirtualListOutlet()
         }
     }
 
@@ -98,66 +117,15 @@ val MessageListScreen = FC<Props> {
     }
 }
 
-
-//@Composable
-//fun MessageListScreen(
-//    uiState: MessageListUiState,
-//    onChangeNewMessageText: (String) -> Unit = { },
-//    onClickSend: () -> Unit = { },
-//){
-//
-//    val pager = remember(uiState.messages) {
-//        Pager(
-//            config = PagingConfig(pageSize = 20, enablePlaceholders = true, maxSize = 200),
-//            pagingSourceFactory = uiState.messages
-//        )
-//    }
-//
-//    val linkExtractor = rememberLinkExtractor()
-//
-//    val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
-//
-//
-//    Column(
-//        modifier = Modifier.fillMaxSize().defaultScreenPadding()
-//    ) {
-//        LazyColumn(
-//            reverseLayout = true,
-//            modifier = Modifier
-//                .defaultItemPadding()
-//                .weight(1f)
-//        ){
-//            ustadPagedItems(
-//                pagingItems = lazyPagingItems,
-//                key = { it.messageUid },
-//            ) {  message ->
-//                ChatItem(
-//                    message = message,
-//                    activeUserUid = uiState.activePersonUid,
-//                    linkExtractor = linkExtractor,
-//                )
-//            }
-//        }
-//
-//        NewMessageBox(
-//            text = uiState.newMessageText,
-//            onChangeNewMessageText = onChangeNewMessageText,
-//            onClickSend = onClickSend,
-//        )
-//    }
-//}
-
 external interface ChatItemProps: Props {
 
     var message: Message?
     var activeUserUid: Long?
 
-//    linkExtractor: ILinkExtractor,
 }
 
 val ChatItem = FC<ChatItemProps> { props ->
 
-    val strings = useStringProvider()
 
     Box {
         sx {
@@ -167,83 +135,55 @@ val ChatItem = FC<ChatItemProps> { props ->
         }
 
 
-//        UstadLinkifyText(
-//            text = message?.messageText ?: "",
-//            //color = MaterialTheme.colorScheme.onPrimaryContainer,
-//            linkExtractor = linkExtractor,
-//        )
+        UstadLinkify.create {
+            //color = MaterialTheme.colorScheme.onPrimaryContainer,
+            + (props.message?.messageText ?: "")
+        }
     }
 }
-//@Composable
-//fun ChatItem(
-//    message: Message?,
-//    activeUserUid: Long,
-//    linkExtractor: ILinkExtractor,
-//) {
-//    val isFromMe = message?.messageSenderPersonUid == activeUserUid
-//
-//    Column(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(4.dp)
-//    ) {
-//        Box(
-//            modifier = Modifier
-//                .align(if (isFromMe) Alignment.End else Alignment.Start)
-//                .clip(
-//                    RoundedCornerShape(
-//                        topStart = 48f,
-//                        topEnd = 48f,
-//                        bottomStart = if (isFromMe) 48f else 0f,
-//                        bottomEnd = if (isFromMe) 0f else 48f
-//                    )
-//                )
-//                .background(MaterialTheme.colorScheme.primaryContainer)
-//                .padding(16.dp)
-//        ) {
-//
-//        }
-//    }
-//}
 
-//@Composable
-//fun NewMessageBox(
-//    text: String,
-//    onChangeNewMessageText: (String) -> Unit,
-//    onClickSend: () -> Unit,
-//    modifier: Modifier = Modifier
-//) {
-//
-//    Row(modifier = modifier.padding(16.dp)) {
-//        TextField(
-//            value = text,
-//            onValueChange = onChangeNewMessageText,
-//            modifier = Modifier
-//                .weight(1f)
-//                .padding(4.dp),
-//            shape = RoundedCornerShape(24.dp),
-//            colors = TextFieldDefaults.colors(
-//                focusedIndicatorColor = Color.Transparent,
-//                unfocusedIndicatorColor = Color.Transparent,
-//                disabledIndicatorColor = Color.Transparent
-//            ),
-//            placeholder = {
-//                Text(text = stringResource(MR.strings.message))
-//            }
-//        )
-//
-//        IconButton(
-//            onClick = onClickSend,
-//            modifier = Modifier
-//                .clip(CircleShape)
-//                .background(color = MaterialTheme.colorScheme.primaryContainer)
-//                .align(Alignment.CenterVertically)
-//        ) {
-//            Icon(
-//                imageVector = Icons.Filled.Send,
-//                contentDescription = stringResource(MR.strings.send),
-//                modifier = Modifier.fillMaxSize().padding(8.dp)
-//            )
-//        }
-//    }
-//}
+external interface NewMessageBoxProps: Props {
+
+    var text: String
+    var fieldsEnabled: Boolean
+    var onChangeNewMessageText: (String) -> Unit
+    var onClickSend: () -> Unit
+
+}
+
+val NewMessageBox = FC<NewMessageBoxProps> { props ->
+
+    val strings = useStringProvider()
+    val theme by useRequiredContext(ThemeContext)
+
+    Stack {
+        direction = responsive(StackDirection.row)
+        spacing = responsive(20.px)
+
+        UstadTextField {
+            id = "newComment"
+//            disabled = !props.fieldsEnabled
+            fullWidth = true
+            value = props.text
+            label = ReactNode(strings[MR.strings.message])
+            onTextChange = {
+                props.onChangeNewMessageText(it)
+            }
+        }
+
+        IconButton {
+           sx {
+               shapeOutside = ShapeOutside.contentBox
+               background = theme.palette.secondary.main
+           }
+            color = IconButtonColor.info
+
+            ariaLabel = strings[MR.strings.message]
+            onClick = {
+                props.onClickSend()
+            }
+
+            Send()
+        }
+    }
+}
