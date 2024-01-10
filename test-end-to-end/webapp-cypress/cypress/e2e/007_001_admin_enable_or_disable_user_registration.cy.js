a155bc0c00dab50c411fb69dc0c89f0e70c28dfd
pooja <pooja@ustadmobile.com>
2024-01-10 15:14:03 +0400

    Added changes to cypress tests

--- test-end-to-end/webapp-cypress/cypress/e2e/007_001_admin_enable_or_disable_user_registration.cy.js
+++ test-end-to-end/webapp-cypress/cypress/e2e/007_001_admin_enable_or_disable_user_registration.cy.js
@@ -20,7 +20,7 @@ describe('007_001_admin_enable_or_disable_user_registration', () => {
   cy.contains('Edit').click()
  //https://docs.cypress.io/api/commands/should#Assert-the-href-attribute-is-equal-to-users
   cy.get('#terms_html_edit .ql-editor.ql-blank').as('editor')
-  cy.get('@editor').should('have.attr', 'contenteditable').and('equal', 'true',{timeout:3000})
+  //cy.get('@editor').should('have.attr', 'contenteditable').and('equal', 'true',{timeout:3000})
   cy.get('@editor').click().clear().type("null")
   cy.get('#registration_allowed').click({force:true}) // disable the registration button
   cy.get('#actionBarButton').click()
