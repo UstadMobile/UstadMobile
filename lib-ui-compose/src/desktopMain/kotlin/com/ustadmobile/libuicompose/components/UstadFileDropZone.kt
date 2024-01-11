7818b2f6b5b6e8fa367f29b5fb9f579e91c21e03
Mike Dawson <mike@ustadmobile.com>
2024-01-11 22:32:52 +0400

    Improve file drop appearance.

--- lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/components/UstadFileDropZone.kt
+++ lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/components/UstadFileDropZone.kt
@@ -1,18 +1,29 @@
 package com.ustadmobile.libuicompose.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileUpload
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.DragData
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.onExternalDrag
 import androidx.compose.ui.unit.dp
+import dev.icerock.moko.resources.compose.stringResource
+import com.ustadmobile.core.MR
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -61,5 +72,30 @@ actual fun UstadFileDropZone(
             )
     ) {
         content()
+        if(isDragging) {
+            Box(
+                modifier = Modifier.background(
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
+                    shape = RoundedCornerShape(16.dp)
+                ).fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(
+                        Icons.Default.FileUpload,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+
+                    Text(
+                        text= stringResource(MR.strings.drop_files_to_import),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+
+            }
+        }
     }
 }
