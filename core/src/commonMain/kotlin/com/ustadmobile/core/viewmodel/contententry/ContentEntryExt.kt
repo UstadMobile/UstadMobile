9230ad47c9393617ec448f705f8ac71e32848482
Mike Dawson <mike@ustadmobile.com>
2024-01-18 16:03:48 +0400

    Fix content type names being displayed in ContentEntryList

---
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/ContentEntryExt.kt
@@ -0,0 +1,17 @@
+package com.ustadmobile.core.viewmodel.contententry
+
+import com.ustadmobile.lib.db.entities.ContentEntry
+import dev.icerock.moko.resources.StringResource
+import com.ustadmobile.core.MR
+
+val ContentEntry.contentTypeStringResource: StringResource
+    get() = when(contentTypeFlag) {
+        ContentEntry.TYPE_COLLECTION -> MR.strings.collection
+        ContentEntry.TYPE_EBOOK -> MR.strings.ebook
+        ContentEntry.TYPE_INTERACTIVE_EXERCISE -> MR.strings.interactive
+        ContentEntry.TYPE_VIDEO -> MR.strings.video
+        ContentEntry.TYPE_AUDIO -> MR.strings.audio
+        ContentEntry.TYPE_DOCUMENT -> MR.strings.document
+        ContentEntry.TYPE_ARTICLE -> MR.strings.article
+        else -> MR.strings.blank
+    }
