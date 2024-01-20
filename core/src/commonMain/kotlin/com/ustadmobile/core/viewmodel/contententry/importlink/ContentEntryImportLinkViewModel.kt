8fb9eaff4b86d211c6ad71a7734fb0fe304e0089
Mike Dawson <mike@ustadmobile.com>
2024-01-20 23:36:46 +0400

    Update ContentEntry editing navigation to handle links and files.

--- core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/importlink/ContentEntryImportLinkViewModel.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/importlink/ContentEntryImportLinkViewModel.kt
@@ -33,6 +33,18 @@ data class ContentEntryImportLinkUiState(
     val fieldsEnabled: Boolean = true,
 )
 
+/**
+ * ContentEntryImportLink can be used in navigation paths as follows:
+ *
+ * 1) User is adding to library folder: arg_next will be set to
+ *    ContentEntryEdit, and popup to on finish will be set to
+ *    ContentEntryImportLinkViewModel.DEST_NAME so that when the user
+ *    finishes saving content, the import link screen is popped off the stack.
+ * 2) User is adding to course: the expected result destination and key will be set, s
+ *    so the normal navigate for result logic will apply.
+ * 3) User is changing updating an existing content entry: again, the expected result
+ *    destination and key will be set and normal result logic will apply.
+ */
 class ContentEntryImportLinkViewModel(
     di: DI,
     savedStateHandle: UstadSavedStateHandle
@@ -45,6 +57,8 @@ class ContentEntryImportLinkViewModel(
     private val httpClient: HttpClient by instance()
 
     private var commitLinkToSavedStateJob: Job? = null
+
+    private val nextDest = savedStateHandle[ARG_NEXT]
 
     init {
         _appUiState.value = AppUiState(
@@ -118,26 +132,29 @@ class ContentEntryImportLinkViewModel(
 
                 val metadataResult: MetadataResult = response.body()
 
-                if(expectedResultDest != null) {
-                    finishWithResult(metadataResult)
-                }else {
-                    //go to ContentEntryEdit
-                    navController.navigate(
-                        viewName = ContentEntryEditViewModel.DEST_NAME,
-                        args = buildMap {
-                            put(
-                                key = ContentEntryEditViewModel.ARG_IMPORTED_METADATA,
-                                value = json.encodeToString(
-                                    serializer = MetadataResult.serializer(),
-                                    value = metadataResult
+                when (nextDest) {
+                    ContentEntryEditViewModel.DEST_NAME -> {
+                        navController.navigate(
+                            viewName = ContentEntryEditViewModel.DEST_NAME,
+                            args = buildMap {
+                                put(
+                                    key = ContentEntryEditViewModel.ARG_IMPORTED_METADATA,
+                                    value = json.encodeToString(
+                                        serializer = MetadataResult.serializer(),
+                                        value = metadataResult
+                                    )
                                 )
-                            )
-                            putFromSavedStateIfPresent(ContentEntryEditViewModel.ARG_COURSEBLOCK)
-                            putFromSavedStateIfPresent(UstadView.ARG_RESULT_DEST_VIEWNAME)
-                            putFromSavedStateIfPresent(UstadView.ARG_RESULT_DEST_KEY)
-                            putFromSavedStateIfPresent(ARG_PARENT_UID)
-                        }
-                    )
+                                putFromSavedStateIfPresent(ContentEntryEditViewModel.ARG_COURSEBLOCK)
+                                putFromSavedStateIfPresent(UstadView.ARG_RESULT_DEST_VIEWNAME)
+                                putFromSavedStateIfPresent(UstadView.ARG_RESULT_DEST_KEY)
+                                putFromSavedStateIfPresent(ARG_PARENT_UID)
+                                put(ARG_POPUPTO_ON_FINISH, DEST_NAME)
+                            }
+                        )
+                    }
+                    else -> {
+                        finishWithResult(metadataResult)
+                    }
                 }
             }catch(e: Exception) {
                 snackDispatcher.showSnackBar(Snack(systemImpl.getString(MR.strings.error) + ": $e"))
