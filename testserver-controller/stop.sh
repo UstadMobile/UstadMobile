8468e1645aa14c8059d32ae64494a9f56996cfa5
pooja <pooja@ustadmobile.com>
2024-02-05 11:23:15 +0400

    Fix for 001_001 test

--- testserver-controller/stop.sh
+++ testserver-controller/stop.sh
@@ -2,13 +2,9 @@
 
 BASEDIR="$(realpath $(dirname $0))"
 
+
 if [ ! -e $BASEDIR/build/server.pid ]; then
     echo "No build/server.pid file. Nothing to stop."
-fi
-
-if [ "$1" == "--generatereport" ]; then
-   java -classpath $BASEDIR/build/libs/testserver-controller-all.jar \
-  com.ustadmobile.test.http.AdbVideoReportMakerKt $BASEDIR/../
 fi
 
 wget -qO- http://localhost:8075/stop
@@ -19,5 +15,8 @@ echo "Stopped server running as PID $PID"
 rm $BASEDIR/build/server.pid
 
 #Generate report
-java -classpath $BASEDIR/build/libs/testserver-controller-all.jar \
+
+if [ "$1" == "--generatereport" ]; then
+   java -classpath $BASEDIR/build/libs/testserver-controller-all.jar \
   com.ustadmobile.test.http.AdbVideoReportMakerKt $BASEDIR/../
+fi
