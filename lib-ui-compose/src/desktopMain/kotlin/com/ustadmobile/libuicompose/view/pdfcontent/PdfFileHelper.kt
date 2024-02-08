667cb242a12322e9f0c9993a8bd5513e9eccc63a
Mike Dawson <mike@ustadmobile.com>
2024-02-09 01:08:38 +0400

    Desktop PDF loader improvements and fix issue #828
    Remove use of positioning measurement that caused display PDF to shrink on Windows. The desktop PdfFileHelper will now load the size of each page, and if a page is not ready, can create a placeholder image. This ensures the size of each item in the LazyColumn does not change, so pages will not jump.

--- lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/pdfcontent/PdfFileHelper.kt
+++ lib-ui-compose/src/desktopMain/kotlin/com/ustadmobile/libuicompose/view/pdfcontent/PdfFileHelper.kt
@@ -2,6 +2,7 @@ package com.ustadmobile.libuicompose.view.pdfcontent
 
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
+import com.ustadmobile.door.util.systemTimeInMillis
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -17,15 +18,22 @@ import org.apache.pdfbox.Loader
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.rendering.PDFRenderer
+import java.awt.image.BufferedImage
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * PDFBox helper to
  *
  * 1) Ensure that all access to the PDFBox object is done through a single (but not Main) thread
  * 2) Close documents when finished.
+ *
+ * When the PDF file is loaded, the size of all pages will be loaded and saved at the same time.
+ * This will allow for the instant creation of correctly sized placeholder images.
  */
-class PdfFileHelper {
+class PdfFileHelper(
+    private val pageCacheSize: Int = 10,
+) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private val context = newSingleThreadContext("pdf-context")
@@ -38,17 +46,45 @@ class PdfFileHelper {
 
     private val _doc = MutableStateFlow<PdfFileHelperState?>(null)
 
+    private data class PageCacheEntry(
+        val imageBitmap: ImageBitmap,
+        val lastAccessTime: Long,
+    )
+
+    private data class PageAndScale(
+        val page: Int,
+        val scale: Float,
+    )
+
+    private val pageCache = ConcurrentHashMap<PageAndScale, PageCacheEntry>()
+
+    private fun getPageAndUpdateLastAccessTime(pageAndScale: PageAndScale): ImageBitmap? {
+        val pageEntry = pageCache[pageAndScale]
+        if(pageEntry != null) {
+            pageCache[pageAndScale] = pageEntry.copy(
+                lastAccessTime = systemTimeInMillis()
+            )
+        }
+
+        return pageEntry?.imageBitmap
+    }
+
     class PdfFileHelperState(
         val doc: PDDocument,
         val renderer: PDFRenderer,
+        val pageSizes: List<PDRectangle>,
     )
 
     fun load(file: File) {
         scope.launch {
             val loadedDoc = Loader.loadPDF(file)
             val renderer = PDFRenderer(loadedDoc)
+            val pageSizes = (0 until loadedDoc.numberOfPages).map {
+                loadedDoc.getPage(it).cropBox
+            }
+
             val oldState = _doc.getAndUpdate {
-                PdfFileHelperState(loadedDoc, renderer)
+                PdfFileHelperState(loadedDoc, renderer, pageSizes)
             }
             oldState?.doc?.close()
 
@@ -56,18 +92,35 @@ class PdfFileHelper {
         }
     }
 
-
-    //https://github.com/JetBrains/compose-multiplatform/blob/master/tutorials/Image_And_Icons_Manipulations/README.md
-    suspend fun getSize(page: Int): PDRectangle? {
-        return withContext(context) {
-            _doc.value?.doc?.getPage(page)?.cropBox
-        }
+    fun getPageSize(page: Int): PDRectangle? {
+        return _doc.value?.pageSizes?.get(page)
     }
 
     suspend fun loadPage(page: Int, scale: Float) : ImageBitmap? {
-        return withContext(context) {
-            _doc.value?.renderer?.renderImage(page, scale)?.toComposeImageBitmap()
+        val pageAndScale = PageAndScale(page, scale)
+        return getPageAndUpdateLastAccessTime(pageAndScale) ?: withContext(context) {
+            _doc.value?.renderer?.renderImage(page, scale)?.toComposeImageBitmap()?.also {
+                pageCache[pageAndScale] = PageCacheEntry(it, systemTimeInMillis())
+                if(pageCache.size > pageCacheSize) {
+                    val pageToRemove = pageCache.entries.minBy { it.value.lastAccessTime }
+                    pageCache.remove(pageToRemove.key)
+                }
+            }
         }
+    }
+
+    /**
+     * If a page has not yet been loaded, a BufferedImage can be created instantly with the correct
+     * size. This avoids any jumping that would otherwise happen if sizes change.
+     */
+    fun getPlaceholderImage(page: Int, scale: Float) : ImageBitmap {
+        return _doc.value?.pageSizes?.get(page)?.let {
+            BufferedImage((it.width * scale).toInt(), (it.height * scale).toInt(), BufferedImage.TYPE_INT_ARGB)
+        }?.toComposeImageBitmap() ?: throw IllegalArgumentException("No size known for page $page")
+    }
+
+    fun getCachedPage(page: Int, scale: Float) : ImageBitmap? {
+        return getPageAndUpdateLastAccessTime(PageAndScale(page, scale))
     }
 
     fun close() {
