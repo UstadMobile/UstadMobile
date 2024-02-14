7b7037a0cff15b4769e49984acd2ab03777c84d0
Mike Dawson <mike@ustadmobile.com>
2024-02-14 10:24:13 +0400

    Fix for Epub display on Android (Issue #837)
    Android WebViewClient WebResourceResponse requires a non-empty reason. When converting an OkHttpResponse, we will now explicitly check the message and ensure it is not empty.

--- core/src/androidMain/kotlin/com/ustadmobile/core/domain/contententry/server/OkHttpResponseExt.kt
+++ core/src/androidMain/kotlin/com/ustadmobile/core/domain/contententry/server/OkHttpResponseExt.kt
@@ -1,15 +1,25 @@
 package com.ustadmobile.core.domain.contententry.server
 
 import android.webkit.WebResourceResponse
+import io.ktor.http.HttpStatusCode
 import okhttp3.Response
 import java.io.ByteArrayInputStream
 
 fun Response.asWebResourceResponse(): WebResourceResponse {
+    /* Android requires a non-empty HTTP response message, however this was dropped in HTTP2
+     *  https://github.com/httpwg/http2-spec/issues/202 .
+     *
+     * Therefor: if the message is empty, add it using the defaults from KTOR.
+     */
+    val effectiveMessage = message.ifEmpty {
+        HttpStatusCode.fromValue(code).description
+    }
+
     return WebResourceResponse(
         header("content-type") ?: "application/octet-stream",
         "utf-8",
         code,
-        message,
+        effectiveMessage,
         headers.toMap(),
         body?.byteStream() ?: ByteArrayInputStream(ByteArray(0))
     )
