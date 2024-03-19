7eb15641029fb059b106ba11f02210b26b87e9e3
Mike Dawson <mike@ustadmobile.com>
2024-03-19 12:16:50 +0400

    Switch Quartz scheduler on JVM to using HSQLDB instead of SQLite.
    HSQLDB has an official JDBC delegate provided. Attempt to avoid issue #854 (SQLITE Busy).

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -124,6 +124,7 @@ dependencies {
     implementation(libs.kxml2)
     implementation(libs.logback.classic)
     implementation(libs.junique)
+    implementation(libs.hsqldb)
 
     //as per https://conveyor.hydraulic.dev/13.0/tutorial/tortoise/2-gradle/#adapting-a-compose-desktop-app
     linuxAmd64(compose.desktop.linux_x64)
