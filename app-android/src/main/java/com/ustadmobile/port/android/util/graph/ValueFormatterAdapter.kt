e7a148b069d1066b9c37131e423e2b8639c5b142
Mike Dawson <mike@ustadmobile.com>
2024-03-20 16:38:18 +0400

    Move all app-android dependency versions into TOML

--- app-android/src/main/java/com/ustadmobile/port/android/util/graph/ValueFormatterAdapter.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/util/graph/ValueFormatterAdapter.kt
@@ -1,14 +1,14 @@
 package com.ustadmobile.port.android.util.graph
 
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.ustadmobile.core.util.graph.LabelValueFormatter
-
-class ValueFormatterAdapter(val formatter: LabelValueFormatter) : ValueFormatter() {
-
-    override fun getFormattedValue(value: Float): String {
-        return formatter.format(value)
-    }
-
-}
-
-fun LabelValueFormatter.asValueFormatter() = ValueFormatterAdapter(this)
+//import com.github.mikephil.charting.formatter.ValueFormatter
+//import com.ustadmobile.core.util.graph.LabelValueFormatter
+//
+//class ValueFormatterAdapter(val formatter: LabelValueFormatter) : ValueFormatter() {
+//
+//    override fun getFormattedValue(value: Float): String {
+//        return formatter.format(value)
+//    }
+//
+//}
+//
+//fun LabelValueFormatter.asValueFormatter() = ValueFormatterAdapter(this)
