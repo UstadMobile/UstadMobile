0c822e8ea97244881fbdcfde1808d7461fab0b23
Mike Dawson <mike@ustadmobile.com>
2024-04-29 00:42:24 +0400

    Attempt to fix some cypress assignment test misbehavior post-design changes.

--- test-end-to-end/webapp-cypress/cypress/e2e/WEB_004_007_user_enter_assignment_page_before_graceperiod_but_submission_after_grace_Period.cy.js
+++ test-end-to-end/webapp-cypress/cypress/e2e/WEB_004_007_user_enter_assignment_page_before_graceperiod_but_submission_after_grace_Period.cy.js
@@ -75,7 +75,7 @@ it('Student not able to submit assignment', () => {
 
 })
 
-it('Teacher add assignment mark and course comment', () => {
+it('Teacher checks submissions', () => {
   cy.ustadClearDbAndLogin('teacher1','test1234')
  //  Assignment block
   cy.contains("Course").click()
@@ -83,7 +83,7 @@ it('Teacher add assignment mark and course comment', () => {
   cy.contains("button","Course").click()
   cy.contains("Assignment 1").click()
   cy.contains('Submissions').click()
-  cy.contains('Student 1').click()
+  cy.contains('Student 1', {timeout:5000}).click()
   cy.contains("Not submitted").should('exist')
 })
 
