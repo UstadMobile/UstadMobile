f5f9ec7fabf8c5336f3747e474b887d7323d3b36
Mike Dawson <mike@ustadmobile.com>
2024-06-18 15:13:23 +0400

    Update Android Gradle Plugin and fix PDF dependency.
    Android PDF dependency artifact group name was changed.

--- app-android-baselineprofile/build.gradle.kts
+++ app-android-baselineprofile/build.gradle.kts
@@ -35,8 +35,8 @@ baselineProfile {
 }
 
 dependencies {
-    implementation("androidx.test.ext:junit:1.1.5")
-    implementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.test.uiautomator:uiautomator:2.2.0")
-    implementation("androidx.benchmark:benchmark-macro-junit4:1.2.2")
+    implementation(libs.androidx.test.junit.ext)
+    implementation(libs.androidx.test.espresso.core)
+    implementation(libs.androidx.test.uiautomator)
+    implementation(libs.androidx.benchmark.macro.junit4)
 }
