ea3790cdcc606416d589a10004361e7c043584de
Mike Dawson <mike@ustadmobile.com>
2024-06-20 13:08:37 +0400

    Improve ClazzProgressReportScreen scrolling on web.
    Use a single element for both horizontal and vertical scrolling. Use CSS sticky property without requiring event listener to ensure pinning student names and headers is smooth.

--- app-react/src/jsMain/kotlin/com/ustadmobile/view/clazz/progressreport/ClazzProgressReportScreen.kt
+++ app-react/src/jsMain/kotlin/com/ustadmobile/view/clazz/progressreport/ClazzProgressReportScreen.kt
@@ -8,6 +8,7 @@ import com.ustadmobile.hooks.useDoorRemoteMediator
 import com.ustadmobile.hooks.usePagingSource
 import com.ustadmobile.hooks.useTabAndAppBarHeight
 import com.ustadmobile.hooks.useUstadViewModel
+import com.ustadmobile.mui.components.ThemeContext
 import com.ustadmobile.view.components.virtuallist.VirtualList
 import com.ustadmobile.view.components.virtuallist.VirtualListOutlet
 import com.ustadmobile.view.components.virtuallist.virtualListContent
@@ -23,16 +24,15 @@ import mui.system.sx
 import react.FC
 import react.Props
 import react.create
-import react.useState
+import react.useRequiredContext
 import web.cssom.Contain
 import web.cssom.Display
 import web.cssom.Height
 import web.cssom.Overflow
+import web.cssom.Position
 import web.cssom.TextOverflow
 import web.cssom.pct
 import web.cssom.px
-import web.cssom.translatex
-import web.html.HTMLElement
 
 external interface ClazzProgressReportProps: Props {
     var uiState: ClazzProgressReportUiState
@@ -55,32 +55,74 @@ val ClazzProgressReportComponent = FC<ClazzProgressReportProps> { props ->
         mediatorResult.pagingSourceFactory, false, 50
     )
 
-    /**
-     * It (might) be possible to make the effect smoother by using the event handler itself to
-     * directly set the translate property, or, dynamically create/update a css class.
-     */
-    var scrollX by useState { 0.toDouble() }
+    val theme by useRequiredContext(ThemeContext)
 
     val tabAndAppBarHeight = useTabAndAppBarHeight()
 
-    val headerRowHeight =  50
+    val headerRowHeight =  30
 
     val totalWidth = NAME_WIDTH + COLUMN_WIDTH * props.uiState.courseBlocks.size
 
-    Container {
-        Box {
-            sx {
-                width = 100.pct
-                overflowX = Overflow.scroll
-                overflowY = Overflow.clip
-                height = "calc(100vh - ${tabAndAppBarHeight + 8}px)".unsafeCast<Height>()
-                contain = Contain.strict
-                display = Display.block
-            }
+    VirtualList {
+        style = jso {
+            height = "calc(100vh - ${tabAndAppBarHeight + 8}px)".unsafeCast<Height>()
+            width = 100.pct
+            contain = Contain.strict
+            overflowY = Overflow.scroll
+            overflowX = Overflow.scroll
+            display = Display.block
+        }
 
-            onScroll = { evt ->
-                scrollX = evt.target.unsafeCast<HTMLElement>().scrollLeft
+        content = virtualListContent {
+            infiniteQueryPagingItemsIndexed(
+                items = infiniteQueryResult,
+                key = { item, index ->
+                    item.student.person?.personUid?.toString() ?: index.toString()
+                }
+            ) { item, index ->
+                Stack.create {
+                    direction = responsive(StackDirection.row)
+                    sx {
+                        width = totalWidth.px
+                    }
+
+                    //Person name and picture
+                    Stack {
+                        sx {
+                            width = NAME_WIDTH.px
+                            overflowInline = Overflow.clip
+                            textOverflow = TextOverflow.ellipsis
+                            paddingTop = 16.px
+                            paddingBottom = 16.px
+                            position = Position.sticky
+
+                            //Setting left to a non-auto value, and then leaving top/bottom alone
+                            // makes sticky apply to the x-axis, not the y axis, as we want.
+                            left = 0.px
+                        }
+
+                        + (item?.student?.person?.fullName() ?: "null")
+                    }
+
+                    //Progress blocks for each here.
+                    props.uiState.courseBlocks.forEach { block ->
+                        Typography {
+                            sx {
+                                width = COLUMN_WIDTH.px
+                                paddingTop = 16.px
+                                paddingBottom = 16.px
+                            }
+
+                            + "-"
+                        }
+                    }
+                }
             }
+        }
+
+        Stack {
+            direction = responsive(StackDirection.column)
+
 
             // Course Block header row
             Stack {
@@ -88,6 +130,12 @@ val ClazzProgressReportComponent = FC<ClazzProgressReportProps> { props ->
                 sx {
                     height = headerRowHeight.px
                     width = totalWidth.px
+
+                    //Setting top to a non-auto value, and then leaving left/right alone
+                    // makes sticky apply to the y-axis, not the x axis, as we want.
+                    position = Position.sticky
+                    top = 0.px
+                    //backgroundColor = theme.palette.background.default
                 }
 
                 //Spacer
@@ -110,56 +158,7 @@ val ClazzProgressReportComponent = FC<ClazzProgressReportProps> { props ->
                 }
             }
 
-            VirtualList {
-                style = jso {
-                    height = "calc(100vh - ${tabAndAppBarHeight + headerRowHeight + 8}px)"
-                        .unsafeCast<Height>()
-                    width = totalWidth.px
-                    contain = Contain.strict
-                    overflowY = Overflow.scroll
-                    display = Display.block
-                }
-
-                content = virtualListContent {
-                    infiniteQueryPagingItemsIndexed(
-                        items = infiniteQueryResult,
-                        key = { item, index ->
-                            item.student.person?.personUid?.toString() ?: index.toString()
-                        }
-                    ) { item, index ->
-                        Stack.create {
-                            direction = responsive(StackDirection.row)
-
-                            //Person name and picture
-                            Stack {
-                                sx {
-                                    transform = translatex(scrollX.px)
-                                    width = NAME_WIDTH.px
-                                    overflowInline = Overflow.clip
-                                    textOverflow = TextOverflow.ellipsis
-                                }
-
-                                + (item?.student?.person?.fullName() ?: "null")
-                            }
-
-                            //Progress blocks for each here.
-                            props.uiState.courseBlocks.forEach { block ->
-                                Typography {
-                                    sx {
-                                        width = COLUMN_WIDTH.px
-                                    }
-
-                                    + "-"
-                                }
-                            }
-                        }
-                    }
-                }
-
-                VirtualListOutlet()
-            }
-
-
+            VirtualListOutlet()
         }
     }
 }
