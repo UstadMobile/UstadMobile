3e230686f654492306cf76fddf839917cc627632
pooja <pooja@ustadmobile.com>
2024-07-18 17:40:30 +0400

    Assignment Changes

--- test-end-to-end/android-maestro/common/setDate.js
+++ test-end-to-end/android-maestro/common/setDate.js
@@ -7,13 +7,20 @@ const currentDate = `${day}/${month}/${year}`; // Format: DD/MM/YYYY
 const yesterday = (today.getDate() - 1).toString().padStart(2, '0');
 const yesterdayDate=`${yesterday}/${month}/${year}`; // Format: DD/MM/YYYY
 
+const tomorrow = (today.getDate() + 1).toString().padStart(2, '0');
+const tomorrowDate=`${tomorrow}/${month}/${year}`; // Format: DD/MM/YYYY
+
 const hours = String(today.getHours()).padStart(2, '0');
-const minutes = String(today.getMinutes()+3).padStart(2, '0');
-const delay1Hr= String(today.getHours()+1).padStart(2, '0');
+const minutes = String(today.getMinutes()).padStart(2, '0');
+const min_3 = String(today.getMinutes()+3).padStart(2, '0');
+const delay1Hr= String(today.getHours()-1).padStart(2, '0');
 const time = `${hours}:${minutes}`;
+const testTime = `${hours}:${min_3}`;
 const delayTime = `${delay1Hr}:${minutes}`;
 
 output.yesterday_Date = yesterdayDate;
-output.after1HrTime = delayTime;
+output.tomorrow_Date = tomorrowDate;
+output.delay_1hr = delayTime;
 output.todayDate = currentDate;
-output.todayTime = time;
+output.Time = time;
+output.test_Time = testTime;
--- test-end-to-end/android-maestro/common/setTime.js
+++
@@ -1,5 +0,0 @@
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()+3).padStart(2, '0');
-    const time = `${hours}:${minutes}`;
-    output.result1 = time;
