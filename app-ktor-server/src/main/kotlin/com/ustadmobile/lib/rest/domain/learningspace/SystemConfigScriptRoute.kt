2a6578fe85c3799efe4c0950c2ef117677aa5b69
lenovo <nikunj.sharma@sutara.org>
2024-10-29 17:06:51 +0530

    commit

--- app-ktor-server/src/main/kotlin/com/ustadmobile/lib/rest/domain/learningspace/SystemConfigScriptRoute.kt
+++ app-ktor-server/src/main/kotlin/com/ustadmobile/lib/rest/domain/learningspace/SystemConfigScriptRoute.kt
@@ -2,6 +2,7 @@ package com.ustadmobile.lib.rest.domain.learningspace
 
 import com.ustadmobile.appconfigdb.SystemDb
 import com.ustadmobile.appconfigdb.SystemDbDataLayer
+import io.github.aakira.napier.Napier
 import io.ktor.http.ContentType
 import io.ktor.server.routing.Route
 import io.ktor.http.HttpStatusCode
@@ -10,6 +11,7 @@ import io.ktor.server.application.call
 import io.ktor.server.request.uri
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import org.kodein.di.DI
 import org.kodein.di.direct
@@ -19,12 +21,12 @@ fun Route.SystemConfigScriptRoute(
     di: DI
 ) {
 
-    post("script") {
+    get("script") {
         val clientUrl = call.request.uri
         val baseUrl = clientUrl.replace("api/sysconfig/script", "")
         val repo: SystemDb? = di.direct.instance<SystemDbDataLayer>().repository
         if (repo != null) {
-            val learningSpace = repo.learningSpaceInfoDao().getLearningSpace(clientUrl)
+            val learningSpace = repo.learningSpaceInfoDao().getLearningSpace(baseUrl)
 
             if (learningSpace != null) {
 
@@ -32,7 +34,7 @@ fun Route.SystemConfigScriptRoute(
                     "var _ustadLearningSpaceExists = true;"
                 }
             } else {
-                call.respond(HttpStatusCode.NotFound, "Learning space not found.")
+                call.respond(HttpStatusCode.ExpectationFailed, "Learning space not found.")
             }
         } else {
             call.respond(HttpStatusCode.NotFound, "Repo is null")
--- app-ktor-server/ustad-appconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -classpath build/libs/ustad-server-all.jar \
-    com.ustadmobile.lib.rest.clitools.appconfig.AppConfigCLIKt "$@"
