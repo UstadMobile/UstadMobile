58446e49ed34bee40284c312f93ea39c68c89d41
Mandvii <mandvi.verma@sutara.org>
2024-11-07 12:04:27 +0530

    Update

--- core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/detailattemptlisttab/ContentEntryDetailAttemptsSessionListViewModel.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/detailattemptlisttab/ContentEntryDetailAttemptsSessionListViewModel.kt
@@ -1,9 +1,25 @@
 package com.ustadmobile.core.viewmodel.contententry.detailattemptlisttab
 
+import com.ustadmobile.core.impl.nav.UstadSavedStateHandle
+import com.ustadmobile.core.viewmodel.UstadListViewModel
+import org.kodein.di.DI
 
-class ContentEntryDetailAttemptsSessionListViewModel(){
+ class ContentEntryDetailAttemptsSessionListUiState()
+
+class ContentEntryDetailAttemptsSessionListViewModel(
+    di: DI, savedStateHandle: UstadSavedStateHandle, destinationName: String = DEST_NAME, )
+    : UstadListViewModel<ContentEntryDetailAttemptsSessionListUiState>(
+    di, savedStateHandle, ContentEntryDetailAttemptsSessionListUiState(), destinationName) {
 
     companion object {
         const val DEST_NAME = "ContentEntryDetailAttemptsSessionList"
+    }
+
+    override fun onUpdateSearchResult(searchText: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickAdd() {
+        TODO("Not yet implemented")
     }
 }
