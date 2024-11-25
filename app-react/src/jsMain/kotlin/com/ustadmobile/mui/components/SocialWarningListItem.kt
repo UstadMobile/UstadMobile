2b15ddc9c10bd98196ea5fabc140203e8a84fc1a
Prashant Deshmukh <prashant.deshmukh@sutara.org>
2024-11-25 21:32:03 +0530

    - Moved `SOCIAL_WARNING_WEB_URL` to `ShowSocialWarningUseCase` - Buttons are part of secondary text with proper alignment in a responsive layout.

--- app-react/src/jsMain/kotlin/com/ustadmobile/mui/components/SocialWarningListItem.kt
+++ app-react/src/jsMain/kotlin/com/ustadmobile/mui/components/SocialWarningListItem.kt
@@ -3,30 +3,29 @@ package com.ustadmobile.mui.components
 import com.ustadmobile.core.MR
 import com.ustadmobile.core.hooks.useStringProvider
 import js.objects.jso
-import mui.material.Box
-import mui.icons.material.Warning as WarningIcon
 import mui.material.Button
 import mui.material.ButtonVariant
 import mui.material.ListItem
 import mui.material.ListItemIcon
 import mui.material.ListItemText
+import mui.material.Stack
+import mui.material.StackDirection
+import mui.system.responsive
 import react.FC
 import react.Props
 import react.ReactNode
-import web.cssom.AlignItems
-import web.cssom.Display
-import web.cssom.FlexDirection
-import web.cssom.JustifyContent
+import react.create
+import react.dom.html.ReactHTML.div
 import web.cssom.px
 import web.window.WindowTarget
 import web.window.window
+import mui.icons.material.Warning as WarningIcon
 
 external interface SocialWarningProps : Props {
     var onDismiss: () -> Unit
 }
 
 val SocialWarningListItem = FC<SocialWarningProps> { props ->
-
     val strings = useStringProvider()
 
     ListItem {
@@ -34,42 +33,43 @@ val SocialWarningListItem = FC<SocialWarningProps> { props ->
             WarningIcon()
         }
 
-        Box {
-            sx = jso {
-                display = Display.flex
-                flexDirection = FlexDirection.column
-                alignItems = AlignItems.start
-            }
+        ListItemText {
+            primary = ReactNode(strings[MR.strings.be_careful_interacting_online])
+            secondary = Stack.create {
+                direction = responsive(StackDirection.column)
 
-            ListItemText {
-                primary = ReactNode(strings[MR.strings.be_careful_interacting_online])
-                secondary = ReactNode(strings[MR.strings.be_careful_not_to_share])
-            }
-
-            Box {
-                sx = jso {
-                    display = Display.flex
-                    flexDirection = FlexDirection.row
-                    gap = 12.px
-                    marginTop = 12.px
+                div {
+                    + strings[MR.strings.be_careful_not_to_share]
                 }
 
-                Button {
-                    variant = ButtonVariant.text
-                    onClick = { props.onDismiss() }
-                    +strings[MR.strings.got_it]
-                }
-
-                Button {
-                    variant = ButtonVariant.text
-                    onClick = {
-                        window.open(
-                            "https://beinternetawesome.withgoogle.com",
-                            WindowTarget._blank
-                        )
+                Stack {
+                    direction = responsive(StackDirection.row)
+                    sx = jso {
+                        gap = 12.px
+                        marginTop = 12.px
                     }
-                    +strings[MR.strings.learn_more]
+
+                    Button {
+                        variant = ButtonVariant.text
+                        onClick = { props.onDismiss() }
+                        +strings[MR.strings.got_it]
+                    }
+
+                    Button {
+                        variant = ButtonVariant.text
+                        onClick = {
+                            window.open(
+                                "https://beinternetawesome.withgoogle.com",
+                                WindowTarget._blank
+                            )
+                        }
+                        +strings[MR.strings.learn_more]
+                    }
                 }
+            }
+
+            secondaryTypographyProps = jso {
+                component = div
             }
         }
     }
