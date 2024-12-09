eaab1c95753e240f10c24845267e81c9c9d73195
Mike Dawson <mike@ustadmobile.com>
2024-12-09 12:31:47 +0400

    Add queries for list of persons who attempted a given piece of content nad list of sessions by person and content uid.

--- lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/dao/xapi/XapiSessionEntityDao.kt
+++ lib-database/src/commonMain/kotlin/com/ustadmobile/core/db/dao/xapi/XapiSessionEntityDao.kt
@@ -2,14 +2,10 @@ package com.ustadmobile.core.db.dao.xapi
 
 import androidx.room.Insert
 import androidx.room.Query
-import app.cash.paging.PagingSource
 import com.ustadmobile.door.annotation.DoorDao
 import com.ustadmobile.door.annotation.HttpAccessible
 import com.ustadmobile.door.annotation.HttpServerFunctionCall
 import com.ustadmobile.door.annotation.Repository
-import com.ustadmobile.lib.db.composites.PersonAndAttemptInfo
-import com.ustadmobile.lib.db.composites.StatementAndPersonAndPicture
-import com.ustadmobile.lib.db.entities.xapi.StatementEntity
 import com.ustadmobile.lib.db.entities.xapi.XapiSessionEntity
 
 @DoorDao
@@ -87,110 +83,6 @@ expect abstract class XapiSessionEntityDao {
         clazzUid: Long,
     ): XapiSessionEntity?
 
-    @HttpAccessible
-    @Query("""
-       
-SELECT 
-    StatementEntity.*, 
-    Person.*, 
-    PersonPicture.*, 
-    VerbEntity.*,
-    
-    (
-        SELECT COUNT(*)
-        FROM StatementEntity se
-        WHERE se.statementActorPersonUid = Person.personUid
-        AND se.completionOrProgress = 1
-        AND se.statementContentEntryUid = :contentEntryUid
-    ) AS numberOfAttempts
-FROM StatementEntity
-JOIN Person
-    ON Person.personUid = StatementEntity.statementActorPersonUid
-LEFT JOIN PersonPicture
-    ON PersonPicture.personPictureUid = Person.personUid 
-JOIN VerbEntity
-    ON VerbEntity.verbUid = StatementEntity.statementVerbUid
-WHERE StatementEntity.completionOrProgress = 1
-AND (StatementEntity.statementIdHi, StatementEntity.statementIdLo) = (
-    SELECT StatementEntity.statementIdHi, StatementEntity.statementIdLo
-    FROM StatementEntity
-    WHERE StatementEntity.statementContentEntryUid = :contentEntryUid
-    AND StatementEntity.statementActorPersonUid = Person.personUid 
-    AND StatementEntity.completionOrProgress = 1
-)
 
-""")
-    abstract   fun getSessionList(contentEntryUid: Long, personUid: Long):
-            PagingSource<Int, StatementAndPersonAndPicture>
-
-    @HttpAccessible(
-        clientStrategy = HttpAccessible.ClientStrategy.PULL_REPLICATE_ENTITIES,
-        pullQueriesToReplicate = arrayOf(
-            HttpServerFunctionCall(
-                functionName = "getAttemptList"
-            ),
-        )
-    )
-    @Query(
-        """
-    SELECT Person.*, PersonPicture.*,
-           (SELECT COUNT(XapiSessionEntity.xseUid)
-            FROM XapiSessionEntity
-            WHERE XapiSessionEntity.xseAccountPersonUid = Person.personUid
-              AND XapiSessionEntity.xseContentEntryUid = :contentEntryUid) AS numberAttempts
-    FROM Person
-    LEFT JOIN PersonPicture 
-                ON PersonPicture.personPictureUid = Person.personUid
-    WHERE numberAttempts >= 1
-               
-"""
-    )
-    abstract fun getAttemptList(contentEntryUid: Long): PagingSource<Int, PersonAndAttemptInfo>
-
-    @HttpAccessible
-    @Query(
-        """
-    SELECT *FROM StatementEntity
-    WHERE StatementEntity.statementActorPersonUid = :personUid
-    And StatementEntity.statementContentEntryUid=:contentEntryUid
-    AND ROWID IN 
-    (SELECT MIN(ROWID)
-    FROM StatementEntity
-    WHERE StatementEntity.resultCompletion=1 AND StatementEntity.statementActorPersonUid = :personUid
-    GROUP BY StatementEntity.statementIdHi, StatementEntity.statementIdLo)
-"""
-    )
-    abstract fun getSessionListDummy(
-        contentEntryUid: Long,
-        personUid: Long
-    ): PagingSource<Int, StatementEntity>
-
-
-    @HttpAccessible
-    @Query("""
-       SELECT StatementEntity.*, Person.*, PersonPicture.*,
-       (
-          SELECT COUNT(*)
-          FROM StatementEntity se
-          WHERE se.statementActorPersonUid = Person.personUid
-          AND se.completionOrProgress = 1
-          AND se.statementContentEntryUid = :contentEntryUid
-       ) AS numberOfAttempts
-       FROM StatementEntity
-       JOIN Person
-            ON Person.personUid = StatementEntity.statementActorPersonUid
-       LEFT JOIN PersonPicture
-            ON PersonPicture.personPictureUid = Person.personUid 
-       WHERE StatementEntity.completionOrProgress = 1 
-       AND (StatementEntity.statementIdHi, StatementEntity.statementIdLo) = (
-           SELECT StatementEntity.statementIdHi, StatementEntity.statementIdLo
-             FROM StatementEntity
-            WHERE StatementEntity.statementContentEntryUid = :contentEntryUid
-              AND StatementEntity.statementActorPersonUid = Person.personUid
-              AND StatementEntity.completionOrProgress = 1
-       )
-""")
-abstract   fun findPersonsWithAttempts(contentEntryUid: Long):
-            PagingSource<Int, StatementAndPersonAndPicture>
 
 }
