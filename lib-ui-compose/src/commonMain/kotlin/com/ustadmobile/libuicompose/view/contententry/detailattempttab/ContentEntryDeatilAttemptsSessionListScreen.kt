10e8ffd979876366fcbbecb1c79dc73466021d17
Mandvii <mandvi.verma@sutara.org>
2024-12-10 12:23:02 +0530

    Attempts Session List- desktop/android

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/contententry/detailattempttab/ContentEntryDeatilAttemptsSessionListScreen.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/contententry/detailattempttab/ContentEntryDeatilAttemptsSessionListScreen.kt
@@ -64,32 +64,28 @@ fun ContentEntryDetailAttemptsSessionListScreen(
         ) { attemptsSessionListItems ->
 
 
-
             androidx.compose.material3.ListItem(
                 modifier = Modifier.clickable {
                     attemptsSessionListItems?.also(onClickEntry)
                 },
                 leadingContent = {
-                    // Choose the appropriate icon based on the condition
                     Icon(
                         imageVector = when {
-                            // If extensionProgress is null, check resultSuccess
-                            attemptsSessionListItems?.isSuccessful!= null -> {
-                                if (attemptsSessionListItems?.isSuccessful== true) Icons.Filled.Check
+                            attemptsSessionListItems?.isSuccessful != null -> {
+                                if (attemptsSessionListItems?.isSuccessful == true) Icons.Filled.Check
                                 else Icons.Filled.Close
                             }
-                            // If extensionProgress is not null, check its value
-                            attemptsSessionListItems?.isCompleted==true -> Icons.Filled.Check
+
+                            attemptsSessionListItems?.isCompleted == true -> Icons.Filled.Check
                             else -> Icons.Filled.Close
                         },
                         contentDescription = when {
-                            // If extensionProgress is null, check resultSuccess
                             attemptsSessionListItems?.isSuccessful != null -> {
                                 if (attemptsSessionListItems?.isSuccessful == true) "Success"
                                 else "Failure"
                             }
-                            // If extensionProgress is 100, set as Completed
-                            attemptsSessionListItems?.isCompleted==true-> "Completed"
+
+                            attemptsSessionListItems?.isCompleted == true -> "Completed"
                             else -> "Incomplete"
                         },
                         modifier = Modifier.padding(end = 8.dp)
@@ -99,16 +95,16 @@ fun ContentEntryDetailAttemptsSessionListScreen(
                     androidx.compose.material3.Text(
                         text = when {
                             attemptsSessionListItems?.isSuccessful != null -> {
-                                // If extensionProgress is  null, check resultSuccess
                                 when {
                                     attemptsSessionListItems?.isSuccessful == true -> "Passed"
                                     attemptsSessionListItems?.isSuccessful == false -> "Failed"
                                     else -> "" // Default if resultSuccess is null
                                 }
                             }
+
                             else -> {
 
-                                if (attemptsSessionListItems?.isCompleted==true) {
+                                if (attemptsSessionListItems?.isCompleted == true) {
                                     "Completed"
                                 } else {
                                     "Incomplete"
@@ -122,21 +118,20 @@ fun ContentEntryDetailAttemptsSessionListScreen(
                     Column {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically // Align vertically in the center
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
                             Icon(
-                                imageVector = Icons.Filled.Star, // You can replace this with your desired icon
+                                imageVector = Icons.Filled.Star,
                                 contentDescription = "Icon",
-                                modifier = Modifier.padding(8.dp) // Add space between the icon and the text
+                                modifier = Modifier.padding(8.dp)
                             )
                             Text(
                                 text = when {
-                                    attemptsSessionListItems?.maxScore!= null -> {
-                                        // If extensionProgress is null, show the score text
+                                    attemptsSessionListItems?.maxScore != null -> {
                                         "${((attemptsSessionListItems?.maxScore ?: 0f) * 100).toInt()}% Score"
                                     }
+
                                     else -> {
-                                        // If extensionProgress is not null, show the completion text
                                         "${(attemptsSessionListItems?.maxProgress)}% Completion"
                                     }
                                 }
