0b066fe89f25dcacff023b7a07ef2426625e05bd
Mandvii <mandvi.verma@sutara.org>
2024-12-11 18:22:59 +0530

    Successful build

--- lib-ui-compose/src/androidMain/kotlin/com/ustadmobile/libuicompose/images/UstadImagePainter.kt
+++ lib-ui-compose/src/androidMain/kotlin/com/ustadmobile/libuicompose/images/UstadImagePainter.kt
@@ -15,8 +15,6 @@ private val nameMap = mapOf(
     UstadImage.COURSE_BANNER_DEFAULT2 to R.drawable.course_banner_default2,
     UstadImage.COURSE_BANNER_DEFAULT3 to R.drawable.course_banner_default3,
     UstadImage.COURSE_BANNER_DEFAULT4 to R.drawable.course_banner_default4,
-    UstadImage.ICON_PERSON to R.drawable.icon_person,
-
     )
 
 @Composable
