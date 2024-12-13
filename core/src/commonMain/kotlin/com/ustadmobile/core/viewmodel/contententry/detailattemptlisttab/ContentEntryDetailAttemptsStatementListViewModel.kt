4087445ab7a5d3692a528dbfeb1b6b400bf95926
Mandvii <mandvi.verma@sutara.org>
2024-12-14 00:06:39 +0530

    title updated

--- core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/detailattemptlisttab/ContentEntryDetailAttemptsStatementListViewModel.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/viewmodel/contententry/detailattemptlisttab/ContentEntryDetailAttemptsStatementListViewModel.kt
@@ -57,7 +57,7 @@ class ContentEntryDetailAttemptsStatementListViewModel(
                     }
                     _appUiState.update { prev ->
                         prev.copy(
-                            title = "${personNames?.firstNames} ${personNames?.lastName} - $entityUidArg"
+                            title = "${personNames?.firstNames} ${personNames?.lastName}"
                         )
                     }
                 }
