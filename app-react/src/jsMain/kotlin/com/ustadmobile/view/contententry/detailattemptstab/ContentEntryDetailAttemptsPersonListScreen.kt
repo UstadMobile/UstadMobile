e5cb70dd07625a66e1d70199c4904cb010c81633
Mandvii <mandvi.verma@sutara.org>
2024-12-18 22:10:05 +0530

    Person

--- app-react/src/jsMain/kotlin/com/ustadmobile/view/contententry/detailattemptstab/ContentEntryDetailAttemptsPersonListScreen.kt
+++ app-react/src/jsMain/kotlin/com/ustadmobile/view/contententry/detailattemptstab/ContentEntryDetailAttemptsPersonListScreen.kt
@@ -2,7 +2,6 @@ package com.ustadmobile.view.contententry.detailattemptstab
 
 import app.cash.paging.PagingSourceLoadResult
 import com.ustadmobile.core.hooks.collectAsState
-import com.ustadmobile.core.impl.appstate.AppUiState
 import com.ustadmobile.core.paging.RefreshCommand
 import com.ustadmobile.core.viewmodel.contententry.detailattemptlisttab.ContentEntryDetailAttemptsPersonListUiState
 import com.ustadmobile.core.viewmodel.contententry.detailattemptlisttab.ContentEntryDetailAttemptsPersonListViewModel
@@ -10,7 +9,8 @@ import com.ustadmobile.hooks.useDoorRemoteMediator
 import com.ustadmobile.hooks.useMuiAppState
 import com.ustadmobile.hooks.usePagingSource
 import com.ustadmobile.hooks.useUstadViewModel
-import com.ustadmobile.lib.db.composites.PersonAndAttemptInfo
+import com.ustadmobile.lib.db.composites.PersonAndPictureAndNumAttempts
+import com.ustadmobile.lib.db.composites.StatementAndPersonAndPicture
 import com.ustadmobile.view.components.UstadPersonAvatar
 import com.ustadmobile.view.components.virtuallist.VirtualList
 import com.ustadmobile.view.components.virtuallist.VirtualListOutlet
@@ -19,91 +19,152 @@ import js.objects.jso
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import mui.material.Container
+import mui.material.LinearProgress
+import mui.material.LinearProgressVariant
 import mui.material.ListItem
 import mui.material.ListItemButton
 import mui.material.ListItemIcon
 import mui.material.ListItemText
+import mui.material.Stack
+import mui.material.StackDirection
+import mui.system.responsive
+import mui.system.sx
 import react.FC
 import react.Props
 import react.ReactNode
 import react.create
 import tanstack.react.query.UseInfiniteQueryResult
+import web.cssom.AlignContent
 import web.cssom.Contain
 import web.cssom.Height
 import web.cssom.Overflow
+import web.cssom.Position
+import web.cssom.VerticalAlign
 import web.cssom.pct
+import web.cssom.px
 
 
-external interface ContentEntryDetailAttemptsPersonListProps: Props {
-
+external interface ContentEntryDetailAttemptsPersonListProps : Props {
     var uiState: ContentEntryDetailAttemptsPersonListUiState
     var refreshCommandFlow: Flow<RefreshCommand>?
-    var onListItemClick: (PersonAndAttemptInfo) -> Unit
+    var onListItemClick: (PersonAndPictureAndNumAttempts) -> Unit
 
 }
-
 
 val ContentEntryDetailAttemptsPersonListScreen = FC<Props> {
 
     val viewModel = useUstadViewModel { di, savedStateHandle ->
         ContentEntryDetailAttemptsPersonListViewModel(di, savedStateHandle)
     }
-
     val uiState by viewModel.uiState.collectAsState(ContentEntryDetailAttemptsPersonListUiState())
 
+    val contentEntryDetailAttemptsPersonListComponent2 =
+        FC<ContentEntryDetailAttemptsPersonListProps>
+        { props ->
 
-    val contentEntryDetailAttemptsPersonListComponent2 = FC<ContentEntryDetailAttemptsPersonListProps>
-    { props ->
+            val remoteMediatorResult = useDoorRemoteMediator(
+                pagingSourceFactory = props.uiState.attemptsPersonList,
+                refreshCommandFlow = (props.refreshCommandFlow ?: emptyFlow())
+            )
+            println("remoteMediatorResult: $remoteMediatorResult")
 
-        val remoteMediatorResult = useDoorRemoteMediator(
-            pagingSourceFactory = props.uiState.attemptsPersonList,
-            refreshCommandFlow = (props.refreshCommandFlow ?: emptyFlow())
-        )
+            val infiniteQueryResult: UseInfiniteQueryResult<PagingSourceLoadResult<Int, PersonAndPictureAndNumAttempts>, Throwable> =
+                usePagingSource(
+                    remoteMediatorResult.pagingSourceFactory, true, 150
+                )
 
-        val infiniteQueryResult : UseInfiniteQueryResult
-        <PagingSourceLoadResult<Int, PersonAndAttemptInfo>, Throwable> = usePagingSource(
-            remoteMediatorResult.pagingSourceFactory, true, 50
-        )
-        val muiAppState = useMuiAppState()
+            val muiAppState = useMuiAppState()
+            VirtualList {
+                style = jso {
+                    height = "calc(100vh - ${muiAppState.appBarHeight}px)".unsafeCast<Height>()
+                    width = 100.pct
+                    contain = Contain.strict
+                    overflowY = Overflow.scroll
+                }
+                content =
+                    virtualListContent {
+                        infiniteQueryPagingItems(
+                            items = infiniteQueryResult,
+                            key = { it.person.personUid.toString() }
+                        ) { attemptsPersonListItems ->
+                            ListItem.create {
+                                Stack {
+                                    direction = responsive(StackDirection.column)
+                                    ListItemButton {
+                                        onClick = {
+                                            attemptsPersonListItems?.also { props.onListItemClick(it) }
+                                        }
 
-        VirtualList {
-            style = jso {
-                height = "calc(100vh - ${muiAppState.appBarHeight}px)".unsafeCast<Height>()
-                width = 100.pct
-                contain = Contain.strict
-                overflowY = Overflow.scroll
-            }
+                                        ListItemIcon {
+                                            UstadPersonAvatar {
+                                                pictureUri =
+                                                    attemptsPersonListItems?.picture?.personPictureThumbnailUri
+                                                personName =
+                                                    attemptsPersonListItems?.person?.fullName()
+                                            }
+                                        }
+                                        ListItemText {
+                                            primary =
+                                                ReactNode(
+                                                    attemptsPersonListItems?.person?.fullName()
+                                                        ?: ""
+                                                )
+                                            secondary = ReactNode(
+                                                "${attemptsPersonListItems?.numAttempts.toString()} attempts"
+                                                    ?: "0 attempts"
+                                            )
+                                        }
 
-            content = virtualListContent {
 
-                infiniteQueryPagingItems(
-                    items = infiniteQueryResult,
-                    key = { it.person?.personUid?.toString() ?: "0" }
-                ) { attemptsPersonListItems ->
-                    ListItem.create {
-                        ListItemButton{
-                            onClick = {
-                                attemptsPersonListItems?.also { props.onListItemClick(it) }
-                            }
-                            ListItemIcon {
-                                UstadPersonAvatar {
-                                    pictureUri = attemptsPersonListItems?.personPicture?.personPictureThumbnailUri
-                                    personName = attemptsPersonListItems?.person?.fullName()
+                                    }
+                                    Stack {
+                                        direction = responsive(StackDirection.row)
+
+                                        LinearProgress {
+                                            sx {
+                                                width = 500.px
+                                                height = 4.px
+
+                                            }
+                                            variant = LinearProgressVariant.determinate
+                                            // Set value based on whether extensionProgress is not null
+                                            value =
+                                                attemptsPersonListItems?.maxProgress?.let {
+                                                    it
+                                                }
+                                                    ?: (attemptsPersonListItems?.maxScore?.times(
+                                                        100
+                                                    )
+                                                        ?: 0)
+                                        }
+                                        ListItemText {
+                                            primary = ReactNode(
+                                                attemptsPersonListItems?.maxProgress?.let {
+                                                    "${(it)}% Completion"
+                                                }
+                                                    ?: "${((attemptsPersonListItems?.maxScore ?: 0f) * 100).toInt()}% Score"
+                                            )
+                                            sx {
+                                                verticalAlign = VerticalAlign.middle
+                                                marginLeft =
+                                                    8.px
+                                                paddingTop =
+                                                    1.px
+                                            }
+                                        }
+                                    }
+
                                 }
-                            }
-                            ListItemText {
-                                primary = ReactNode(attemptsPersonListItems?.person?.fullName() ?: "")
+
                             }
                         }
                     }
+
+                Container {
+                    VirtualListOutlet()
                 }
             }
-
-            Container {
-                VirtualListOutlet()
-            }
         }
-    }
 
 
     contentEntryDetailAttemptsPersonListComponent2 {
