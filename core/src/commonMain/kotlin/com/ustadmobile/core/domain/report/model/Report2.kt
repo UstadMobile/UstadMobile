d989f509abbbb8e93a4e234ac027c4cc5b735232
Mike Dawson <mike@ustadmobile.com>
2024-12-20 15:45:57 +0400

    Rename Report2 to ReportOptions2

--- core/src/commonMain/kotlin/com/ustadmobile/core/domain/report/model/Report2.kt
+++
@@ -1,9 +0,0 @@
-package com.ustadmobile.core.domain.report.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Report2(
-    val title: String,
-    val series: List<ReportSeries2>
-)
---
+++ core/src/commonMain/kotlin/com/ustadmobile/core/domain/report/model/ReportOptions2.kt
@@ -0,0 +1,13 @@
+package com.ustadmobile.core.domain.report.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents options selected by the user to generate a report. This is serialized into JSON
+ * which is saved as to a string field on the Report entity.
+ */
+@Serializable
+data class ReportOptions2(
+    val title: String,
+    val series: List<ReportSeries2>
+)
