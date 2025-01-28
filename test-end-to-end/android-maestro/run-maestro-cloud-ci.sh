89d1bbc37370cbefa06e339cc8c03b1bd30cb8d2
Mike Dawson <mike@ustadmobile.com>
2025-01-28 13:39:43 +0400

    Add CI maestro cloud CI script

---
+++ test-end-to-end/android-maestro/run-maestro-cloud-ci.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Run Maestro tests using Maestro Cloud
+
+SCRIPTDIR=$(realpath $(dirname $0))
+cd $SCRIPTDIR
+
+
+if [ "$MAESTRO_CLOUD_PROJECTID" == "" ]; then
+  echo "Must set Maestro cloud project id as MAESTRO_CLOUD_PROJECTID environment var"
+  exit 1
+fi
+
+if [ "$MAESTRO_CLOUD_APIKEY" == "" ]; then
+  echo "Must set Maestro cloud API key as MAESTRO_CLOUD_APIKEY environment var"
+  exit 1
+fi
+
+if [ "$TESTCONTROLLER_URL" == "" ]; then
+  echo "TESTCONTROLLER_URL environment var must be set"
+
+function cleanup() {
+    if [ "$TESTCONTROLLER_PID" != "" ]; then
+        wget -qO- "${TESTCONTROLLER_URL}stop"
+        kill $TESTCONTROLLER_PID
+    fi
+}
+
+trap cleanup EXIT
+
+if [ ! -e build/results ]; then
+    mkdir -p build/results
+fi
+
+if [ ! -e build/reports/maestro ]; then
+    mkdir -p build/reports/maestro
+fi
+
+echo "run-maestro-cloud-ci: Time to run Maestro tests"
+
+java -jar ../../testserver-controller/build/libs/testserver-controller-all.jar \
+  -P:url=$TESTCONTROLLER_URL -P:srcRoot=../../ -P:mode=maestro &
+TESTCONTROLLER_PID=$!
+
+maestro cloud \
+    --api-key=$MAESTRO_CLOUD_APIKEY \
+    --project-id=$MAESTRO_CLOUD_PROJECTID \
+    --app-file=../../app-android/build/outputs/apk/release/app-android-release.apk \
+    --flows=e2e-tests
+    --include-tags=hello_world
+    -e TESTCONTROLLER_URL=$TESTCONTROLLER_URL
+
+
+
