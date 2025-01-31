33ec306301701d8c5a2f121bf2876c3289da856e
Mike Dawson <mike@ustadmobile.com>
2025-01-31 17:55:20 +0400

    Update Maestro scripts that make http calls to add retry logic. One test failure seen where retry was probably needed.

--- test-end-to-end/android-maestro/e2e-tests/common/startserver.js
+++ test-end-to-end/android-maestro/e2e-tests/common/startserver.js
@@ -1,5 +1,7 @@
 
 console.log("TESTCONTROLLER_URL = " + TESTCONTROLLER_URL);
+const maxAttempts = 4;
+
 if(typeof(TESTCONTROLLER_URL) == "string" &&
     (TESTCONTROLLER_URL.toLowerCase().startsWith("http://") ||
     TESTCONTROLLER_URL.toLowerCase().startsWith("https://"))
@@ -10,9 +12,23 @@ if(typeof(TESTCONTROLLER_URL) == "string" &&
     output.testServerControllerUrl = "http://localhost:8075/";
     console.log("TESTCONTROLLER_URL not set " + output.testServerControllerUrl);
 }
-const response = http.get(output.testServerControllerUrl + "testcontroller/start");
-const responseJson = JSON.parse(response.body);
-output.SERVER_URL = responseJson.url;
-output.adminUsername = responseJson.adminUsername;
-output.adminPassword = responseJson.adminPassword;
+
+/* Retry should not be needed, but as this can run over a real network, it is implemented just for
+ * rare occasions
+ */
+for(var i = 0; i < maxAttempts; i++) {
+    try {
+        const response = http.get(output.testServerControllerUrl + "testcontroller/start");
+        const responseJson = JSON.parse(response.body);
+        output.SERVER_URL = responseJson.url;
+        output.adminUsername = responseJson.adminUsername;
+        output.adminPassword = responseJson.adminPassword;
+        console.log("startserver.js: SUCCESS : server available on " + output.SERVER_URL);
+        break;
+    }catch(err) {
+        console.log("startserver.js: FAIL : attempt " + i + " failed: " + err);
+        if(i == maxAttempts - 1)
+            throw err
+    }
+}
 
