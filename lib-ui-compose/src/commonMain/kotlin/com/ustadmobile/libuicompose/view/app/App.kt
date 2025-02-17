0f3618d2f5afb685282528ada961288087c03d6d
Anugraha-sutara <Anugraha@sutara.org>
2025-02-17 15:26:11 +0530

    change report string

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/app/App.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/view/app/App.kt
@@ -86,7 +86,7 @@ val APP_TOP_LEVEL_NAV_ITEMS = listOf(
     TopNavigationItem(
         destRoute = ReportListViewModel.DEST_NAME_HOME,
         icon = Icons.Outlined.Analytics,
-        label = MR.strings.report,
+        label = MR.strings.reports,
     )
 
 )
