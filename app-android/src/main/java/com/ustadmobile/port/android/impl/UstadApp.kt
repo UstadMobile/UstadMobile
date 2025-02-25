c7d424e81620ec5de5c6c2951c3edb275a097571
Mike Dawson <mike@ustadmobile.com>
2025-02-25 15:06:42 +0400

    Add migration and generation of contextRegistrationHash.
    ContextRegistrationHash is required for report generation (to count distinct number of sessions).
    
    This requires a migration to update existing statements. It also requires new statements saved in the database to have the hash set.

--- app-android/src/main/java/com/ustadmobile/port/android/impl/UstadApp.kt
+++ app-android/src/main/java/com/ustadmobile/port/android/impl/UstadApp.kt
@@ -174,7 +174,9 @@ import com.ustadmobile.core.uri.UriHelper
 import com.ustadmobile.core.uri.UriHelperAndroid
 import com.ustadmobile.core.util.ext.appMetaData
 import com.ustadmobile.core.util.ext.getOrGenerateNodeIdAndAuth
+import com.ustadmobile.core.util.ext.toByteArray
 import com.ustadmobile.core.util.ext.toNullIfBlank
+import com.ustadmobile.door.migration.DoorMigrationSync
 import com.ustadmobile.lib.db.entities.UmAccount
 import com.ustadmobile.libcache.CachePathsProvider
 import com.ustadmobile.libcache.UstadCache
@@ -351,6 +353,7 @@ class UstadApp : Application(), DIAware, ImageLoaderFactory{
 
 
             val nodeIdAndAuth: NodeIdAndAuth = instance()
+            val xxHasher64Factory: XXHasher64Factory = instance()
 
             Log.i("MigrateIssue", "Creating database name=$dbName")
             val db = DatabaseBuilder.databaseBuilder(
@@ -367,6 +370,41 @@ class UstadApp : Application(), DIAware, ImageLoaderFactory{
                 .addMigrations(MIGRATION_161_162_CLIENT)
                 .addMigrations(MIGRATION_169_170_CLIENT)
                 .addMigrations(MIGRATE_USERNAME_CLIENT)
+                .addMigrations(
+                    DoorMigrationSync(203, 204) { db ->
+                        db.beginTransaction()
+                        db.execSQL("ALTER TABLE StatementEntity ADD COLUMN contextRegistrationHash INTEGER NOT NULL DEFAULT 0")
+                        db.query("""
+                             SELECT DISTINCT StatementEntity.contextRegistrationHi, StatementEntity.contextRegistrationLo,
+                               FROM StatementEntity
+                              WHERE StatementEntity.contextRegistrationHash = 0
+                                AND NOT(StatementEntity.statementIdHi = 0 AND StatementEntity.statementIdLo = 0)
+                              LIMIT 1000  
+                        """).use { cursor ->
+                            while(cursor.moveToNext()) {
+                                val regHi = cursor.getLong(cursor.getColumnIndexOrThrow("contextRegistrationHi"))
+                                val regLo = cursor.getLong(cursor.getColumnIndexOrThrow("contextRegistrationLo"))
+                                val hasher = xxHasher64Factory.newHasher(0)
+                                hasher.update(regHi.toByteArray())
+                                hasher.update(regLo.toByteArray())
+                                val hash = hasher.digest()
+
+                                db.execSQL(
+                                    """
+                                        UPDATE StatementEntity
+                                           SET contextRegistrationHash = ?
+                                         WHERE contextRegistrationHi = ?
+                                           AND contextRegistrationLo = ?   
+                                    """,
+                                    arrayOf(hash, regHi, regLo)
+                                )
+                            }
+                        }
+
+                        db.setTransactionSuccessful()
+                        db.endTransaction()
+                    }
+                )
                 .build()
 
             Log.i("MigrateIssue", "Database built: name=$dbName")
