e52834b09d0b571ea4d40d71a1ab9fcd4324e90b
Mandvii <mandvi.verma@sutara.org>
2025-03-31 02:55:58 +0530

    Test

--- app-react/src/jsMain/kotlin/com/ustadmobile/view/clazz/detailoverview/ClazzDetailOverviewScreen.kt
+++ app-react/src/jsMain/kotlin/com/ustadmobile/view/clazz/detailoverview/ClazzDetailOverviewScreen.kt
@@ -115,6 +115,13 @@ val ClazzDetailOverviewComponent = FC<ClazzDetailOverviewProps> { props ->
                                             props.onClickPermissions()
                                         }
                                     }
+                                    UstadQuickActionButton {
+                                        text = strings[MR.strings.copy]
+                                        icon = FileCopyIcon.create()
+                                        onClick = {
+                                            props.onClickEdit(ClazzDetailOverviewViewModel.COPY)
+                                        }
+                                    }
 
                                 }
                             }
