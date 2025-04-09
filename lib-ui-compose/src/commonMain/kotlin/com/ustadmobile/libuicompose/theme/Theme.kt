51aa68a39d00be1c8c7f743a931eed332ba50b15
POOJALAKSHMI P RAVI <pooja@ustadmobile.com>
2025-04-09 14:04:21 +0400

    Update lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/theme/Theme.kt

--- lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/theme/Theme.kt
+++ lib-ui-compose/src/commonMain/kotlin/com/ustadmobile/libuicompose/theme/Theme.kt
@@ -3,11 +3,15 @@ package com.ustadmobile.libuicompose.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+
+
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 
 
 private val lightScheme = lightColorScheme(
@@ -266,3 +270,4 @@ fun UstadAppTheme(
         content = content
     )
 }
+
