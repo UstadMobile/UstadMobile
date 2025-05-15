9e20fad4ead4cdf4116aef872bd764dd65832c76
Mike Dawson <mike@ustadmobile.com>
2025-05-15 13:59:59 +0400

    Refactor passkey rpId and user id handling:
    Update CreatePublicKeyCredentialCreationOptionsJsonUseCase - set the user handle (user id) as passkeyUid@learningspace - as per the spec avoiding the use of any personal information, but including the server url.
    
    Remove rpId from PersonPasskey (this is system wide, not a per entity variable).

--- core/src/commonMain/kotlin/com/ustadmobile/core/domain/credentials/passkey/request/CreatePublicKeyCredentialCreationOptionsJsonUseCase.kt
+++ core/src/commonMain/kotlin/com/ustadmobile/core/domain/credentials/passkey/request/CreatePublicKeyCredentialCreationOptionsJsonUseCase.kt
@@ -1,16 +1,19 @@
 package com.ustadmobile.core.domain.credentials.passkey.request
 
-import com.benasher44.uuid.uuid4
 import com.ustadmobile.core.impl.UstadMobileSystemImpl
 import com.ustadmobile.core.impl.config.SystemUrlConfig
 import com.ustadmobile.core.domain.credentials.passkey.model.PublicKeyCredentialCreationOptionsJSON
 import com.ustadmobile.core.domain.credentials.passkey.model.PublicKeyCredentialRpEntity
 import com.ustadmobile.lib.util.randomString
 import com.ustadmobile.core.MR
+import com.ustadmobile.core.account.LearningSpace
+import com.ustadmobile.core.db.UmAppDatabase
 import com.ustadmobile.core.domain.credentials.passkey.model.AuthenticatorSelectionCriteria
 import com.ustadmobile.core.domain.credentials.passkey.model.PublicKeyCredentialParameters
 import com.ustadmobile.core.domain.credentials.passkey.model.PublicKeyCredentialUserEntityJSON
 import com.ustadmobile.core.domain.credentials.username.CreateCredentialUsernameUseCase
+import com.ustadmobile.door.ext.doorPrimaryKeyManager
+import com.ustadmobile.lib.db.entities.PersonPasskey
 import io.ktor.http.Url
 import io.ktor.util.encodeBase64
 
@@ -19,20 +22,37 @@ import io.ktor.util.encodeBase64
  * platforms where passkeys are supported (Android, Web, etc).
  *
  * As per https://w3c.github.io/webauthn/#dictdef-publickeycredentialcreationoptionsjson
+ *
+ * The passkey rpId will always be the SystemUrlConfig.systemBaseUrl host
+ * The passkey user.id will be a unique 64bit long @
+ *
+ * The passkey userHandle (e.g. PublicKeyCredentialUserEntityJSON.id) will be
+ * "passkeyUid@https://learningspace.example.org/" where passkeyUid is the UID of the PersonPasskey
+ * entity in the database and https://learningspace.example.org/ is the learning space url.
+ * As per https://w3c.github.io/webauthn/#dictionary-user-credential-params the userHandle MUST
+ * NOT contain any personally identifiable information (like usernames, email, phone etc).
+ *
+ * This user handle allows the app to verify the passkey on the server because it includes a) the
+ * learning space url and b) the uid of the passkey itself. The server can then authenticate a
+ * credential manager response (including determining the related user).
  */
 class CreatePublicKeyCredentialCreationOptionsJsonUseCase(
     private val systemUrlConfig: SystemUrlConfig,
     private val systemImpl: UstadMobileSystemImpl,
     private val createCredentialUsernameUseCase: CreateCredentialUsernameUseCase,
+    private val learningSpace: LearningSpace,
+    private val db: UmAppDatabase,
 ) {
 
     operator fun invoke(
         username: String,
     ): PublicKeyCredentialCreationOptionsJSON {
-        val challenge = randomString(16)
+        val challenge = randomString(16) //TODO note: this should really take place on the server side
         val credentialUsername = createCredentialUsernameUseCase(
             username = username,
         )
+
+        val personPasskeyUid = db.doorPrimaryKeyManager.nextId(PersonPasskey.TABLE_ID)
 
         return PublicKeyCredentialCreationOptionsJSON(
             rp = PublicKeyCredentialRpEntity(
@@ -41,7 +61,7 @@ class CreatePublicKeyCredentialCreationOptionsJsonUseCase(
                 icon = null,
             ),
             user = PublicKeyCredentialUserEntityJSON(
-                id = uuid4().toString(),
+                id = "$personPasskeyUid@${learningSpace.url}",
                 name = credentialUsername,
                 displayName = credentialUsername,
             ),
