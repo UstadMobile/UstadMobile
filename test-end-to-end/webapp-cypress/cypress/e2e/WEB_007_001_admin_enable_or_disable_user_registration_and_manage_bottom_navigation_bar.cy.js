e364661942311b954d4cbba381a45735a0add144
pooja <pooja@ustadmobile.com>
2025-06-16 16:42:46 +0400

    changed visible to exist

--- test-end-to-end/webapp-cypress/cypress/e2e/WEB_007_001_admin_enable_or_disable_user_registration_and_manage_bottom_navigation_bar.cy.js
+++ test-end-to-end/webapp-cypress/cypress/e2e/WEB_007_001_admin_enable_or_disable_user_registration_and_manage_bottom_navigation_bar.cy.js
@@ -54,7 +54,6 @@ it('Admin disable registration', () => {
   cy.get('@editor').click().clear()
   cy.get('.Mui-checked.PrivateSwitchBase-root', { timeout: 5000 }).eq(0).should('exist') //verified registration_allowed switch is on
   cy.get('#registration_allowed').click()
-  //cy.get('.Mui-checked.PrivateSwitchBase-root', { timeout: 5000 }).should('not.exist') //verified registration_allowed switch is off
   cy.get('#actionBarButton').click()
   cy.contains('Yes', { timeout: 5000 }).should('not.exist')
   cy.contains('No', { timeout: 10000 }).should('exist')
@@ -62,7 +61,6 @@ it('Admin disable registration', () => {
 
 it('Verify New user registration is disabled', () => {
   cy.ustadClearIndexDb()
-
   cy.visit('/', {timeout:60000})
   cy.contains('button[class*="MuiButton-outlinedPrimary"]', 'New user').should('not.exist') // Verified new user registration is disabled
   cy.get('input#username', { timeout: 10000 }).should('exist')
@@ -74,10 +72,8 @@ it('Admin manage bottom navigation bar', () => {
   cy.get('#settings_button').click()
   cy.contains('Site').click()
   cy.contains('Edit').click()
- //https://docs.cypress.io/api/commands/should#Assert-the-href-attribute-is-equal-to-users
   cy.get('#Messages').click()
   cy.get('#People').click()
-  //cy.get('.Mui-checked.PrivateSwitchBase-root', { timeout: 5000 }).should('not.exist') //verified registration_allowed switch is off
   cy.get('#actionBarButton').click()
   cy.contains('Courses').should('exist')
   cy.contains('Library').should('exist')
